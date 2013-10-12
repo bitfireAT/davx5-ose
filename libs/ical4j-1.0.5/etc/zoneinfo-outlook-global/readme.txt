cefaffa6503ebf166a821123c99b8867801eed67
Ricki Hirner <hirner@bitfire.at>
2013-10-12 14:49:06 +0200

    Ant build support
    * build.xml for Ant
    * necessary library re-organization
    * compatiblity changes to code
    * now buildable via Ant and Eclipse ADT
    * eclipse-libs has to be included for ADT building only
    * ready for Fdroid

--- libs/ical4j-1.0.5/etc/zoneinfo-outlook-global/readme.txt
+++
@@ -1,2 +0,0 @@
-
-Please see the ical4j-zoneinfo-outlook sub-project for Outlook-compatible timezone definitions.
--- libs/simple-xml-2.7/test/src/org/simpleframework/xml/core/Marshaller.java
+++
@@ -1,6 +0,0 @@
-package org.simpleframework.xml.core;
-
-public interface Marshaller {
-   String marshallText(Object value, Class type) throws Exception;
-   Object unmarshallText(String value, Class type) throws Exception;
-}
