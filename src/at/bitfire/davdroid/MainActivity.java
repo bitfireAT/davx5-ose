db32d909b06b3cde8937431941f6616bc489c832
Ricki Hirner <hirner@bitfire.at>
2013-10-30 11:37:27 +0100

    Main activity, minor changes
    * Main activity with description and links
    * Decrease MAX_UPDATES_BEFORE_COMMIT to 25 (may fix #25)

---
+++ src/at/bitfire/davdroid/MainActivity.java
@@ -0,0 +1,51 @@
+package at.bitfire.davdroid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_main);
+		
+		TextView tv = (TextView)findViewById(R.id.text_info);
+		tv.setText(Html.fromHtml(getString(R.string.html_info)));
+	    tv.setMovementMethod(LinkMovementMethod.getInstance());
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.main_activity, menu);
+	    return true;
+	}
+
+	
+	public void addAccount(MenuItem item) {
+		Intent intent = new Intent(Settings.ACTION_ADD_ACCOUNT);
+		startActivity(intent);
+	}
+
+	public void showSyncSettings(MenuItem item) {
+		Intent intent = new Intent(Settings.ACTION_SYNC_SETTINGS);
+		startActivity(intent);
+	}
+
+	public void showWebsite(MenuItem item) {
+		Intent intent = new Intent(Intent.ACTION_VIEW);
+		intent.setData(Uri.parse(Constants.WEB_URL_HELP + "&pk_kwd=main-activity"));
+		startActivity(intent);
+	}
+}
