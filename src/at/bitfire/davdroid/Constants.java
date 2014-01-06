4dc65d01444213d2eec10f76838b1de4df49bf9b
rfc2822 <hirner@bitfire.at>
2014-01-06 05:48:56 +0100

    Version bump to 0.5.5-alpha

--- src/at/bitfire/davdroid/Constants.java
+++ src/at/bitfire/davdroid/Constants.java
@@ -12,7 +12,7 @@ package at.bitfire.davdroid;
 
 public class Constants {
 	public static final String
-		APP_VERSION = "0.5.4-alpha",
+		APP_VERSION = "0.5.5-alpha",
 		
 		ACCOUNT_TYPE = "bitfire.at.davdroid",
 		
