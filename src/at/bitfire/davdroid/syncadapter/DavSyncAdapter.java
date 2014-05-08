3e4efe8b82370a9ae18e8be313eb148b298b2f33
rfc2822 <hirner@bitfire.at>
2014-05-08 21:18:45 +0200

    Debug settings
    * preference screen for debug settings
    * allow to disable HTTP compression
    * verbose network logging only when activated
    * single HttpClient for all threads (before: single HttpClient per adapter)
    * bump version to 0.5.13-alpha

--- src/at/bitfire/davdroid/syncadapter/DavSyncAdapter.java
+++ src/at/bitfire/davdroid/syncadapter/DavSyncAdapter.java
@@ -15,10 +15,10 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
+import lombok.Getter;
+
 import org.apache.http.HttpStatus;
 
-import ch.boye.httpclientandroidlib.impl.client.CloseableHttpClient;
-import lombok.Getter;
 import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.content.AbstractThreadedSyncAdapter;
@@ -36,6 +36,7 @@ import at.bitfire.davdroid.resource.RemoteCollection;
 import at.bitfire.davdroid.webdav.DavException;
 import at.bitfire.davdroid.webdav.DavHttpClient;
 import at.bitfire.davdroid.webdav.HttpException;
+import ch.boye.httpclientandroidlib.impl.client.CloseableHttpClient;
 
 public abstract class DavSyncAdapter extends AbstractThreadedSyncAdapter implements Closeable {
 	private final static String TAG = "davdroid.DavSyncAdapter";
@@ -44,8 +45,17 @@ public abstract class DavSyncAdapter extends AbstractThreadedSyncAdapter impleme
 	
 	protected AccountManager accountManager;
 	
-	protected CloseableHttpClient httpClient = DavHttpClient.create();
-	final ReentrantReadWriteLock httpClientLock = new ReentrantReadWriteLock();
+	/* We use one static httpClient for
+	 *   - all sync adapters  (CalendarsSyncAdapter, ContactsSyncAdapter)
+	 *   - and all threads (= accounts) of each sync adapter
+	 * so that HttpClient's threaded pool management can do its best.
+	 */
+	protected static CloseableHttpClient httpClient;
+
+	/* One static read/write lock pair for the static httpClient:
+	 *    Use the READ  lock when httpClient will only be called (to prevent it from being unset while being used).
+	 *    Use the WRITE lock when httpClient will be modified (set/unset). */
+	private final static ReentrantReadWriteLock httpClientLock = new ReentrantReadWriteLock();
 
 	
 	public DavSyncAdapter(Context context) {
@@ -61,14 +71,18 @@ public abstract class DavSyncAdapter extends AbstractThreadedSyncAdapter impleme
 	
 	@Override
 	public void close() {
-		// apparently may be called from a GUI thread
+		Log.d(TAG, "Closing httpClient");
+		
+		// may be called from a GUI thread, so we need an AsyncTask
 		new AsyncTask<Void, Void, Void>() {
 			@Override
 			protected Void doInBackground(Void... params) {
 				try {
 					httpClientLock.writeLock().lock();
-					httpClient.close();
-					httpClient = null;
+					if (httpClient != null) {
+						httpClient.close();
+						httpClient = null;
+					}
 					httpClientLock.writeLock().unlock();
 				} catch (IOException e) {
 					Log.w(TAG, "Couldn't close HTTP client", e);
@@ -77,7 +91,6 @@ public abstract class DavSyncAdapter extends AbstractThreadedSyncAdapter impleme
 			}
 		}.execute();
 	}
-	
 	
 	protected abstract Map<LocalCollection<?>, RemoteCollection<?>> getSyncPairs(Account account, ContentProviderClient provider);
 	
@@ -89,42 +102,57 @@ public abstract class DavSyncAdapter extends AbstractThreadedSyncAdapter impleme
 		// set class loader for iCal4j ResourceLoader
 		Thread.currentThread().setContextClassLoader(getContext().getClassLoader());
 		
-		Map<LocalCollection<?>, RemoteCollection<?>> syncCollections = getSyncPairs(account, provider);
-		if (syncCollections == null)
-			Log.i(TAG, "Nothing to synchronize");
-		else
-			try {
-				// prevent httpClient shutdown until we're ready
-				httpClientLock.readLock().lock();
-				
-				for (Map.Entry<LocalCollection<?>, RemoteCollection<?>> entry : syncCollections.entrySet())
-					new SyncManager(entry.getKey(), entry.getValue()).synchronize(extras.containsKey(ContentResolver.SYNC_EXTRAS_MANUAL), syncResult);
-				
-			} catch (DavException ex) {
-				syncResult.stats.numParseExceptions++;
-				Log.e(TAG, "Invalid DAV response", ex);
-				
-			} catch (HttpException ex) {
-				if (ex.getCode() == HttpStatus.SC_UNAUTHORIZED) {
-					Log.e(TAG, "HTTP Unauthorized " + ex.getCode(), ex);
-					syncResult.stats.numAuthExceptions++;
-				} else if (ex.isClientError()) {
-					Log.e(TAG, "Hard HTTP error " + ex.getCode(), ex);
+		// create httpClient, if necessary
+		httpClientLock.writeLock().lock();
+		if (httpClient == null) {
+			Log.d(TAG, "Creating new DavHttpClient");
+			httpClient = DavHttpClient.create(getContext());
+		}
+		
+		// prevent httpClient shutdown until we're ready by holding a read lock
+		// acquiring read lock before releasing write lock will downgrade the write lock to a read lock
+		httpClientLock.readLock().lock();
+		httpClientLock.writeLock().unlock();
+		
+		try {	
+			// get local <-> remote collection pairs
+			Map<LocalCollection<?>, RemoteCollection<?>> syncCollections = getSyncPairs(account, provider);
+			if (syncCollections == null)
+				Log.i(TAG, "Nothing to synchronize");
+			else
+				try {
+					for (Map.Entry<LocalCollection<?>, RemoteCollection<?>> entry : syncCollections.entrySet())
+						new SyncManager(entry.getKey(), entry.getValue()).synchronize(extras.containsKey(ContentResolver.SYNC_EXTRAS_MANUAL), syncResult);
+					
+				} catch (DavException ex) {
 					syncResult.stats.numParseExceptions++;
-				} else {
-					Log.w(TAG, "Soft HTTP error " + ex.getCode() + " (Android will try again later)", ex);
+					Log.e(TAG, "Invalid DAV response", ex);
+					
+				} catch (HttpException ex) {
+					if (ex.getCode() == HttpStatus.SC_UNAUTHORIZED) {
+						Log.e(TAG, "HTTP Unauthorized " + ex.getCode(), ex);
+						syncResult.stats.numAuthExceptions++;
+					} else if (ex.isClientError()) {
+						Log.e(TAG, "Hard HTTP error " + ex.getCode(), ex);
+						syncResult.stats.numParseExceptions++;
+					} else {
+						Log.w(TAG, "Soft HTTP error " + ex.getCode() + " (Android will try again later)", ex);
+						syncResult.stats.numIoExceptions++;
+					}
+					
+				} catch (LocalStorageException ex) {
+					syncResult.databaseError = true;
+					Log.e(TAG, "Local storage (content provider) exception", ex);
+				} catch (IOException ex) {
 					syncResult.stats.numIoExceptions++;
+					Log.e(TAG, "I/O error (Android will try again later)", ex);
 				}
-				
-			} catch (LocalStorageException ex) {
-				syncResult.databaseError = true;
-				Log.e(TAG, "Local storage (content provider) exception", ex);
-			} catch (IOException ex) {
-				syncResult.stats.numIoExceptions++;
-				Log.e(TAG, "I/O error (Android will try again later)", ex);
-			} finally {
-				// allow httpClient shutdown
-				httpClientLock.readLock().unlock();
-			}
+		} finally {
+			// allow httpClient shutdown
+			httpClientLock.readLock().unlock();
+		}
+		
+		Log.i(TAG, "Sync complete for " + authority);
 	}
+
 }
