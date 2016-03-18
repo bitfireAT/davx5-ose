17fbae2aed0b7d248a05c6527f57d3a7e424725c
Ricki Hirner <hirner@bitfire.at>
2016-03-18 15:40:05 +0100

    Account management: Create address book (similar to create calendar)

--- app/src/main/java/at/bitfire/davdroid/log/LogcatHandler.java
+++ app/src/main/java/at/bitfire/davdroid/log/LogcatHandler.java
@@ -10,13 +10,14 @@ package at.bitfire.davdroid.log;
 
 import android.util.Log;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 
 public class LogcatHandler extends Handler {
-    private static final String TAG = "davdroid";
-
+    private static final int MAX_LINE_LENGTH = 3000;
     public static final LogcatHandler INSTANCE = new LogcatHandler();
 
     private LogcatHandler() {
@@ -27,19 +28,24 @@ public class LogcatHandler extends Handler {
 
     @Override
     public void publish(LogRecord r) {
-        String line = getFormatter().format(r);
+        String text = getFormatter().format(r);
         int level = r.getLevel().intValue();
 
-        if (level >= Level.SEVERE.intValue())
-            Log.e(r.getLoggerName(), line);
-        else if (level >= Level.WARNING.intValue())
-            Log.w(r.getLoggerName(), line);
-        else if (level >= Level.CONFIG.intValue())
-            Log.i(r.getLoggerName(), line);
-        else if (level >= Level.FINER.intValue())
-            Log.d(r.getLoggerName(), line);
-        else
-            Log.v(r.getLoggerName(), line);
+        int end = text.length();
+        for (int pos = 0; pos < end; pos += MAX_LINE_LENGTH) {
+            String line = text.substring(pos, NumberUtils.min(pos + MAX_LINE_LENGTH, end));
+
+            if (level >= Level.SEVERE.intValue())
+                Log.e(r.getLoggerName(), line);
+            else if (level >= Level.WARNING.intValue())
+                Log.w(r.getLoggerName(), line);
+            else if (level >= Level.CONFIG.intValue())
+                Log.i(r.getLoggerName(), line);
+            else if (level >= Level.FINER.intValue())
+                Log.d(r.getLoggerName(), line);
+            else
+                Log.v(r.getLoggerName(), line);
+        }
     }
 
     @Override
