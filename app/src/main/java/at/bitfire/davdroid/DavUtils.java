2280f899ee268333df2f1530f3585cd2d7610cf6
Ricki Hirner <hirner@bitfire.at>
2016-03-31 00:52:10 +0200

    Use last path segment as collection display name if there's no DAV:displayName
    * use last path segment as collection display name if there's no DAV:displayName
    * add Contacts Provider Settings again to show contacts without groups in all clients (bluetooth cars etc.)

--- app/src/main/java/at/bitfire/davdroid/DavUtils.java
+++ app/src/main/java/at/bitfire/davdroid/DavUtils.java
@@ -8,8 +8,19 @@
 
 package at.bitfire.davdroid;
 
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+
+import org.w3c.dom.Text;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import ezvcard.util.StringUtils;
+import okhttp3.HttpUrl;
 
 public class DavUtils {
 
@@ -17,6 +28,18 @@ public class DavUtils {
         byte alpha = (byte)(colorWithAlpha >> 24);
         int color = colorWithAlpha & 0xFFFFFF;
         return String.format("#%06X%02X", color, alpha);
+    }
+
+    public static String lastSegmentOfUrl(@NonNull String url) {
+        // the list returned by HttpUrl.pathSegments() is unmodifiable, so we have to create a copy
+        List<String> segments = new LinkedList<>(HttpUrl.parse(url).pathSegments());
+        Collections.reverse(segments);
+
+        for (String segment : segments)
+            if (!TextUtils.isEmpty(segment))
+                return segment;
+
+        return "/";
     }
 
 }
