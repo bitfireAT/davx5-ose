0b9727cca6677b004ee62b5fd9fa0ed51f69e867
Ricki Hirner <hirner@bitfire.at>
2016-04-10 18:44:51 +0200

    More detailled OpenTasks installation hint
    * show "reinstall DAVdroid" hint only for Android <6
    * fetch translations from Transifex

--- scripts/fetch-translations.sh
+++ scripts/fetch-translations.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 declare -A android
-android=([ca]=ca [cs]=cs [da]=da [de]=de [es]=es [fr]=fr [hu]=hu [ja]=ja [nl]=nl [pl]=pl [pt]=pt [pt_BR]=pt-rBR [ru]=ru [sr]=sr [tr_TR]=tr-rTR [uk]=uk [zh_CN]=zh-rCN)
+android=([ca]=ca [cs]=cs [da]=da [de]=de [es]=es [fr]=fr [hu]=hu [it]=it [ja]=ja [nl]=nl [pl]=pl [pt]=pt [pt_BR]=pt-rBR [ru]=ru [sr]=sr [tr_TR]=tr-rTR [uk]=uk [zh_CN]=zh-rCN)
 
 for lang in ${!android[@]}
 do
