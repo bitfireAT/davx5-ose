c350baa8634ac479c26476d28b8fc28a30adf826
Ricki Hirner <hirner@bitfire.at>
2016-09-02 12:13:22 +0200

    Fetch translations from Transifex

--- scripts/fetch-translations.sh
+++ scripts/fetch-translations.sh
@@ -5,7 +5,12 @@ android=([ca]=ca [cs]=cs [da]=da [de]=de [es]=es [fr]=fr [hu]=hu [it]=it [ja]=ja
 
 for lang in ${!android[@]}
 do
-	target=../app/src/davdroid/res/values-${android[$lang]}
-	mkdir -p $target
-	curl -n "https://www.transifex.com/api/2/project/davdroid/resource/davdroid/translation/$lang?file" >$target/strings.xml
+	target_app=../app/src/main/res/values-${android[$lang]}
+	target_cert4android=../cert4android/src/main/res/values-${android[$lang]}
+
+	mkdir -p $target_app
+	curl -n "https://www.transifex.com/api/2/project/davdroid/resource/app/translation/$lang?file" >$target_app/strings.xml
+
+	mkdir -p $target_cert4android
+	curl -n "https://www.transifex.com/api/2/project/davdroid/resource/cert4android/translation/$lang?file" >$target_cert4android/strings.xml
 done
