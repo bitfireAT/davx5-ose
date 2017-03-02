5bfdb35cb89f76aae6a170a68792041bd316dcfe
Ricki Hirner <hirner@bitfire.at>
2017-03-02 23:25:01 +0100

    AccountsActivity: show message when global sync is disabled

--- app/src/main/java/at/bitfire/davdroid/ui/AccountsActivity.java
+++ app/src/main/java/at/bitfire/davdroid/ui/AccountsActivity.java
@@ -8,10 +8,13 @@
 
 package at.bitfire.davdroid.ui;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.SyncStatusObserver;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -26,10 +29,16 @@ import java.util.ServiceLoader;
 import at.bitfire.davdroid.R;
 import at.bitfire.davdroid.ui.setup.LoginActivity;
 
-public class AccountsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+import static android.content.ContentResolver.SYNC_OBSERVER_TYPE_SETTINGS;
+
+public class AccountsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, SyncStatusObserver {
 
     private static final ServiceLoader<IAccountsDrawerHandler> serviceLoader = ServiceLoader.load(IAccountsDrawerHandler.class);
     private static final IAccountsDrawerHandler accountsDrawerHandler = serviceLoader.iterator().next();
+
+    private Snackbar syncStatusSnackbar;
+    private Object syncStatusObserver;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +75,42 @@ public class AccountsActivity extends AppCompatActivity implements NavigationVie
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        onStatusChanged(SYNC_OBSERVER_TYPE_SETTINGS);
+        syncStatusObserver = ContentResolver.addStatusChangeListener(SYNC_OBSERVER_TYPE_SETTINGS, this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (syncStatusObserver != null) {
+            ContentResolver.removeStatusChangeListener(syncStatusObserver);
+            syncStatusObserver = null;
+        }
+    }
+
+    @Override
+    public void onStatusChanged(int which) {
+        if (syncStatusSnackbar != null) {
+            syncStatusSnackbar.dismiss();
+            syncStatusSnackbar = null;
+        }
+
+        if (!ContentResolver.getMasterSyncAutomatically()) {
+            syncStatusSnackbar = Snackbar.make(findViewById(R.id.coordinator), R.string.accounts_global_sync_disabled, Snackbar.LENGTH_INDEFINITE)
+                    .setAction(R.string.accounts_global_sync_enable, new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            ContentResolver.setMasterSyncAutomatically(true);
+                        }
+                    });
+            syncStatusSnackbar.show();
+        }
+    }
+
+
+    @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START))
@@ -83,5 +128,6 @@ public class AccountsActivity extends AppCompatActivity implements NavigationVie
 
         return processed;
     }
+
 
 }
