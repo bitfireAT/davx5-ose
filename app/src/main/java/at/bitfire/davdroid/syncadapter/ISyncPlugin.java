87186a8b5131c007db863a29500234583d6fdae9
Ricki Hirner <hirner@bitfire.at>
2017-04-08 20:24:43 +0200

    Billing: ignore DeadObjectExceptions
    * update Android gradle plugin to 2.3.1
    * dav4android update: follow redirects on DELETE

--- app/src/main/java/at/bitfire/davdroid/syncadapter/ISyncPlugin.java
+++ app/src/main/java/at/bitfire/davdroid/syncadapter/ISyncPlugin.java
@@ -1,11 +1,12 @@
 package at.bitfire.davdroid.syncadapter;
 
 import android.content.Context;
+import android.content.SyncResult;
 import android.support.annotation.NonNull;
 
 interface ISyncPlugin {
 
-    boolean beforeSync(@NonNull Context context);
-    void afterSync(@NonNull Context context);
+    boolean beforeSync(@NonNull Context context, @NonNull SyncResult syncResult);
+    void afterSync(@NonNull Context context, @NonNull SyncResult syncResult);
 
 }
