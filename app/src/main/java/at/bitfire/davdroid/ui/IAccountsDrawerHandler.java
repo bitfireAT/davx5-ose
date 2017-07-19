b014ff1ab74587b9da116252c2add2bcf63afd9f
Ricki Hirner <hirner@bitfire.at>
2017-07-19 19:30:57 +0200

    Update copyright

--- app/src/main/java/at/bitfire/davdroid/ui/IAccountsDrawerHandler.java
+++ app/src/main/java/at/bitfire/davdroid/ui/IAccountsDrawerHandler.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright © Ricki Hirner (bitfire web engineering).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ */
+
 package at.bitfire.davdroid.ui;
 
 import android.app.Activity;
