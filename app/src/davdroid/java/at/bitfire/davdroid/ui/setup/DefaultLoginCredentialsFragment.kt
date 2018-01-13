38c6486610c0cbb0e48db497c7c91640e2729b1b
Ricki Hirner <hirner@bitfire.at>
2018-01-13 22:19:53 +0100

    Login with client certificates
    * setup UI: login with URL and client certificate
    * account settings UI: show either username/password or client certificate alias
    * AccountSettings: serve credentials in generalized Credentials objects
    * HttpClient: use Credentials (instead of username/password) for authentication
    * HttpClient: always use CustomTlsSocketFactory
    * CustomTlsSocketFactory: support client certificates

--- app/src/davdroid/java/at/bitfire/davdroid/ui/setup/DefaultLoginCredentialsFragment.kt
+++ app/src/davdroid/java/at/bitfire/davdroid/ui/setup/DefaultLoginCredentialsFragment.kt
@@ -11,6 +11,8 @@ package at.bitfire.davdroid.ui.setup
 import android.app.Fragment
 import android.net.Uri
 import android.os.Bundle
+import android.security.KeyChain
+import android.security.KeyChainAliasCallback
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -37,10 +39,10 @@ class DefaultLoginCredentialsFragment: Fragment(), CompoundButton.OnCheckedChang
                 val password = it.getStringExtra(LoginActivity.EXTRA_PASSWORD)
 
                 if (url != null) {
-                    v.login_type_url.isChecked = true
-                    v.base_url.setText(url)
-                    v.user_name.setText(username)
-                    v.url_password.setText(password)
+                    v.login_type_urlpwd.isChecked = true
+                    v.urlpwd_base_url.setText(url)
+                    v.urlpwd_user_name.setText(username)
+                    v.urlpwd_password.setText(password)
                 } else {
                     v.login_type_email.isChecked = true
                     v.email_address.setText(username)
@@ -49,17 +51,25 @@ class DefaultLoginCredentialsFragment: Fragment(), CompoundButton.OnCheckedChang
             }
         }
 
-        v.login.setOnClickListener({ _ ->
-            validateLoginData()?.let { credentials ->
-                DetectConfigurationFragment.newInstance(credentials).show(fragmentManager, null)
+        v.urlcert_select_cert.setOnClickListener {
+            val baseUrl = Uri.parse(view.urlcert_base_url.text.toString())
+            KeyChain.choosePrivateKeyAlias(activity, KeyChainAliasCallback { alias ->
+                v.urlcert_cert_alias.text = alias
+            }, null, null, baseUrl.host, baseUrl.port,view.urlcert_cert_alias.text.toString())
+        }
+
+        v.login.setOnClickListener {
+            validateLoginData()?.let { info ->
+                DetectConfigurationFragment.newInstance(info).show(fragmentManager, null)
             }
-        })
+        }
 
         // initialize to Login by email
         onCheckedChanged(v)
 
         v.login_type_email.setOnCheckedChangeListener(this)
-        v.login_type_url.setOnCheckedChangeListener(this)
+        v.login_type_urlpwd.setOnCheckedChangeListener(this)
+        v.login_type_urlcert.setOnCheckedChangeListener(this)
 
         return v
     }
@@ -69,90 +79,120 @@ class DefaultLoginCredentialsFragment: Fragment(), CompoundButton.OnCheckedChang
     }
 
     private fun onCheckedChanged(v: View) {
-        val loginByEmail = !v.login_type_url.isChecked
-        v.login_type_email_details.visibility = if (loginByEmail) View.VISIBLE else View.GONE
-        v.login_type_url_details.visibility = if (loginByEmail) View.GONE else View.VISIBLE
-        (if (loginByEmail) v.email_address else v.base_url).requestFocus()
+        v.login_type_email_details.visibility = if (v.login_type_email.isChecked) View.VISIBLE else View.GONE
+        v.login_type_urlpwd_details.visibility = if (v.login_type_urlpwd.isChecked) View.VISIBLE else View.GONE
+        v.login_type_urlcert_details.visibility = if (v.login_type_urlcert.isChecked) View.VISIBLE else View.GONE
     }
 
-    private fun validateLoginData(): LoginCredentials? {
-        if (view.login_type_email.isChecked) {
-            var uri: URI? = null
-            var valid = true
+    private fun validateLoginData(): LoginInfo? {
+        when {
+            // Login with email address
+            view.login_type_email.isChecked -> {
+                var uri: URI? = null
+                var valid = true
 
-            val email = view.email_address.text.toString()
-            if (!email.matches(Regex(".+@.+"))) {
-                view.email_address.error = getString(R.string.login_email_address_error)
-                valid = false
-            } else
-                try {
-                    uri = URI("mailto", email, null)
-                } catch(e: URISyntaxException) {
-                    view.email_address.error = e.localizedMessage
-                    valid = false
-                }
-
-            val password = view.email_password.getText().toString()
-            if (password.isEmpty()) {
-                view.email_password.setError(getString(R.string.login_password_required))
-                valid = false
-            }
-
-            return if (valid && uri != null)
-                LoginCredentials(uri, email, password)
-            else
-                null
-
-        } else if (view.login_type_url.isChecked) {
-            var uri: URI? = null
-            var valid = true
-
-            val baseUrl = Uri.parse(view.base_url.text.toString())
-            val scheme = baseUrl.scheme
-            if (scheme.equals("http", true) || scheme.equals("https", true)) {
-                var host = baseUrl.host
-                if (host.isNullOrBlank()) {
-                    view.base_url.error = getString(R.string.login_url_host_name_required)
+                val email = view.email_address.text.toString()
+                if (!email.matches(Regex(".+@.+"))) {
+                    view.email_address.error = getString(R.string.login_email_address_error)
                     valid = false
                 } else
                     try {
-                        host = IDN.toASCII(host)
-                    } catch(e: IllegalArgumentException) {
-                        Constants.log.log(Level.WARNING, "Host name not conforming to RFC 3490", e)
+                        uri = URI("mailto", email, null)
+                    } catch (e: URISyntaxException) {
+                        view.email_address.error = e.localizedMessage
+                        valid = false
                     }
 
-                val path = baseUrl.encodedPath
-                val port = baseUrl.port
-                try {
-                    uri = URI(baseUrl.scheme, null, host, port, path, null, null)
-                } catch(e: URISyntaxException) {
-                    view.base_url.error = e.localizedMessage
+                val password = view.email_password.getText().toString()
+                if (password.isEmpty()) {
+                    view.email_password.setError(getString(R.string.login_password_required))
                     valid = false
                 }
-            } else {
-                view.base_url.error = getString(R.string.login_url_must_be_http_or_https)
-                valid = false
+
+                return if (valid && uri != null)
+                    LoginInfo(uri, email, password)
+                else
+                    null
+
             }
 
-            val userName = view.user_name.text.toString()
-            if (userName.isBlank()) {
-                view.user_name.error = getString(R.string.login_user_name_required)
-                valid = false
+            // Login with URL and user name
+            view.login_type_urlpwd.isChecked -> {
+                var valid = true
+
+                val baseUrl = Uri.parse(view.urlpwd_base_url.text.toString())
+                val uri = validateBaseUrl(baseUrl, false, { message ->
+                    view.urlpwd_base_url.error = message
+                    valid = false
+                })
+
+                val userName = view.urlpwd_user_name.text.toString()
+                if (userName.isBlank()) {
+                    view.urlpwd_user_name.error = getString(R.string.login_user_name_required)
+                    valid = false
+                }
+
+                val password = view.urlpwd_password.getText().toString()
+                if (password.isEmpty()) {
+                    view.urlpwd_password.setError(getString(R.string.login_password_required))
+                    valid = false
+                }
+
+                return if (valid && uri != null)
+                    LoginInfo(uri, userName, password)
+                else
+                    null
             }
 
-            val password = view.url_password.getText().toString()
-            if (password.isEmpty()) {
-                view.url_password.setError(getString(R.string.login_password_required))
-                valid = false
-            }
+            // Login with URL and client certificate
+            view.login_type_urlcert.isChecked -> {
+                var valid = true
 
-            return if (valid && uri != null)
-                LoginCredentials(uri, userName, password)
-            else
-                null
+                val baseUrl = Uri.parse(view.urlcert_base_url.text.toString())
+                val uri = validateBaseUrl(baseUrl, true, { message ->
+                    view.urlcert_base_url.error = message
+                    valid = false
+                })
+
+                val alias = view.urlcert_cert_alias.text.toString()
+                if (alias.isEmpty())
+                    valid = false
+
+                if (valid && uri != null)
+                    return LoginInfo(uri, certificateAlias = alias)
+            }
         }
 
         return null
+    }
+
+    private fun validateBaseUrl(baseUrl: Uri, httpsRequired: Boolean, reportError: (String) -> Unit): URI? {
+        var uri: URI? = null
+        val scheme = baseUrl.scheme
+        if ((!httpsRequired && scheme.equals("http", true)) || scheme.equals("https", true)) {
+            var host = baseUrl.host
+            if (host.isNullOrBlank())
+                reportError(getString(R.string.login_url_host_name_required))
+            else
+                try {
+                    host = IDN.toASCII(host)
+                } catch (e: IllegalArgumentException) {
+                    Constants.log.log(Level.WARNING, "Host name not conforming to RFC 3490", e)
+                }
+
+            val path = baseUrl.encodedPath
+            val port = baseUrl.port
+            try {
+                uri = URI(baseUrl.scheme, null, host, port, path, null, null)
+            } catch (e: URISyntaxException) {
+                reportError(e.localizedMessage)
+            }
+        } else
+            reportError(getString(if (httpsRequired)
+                    R.string.login_url_must_be_https
+                else
+                    R.string.login_url_must_be_http_or_https))
+        return uri
     }
 
 
