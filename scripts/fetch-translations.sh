8b12cb7a29af50496bc98e7f9a352f0f8e458d64
Ricki Hirner <hirner@bitfire.at>
2018-12-30 11:59:11 +0100

    Fetch translations from Transifex

--- scripts/fetch-translations.sh
+++ scripts/fetch-translations.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 declare -A android
-android=([ar_SA]=ar [ca]=ca [cs]=cs [da]=da [de]=de [es]=es [fr]=fr [hu]=hu [it]=it [ja]=ja [nl]=nl [nb_NO]=nb-rNO [pl]=pl [pt]=pt [pt_BR]=pt-rBR [ru]=ru [sr]=sr [tr_TR]=tr-rTR [uk]=uk [zh_CN]=zh-rCN [zh_TW]=zh-rTW)
+android=([ar_SA]=ar [ca]=ca [cs]=cs [da]=da [de]=de [es]=es [fa]=fa [fr]=fr [hu]=hu [it]=it [ja]=ja [nl]=nl [nb_NO]=nb-rNO [pl]=pl [pt]=pt [pt_BR]=pt-rBR [ru]=ru [sl_SI]=sl-rSI [sr]=sr [tr_TR]=tr-rTR [uk]=uk [zh_CN]=zh-rCN [zh_TW]=zh-rTW)
 
 for lang in ${!android[@]}
 do
@@ -9,9 +9,9 @@ do
 	target_cert4android=../cert4android/src/main/res/values-${android[$lang]}
 
 	mkdir -p $target_app
-	curl -n "https://www.transifex.com/api/2/project/davdroid/resource/app/translation/$lang?file" |
+	curl -n "https://www.transifex.com/api/2/project/davx5/resource/app/translation/$lang?file" |
 		sed 's/\.\.\./…/g' > $target_app/strings.xml
 
 	#mkdir -p $target_cert4android
-	#curl -n "https://www.transifex.com/api/2/project/davdroid/resource/cert4android/translation/$lang?file" >$target_cert4android/strings.xml
+	#curl -n "https://www.transifex.com/api/2/project/davx5/resource/cert4android/translation/$lang?file" >$target_cert4android/strings.xml
 done
