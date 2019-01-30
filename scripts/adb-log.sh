f241de264d9d24add3035d05791f000c7f8c22c4
Ricki Hirner <hirner@bitfire.at>
2019-01-30 17:45:15 +0100

    Update ical4j; show new davx5 logger in scripts/adb-log.sh

--- scripts/adb-log.sh
+++ scripts/adb-log.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 adb shell setprop log.tag.cert4android VERBOSE
-adb shell setprop log.tag.davdroid VERBOSE
+adb shell setprop log.tag.davx5 VERBOSE
 adb shell setprop log.tag.dav4jvm VERBOSE
