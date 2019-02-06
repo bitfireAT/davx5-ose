1b6689133a751762838de08a13963eacbedc86d5
Ricki Hirner <hirner@bitfire.at>
2019-02-06 16:24:12 +0100

    Add Greek translation (thanks Efstathios Iosifidis)

--- scripts/fetch-translations.sh
+++ scripts/fetch-translations.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 declare -A android
-android=([ar_SA]=ar [ca]=ca [cs]=cs [da]=da [de]=de [es]=es [fa]=fa [fr]=fr [gl]=gl [hu]=hu [it]=it [ja]=ja [nl]=nl [nb_NO]=nb-rNO [pl]=pl [pt]=pt [pt_BR]=pt-rBR [ru]=ru [sl_SI]=sl-rSI [sr]=sr [tr_TR]=tr-rTR [uk]=uk [zh_CN]=zh-rCN [zh_TW]=zh-rTW)
+android=([ar_SA]=ar [ca]=ca [cs]=cs [da]=da [de]=de [el]=el [es]=es [fa]=fa [fr]=fr [gl]=gl [hu]=hu [it]=it [ja]=ja [nl]=nl [nb_NO]=nb-rNO [pl]=pl [pt]=pt [pt_BR]=pt-rBR [ru]=ru [sl_SI]=sl-rSI [sr]=sr [tr_TR]=tr-rTR [uk]=uk [zh_CN]=zh-rCN [zh_TW]=zh-rTW)
+
+BASE_DIR=`realpath -L $0 | xargs dirname`/../
 
 for lang in ${!android[@]}
 do
-	target_app=../app/src/main/res/values-${android[$lang]}
-	target_cert4android=../cert4android/src/main/res/values-${android[$lang]}
+	echo Fetching translations for $lang
+	target_app=$BASE_DIR/app/src/main/res/values-${android[$lang]}
+	target_cert4android=$BASE_DIR/cert4android/src/main/res/values-${android[$lang]}
 
 	mkdir -p $target_app
 	curl -n "https://www.transifex.com/api/2/project/davx5/resource/app/translation/$lang?file" |
