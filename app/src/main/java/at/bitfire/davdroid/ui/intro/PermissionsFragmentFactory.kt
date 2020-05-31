0e4e655d511a980999f76f5a2fdb0875507c0063
Ricki Hirner <hirner@bitfire.at>
2020-05-31 14:16:49 +0200

    Link PermissionsActivity from app settings

--- app/src/main/java/at/bitfire/davdroid/ui/intro/PermissionsFragmentFactory.kt
+++ app/src/main/java/at/bitfire/davdroid/ui/intro/PermissionsFragmentFactory.kt
@@ -1,12 +1,16 @@
 package at.bitfire.davdroid.ui.intro
 
 import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import at.bitfire.davdroid.PermissionUtils
 import at.bitfire.davdroid.PermissionUtils.CALENDAR_PERMISSIONS
 import at.bitfire.davdroid.PermissionUtils.CONTACT_PERMSSIONS
 import at.bitfire.davdroid.PermissionUtils.TASKS_PERMISSIONS
+import at.bitfire.davdroid.R
 import at.bitfire.davdroid.settings.Settings
-import at.bitfire.davdroid.ui.PermissionsFragment
 import at.bitfire.davdroid.ui.intro.IIntroFragmentFactory.ShowMode
 
 class PermissionsFragmentFactory: IIntroFragmentFactory {
@@ -20,6 +24,14 @@ class PermissionsFragmentFactory: IIntroFragmentFactory {
             ShowMode.SHOW
     }
 
-    override fun create() = PermissionsFragment()
+    override fun create() = PermissionsIntroFragment()
+
+
+    class PermissionsIntroFragment(): Fragment() {
+
+        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
+                inflater.inflate(R.layout.intro_permissions, container, false)
+
+    }
 
 }
