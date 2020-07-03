71111172cb779622fcd5bbf34f2a2342ce2a23c8
Ricki Hirner <hirner@bitfire.at>
2020-07-03 16:36:48 +0200

    Task improvements

--- scripts/fetch-db.sh
+++ scripts/fetch-db.sh
@@ -2,3 +2,4 @@
 cd ~/tmp
 adb pull /data/data/com.android.providers.contacts/databases/contacts2.db
 adb pull /data/data/com.android.providers.calendar/databases/calendar.db
+adb pull /data/data/org.dmfs.tasks/databases/tasks.db
