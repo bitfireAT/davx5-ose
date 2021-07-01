663957b7dc167588ac8e20235afb95e6c195ab9c
Ricki Hirner <hirner@bitfire.at>
2021-07-02 00:31:22 +0200

    Rewrite HomeSetAdapter so that it uses a Material-style text field instead of an old-style Spinner that doesn't work with dark mode anymore

--- app/src/main/java/at/bitfire/davdroid/ui/HomeSetAdapter.kt
+++ app/src/main/java/at/bitfire/davdroid/ui/HomeSetAdapter.kt
@@ -6,37 +6,44 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Filter
 import android.widget.TextView
+import at.bitfire.davdroid.DavUtils
+import at.bitfire.davdroid.R
 import at.bitfire.davdroid.model.HomeSet
 
 class HomeSetAdapter(
         context: Context
-): ArrayAdapter<HomeSet>(context, android.R.layout.simple_list_item_2, android.R.id.text1) {
+): ArrayAdapter<HomeSet>(context, R.layout.text_list_item, android.R.id.text1) {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val data = getItem(position)!!
 
         val v: View
-        if (!data.displayName.isNullOrBlank()) {
-            v = convertView ?: LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_2, null, false)
-            v.findViewById<TextView>(android.R.id.text1).text = data.displayName
-            v.findViewById<TextView>(android.R.id.text2).apply {
-                text = data.url.toString()
-                setSingleLine()
-                ellipsize = TextUtils.TruncateAt.START
-            }
-        } else {
-            v = convertView ?: LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1, null, false)
-            v.findViewById<TextView>(android.R.id.text1).apply {
-                text = data.url.toString()
-                setSingleLine()
-                ellipsize = TextUtils.TruncateAt.START
-            }
+        v = convertView ?: LayoutInflater.from(context).inflate(R.layout.text_list_item, parent, false)
+        v.findViewById<TextView>(android.R.id.text1).apply {
+            text = data.displayName ?: DavUtils.lastSegmentOfUrl(data.url)
+        }
+        v.findViewById<TextView>(android.R.id.text2).apply {
+            text = data.url.toString()
+            setSingleLine()
+            ellipsize = TextUtils.TruncateAt.START
         }
         return v
     }
 
     override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup) =
             getView(position, convertView, parent)
+
+
+    override fun getFilter() = object: Filter() {
+        override fun convertResultToString(resultValue: Any?): CharSequence {
+            val homeSet = resultValue as HomeSet
+            return homeSet.url.toString()
+        }
+        override fun performFiltering(constraint: CharSequence?) = FilterResults()
+        override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+        }
+    }
 
 }
