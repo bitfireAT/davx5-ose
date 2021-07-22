265d08630101498ac114e1a49c252bdacc599cf6
Ricki Hirner <hirner@bitfire.at>
2021-07-22 16:01:23 +0200

    Use ./fetch-translations instead of "tx pull" again (also fetches translators)

---
+++ scripts/fetch-translations.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+TX_TOKEN=`awk '/password =/ { print $3; }' <$HOME/.transifexrc`
+
+(cd .. && tx pull)
+
+curl -H "Authorization: Bearer $TX_TOKEN" 'https://rest.api.transifex.com/team_memberships?filter\[organization\]=o:bitfireAT&filter\[team\]=o:bitfireAT:t:davx5-team' \
+  | ./rewrite-translators.rb >../app/src/main/assets/translators.json
--- scripts/rewrite-translators.rb
+++ scripts/rewrite-translators.rb
@@ -5,10 +5,17 @@ require 'json'
 contributors = {}
 
 transifex = JSON.parse(STDIN.read, :symbolize_names => true)
-for t in transifex
-  lang = t[:language_code]
-  people = t[:translators]
-  contributors[lang] = people.sort_by { |nick| nick.downcase }
+for t in transifex[:data]
+  raise unless t[:type] == 'team_memberships'
+  next unless t[:attributes][:role] == 'translator'
+  rel = t[:relationships]
+  lang = rel[:language][:data][:id].delete_prefix('l:')
+  user = rel[:user][:data][:id].delete_prefix('u:')
+
+  contributors[lang] = [] if contributors[lang].nil?
+  contributors[lang] << user
 end
+
+contributors.transform_values! { |u| u.sort }
 
 puts contributors.sort.to_h.to_json
