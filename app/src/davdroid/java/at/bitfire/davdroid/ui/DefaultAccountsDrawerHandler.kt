11a5e68093441b2eb89e0b11c662ab1f1532a6a5
Ricki Hirner <hirner@bitfire.at>
2021-09-29 11:03:22 +0200

    Squashed commit of the following:
    commit 56f5f5a6cbad595534148a160678972734ca6cf2
    Author: Ricki Hirner <hirner@bitfire.at>
    Date:   Wed Sep 29 10:59:23 2021 +0200
    
        Version bump to 4.0-beta1
    
    commit 2a4d7501d9d6cd572269de21e82c204c5a2a57e8
    Author: Ricki Hirner <hirner@bitfire.at>
    Date:   Tue Sep 28 21:45:49 2021 +0200
    
        Fix UiUtils.launchUri once again
    
    commit 293accaf8f26842d198e9d5c4fc9029cb6ac71cc
    Author: Ricki Hirner <hirner@bitfire.at>
    Date:   Tue Sep 28 13:47:06 2021 +0200
    
        Adjust gradle daemon heap size
    
    commit 7763a51c9bbc7baee36ef71648889f7fb4fe965d
    Author: Ricki Hirner <hirner@bitfire.at>
    Date:   Tue Sep 28 12:21:37 2021 +0200
    
        Query and show quota
    
    commit 95f4dbfd8ffadd930aab9fc36dc3865bfee54fb7
    Author: Ricki Hirner <hirner@bitfire.at>
    Date:   Mon Sep 27 22:16:48 2021 +0200
    
        Persist cookies over WebDAV sessions
    
    commit 17c9327eb3ef3d9384b28b70aa9637718d34e8a2
    Author: Ricki Hirner <hirner@bitfire.at>
    Date:   Sun Sep 26 19:54:57 2021 +0200
    
        Add WebDAV caches
    
        * add random access page cache
        * add thumbnail cache
        * add HEAD response cache
    
    commit 0e5237f8b178ab66099999f72e657c59c9302d71
    Author: Ricki Hirner <hirner@bitfire.at>
    Date:   Tue Aug 31 13:25:36 2021 +0200
    
        Add WebDAV Access over Storage Access Framework

--- app/src/davdroid/java/at/bitfire/davdroid/ui/DefaultAccountsDrawerHandler.kt
+++ app/src/davdroid/java/at/bitfire/davdroid/ui/DefaultAccountsDrawerHandler.kt
@@ -18,6 +18,7 @@ import android.widget.Toast
 import at.bitfire.davdroid.App
 import at.bitfire.davdroid.BuildConfig
 import at.bitfire.davdroid.R
+import at.bitfire.davdroid.ui.webdav.WebdavMountsActivity
 
 class DefaultAccountsDrawerHandler: IAccountsDrawerHandler {
 
@@ -37,17 +38,22 @@ class DefaultAccountsDrawerHandler: IAccountsDrawerHandler {
         when (item.itemId) {
             R.id.nav_about ->
                 activity.startActivity(Intent(activity, AboutActivity::class.java))
-            R.id.nav_app_settings ->
-                activity.startActivity(Intent(activity, AppSettingsActivity::class.java))
             R.id.nav_beta_feedback ->
                 if (!UiUtils.launchUri(activity, Uri.parse(BETA_FEEDBACK_URI), Intent.ACTION_SENDTO, false))
                     Toast.makeText(activity, R.string.install_email_client, Toast.LENGTH_LONG).show()
+            R.id.nav_app_settings ->
+                activity.startActivity(Intent(activity, AppSettingsActivity::class.java))
+
             R.id.nav_twitter ->
                 UiUtils.launchUri(activity,
                         Uri.parse("https://twitter.com/" + activity.getString(R.string.twitter_handle)))
             R.id.nav_website ->
                 UiUtils.launchUri(activity,
                         App.homepageUrl(activity))
+
+            R.id.nav_webdav_mounts ->
+                activity.startActivity(Intent(activity, WebdavMountsActivity::class.java))
+
             R.id.nav_manual ->
                 UiUtils.launchUri(activity,
                         App.homepageUrl(activity).buildUpon().appendPath("manual").build())
@@ -64,6 +70,7 @@ class DefaultAccountsDrawerHandler: IAccountsDrawerHandler {
             R.id.nav_privacy ->
                 UiUtils.launchUri(activity,
                         App.homepageUrl(activity).buildUpon().appendPath("privacy").build())
+
             else ->
                 return false
         }
