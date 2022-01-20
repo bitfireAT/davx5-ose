ee83ec0bcfd280e2b6c53f9e2e3b1bb24a303c93
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2022-01-20 11:24:34 +0100

    Cleaned some code

--- app/src/main/java/at/bitfire/davdroid/ui/AccountsActivity.kt
+++ app/src/main/java/at/bitfire/davdroid/ui/AccountsActivity.kt
@@ -125,14 +125,14 @@ class AccountsActivity: AppCompatActivity(), NavigationView.OnNavigationItemSele
             finish()
         else if (requestCode == WEB_VIEW_LOGIN_REQ && resultCode == RESULT_OK) {
             val code = data?.extras?.getString(InfomaniakLogin.CODE_TAG)
-            if (!code.isNullOrBlank()) {
-                val intent = Intent(this, LoginActivity::class.java).apply {
-                    putExtra("code", code)
-                }
-                startActivity(intent)
-            } else {
+            if (code.isNullOrBlank()) {
                 val translatedError = data?.extras?.getString(InfomaniakLogin.ERROR_TRANSLATED_TAG)
                 Toast.makeText(this, translatedError, Toast.LENGTH_LONG).show()
+            } else {
+                Intent(this, LoginActivity::class.java).apply {
+                    putExtra("code", code)
+                    startActivity(this)
+                }
             }
         }
         else
