98633e3d32e8138af6c6227be5bd618b32304e99
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2022-01-20 11:28:08 +0100

    Replace tab character with spaces for indent

--- app/src/main/java/com/infomaniak/sync/GlobalConstants.kt
+++ app/src/main/java/com/infomaniak/sync/GlobalConstants.kt
@@ -7,7 +7,7 @@ object GlobalConstants {
     private const val API_ENDPOINT = "https://api.infomaniak.com/1"
     private const val LOGIN_ENDPOINT = "https://login.infomaniak.com"
 
-	const val TOKEN_LOGIN_URL = "$LOGIN_ENDPOINT/token"
+    const val TOKEN_LOGIN_URL = "$LOGIN_ENDPOINT/token"
     const val PASSWORD_API_URL = "$API_ENDPOINT/profile/password"
     const val PROFILE_API_URL = "$API_ENDPOINT/profile"
 }
