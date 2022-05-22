dddbce93f72ff11862aa02c38af54775ea7e1ac5
Ricki Hirner <hirner@bitfire.at>
2022-05-22 18:48:15 +0200

    Use Koin for all tests; CI: run tests for all flavors (#91)
    Closes bitfireAT/davx5#89

---
+++ app/src/davdroid/java/at/bitfire/davdroid/FlavorModules.kt
@@ -0,0 +1,14 @@
+/***************************************************************************************************
+ * Copyright © All Contributors. See LICENSE and AUTHORS in the root directory for details.
+ **************************************************************************************************/
+
+package at.bitfire.davdroid
+
+import org.koin.dsl.module
+
+object FlavorModules {
+
+    val flavorModule = module {
+    }
+
+}
