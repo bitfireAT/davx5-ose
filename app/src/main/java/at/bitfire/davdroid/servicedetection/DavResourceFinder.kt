6d7d9257c8898009f8fa4a8d9f5e60ef9715c145
Michael Biebl <mbiebl@messageconcept.com>
2022-12-11 16:49:15 +0100

    Remove duplicate copyright header

--- app/src/main/java/at/bitfire/davdroid/servicedetection/DavResourceFinder.kt
+++ app/src/main/java/at/bitfire/davdroid/servicedetection/DavResourceFinder.kt
@@ -1,10 +1,6 @@
 /***************************************************************************************************
  * Copyright © All Contributors. See LICENSE and AUTHORS in the root directory for details.
  **************************************************************************************************/
-
-/***************************************************************************************************
- * Copyright © All Contributors. See LICENSE and AUTHORS in the root directory for details.
- **************************************************************************************************/
 package at.bitfire.davdroid.servicedetection
 
 import android.content.Context
