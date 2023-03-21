a3a8317b2fcf70c25274dd04bfcd602bef4b4026
Arnau Mora <Arnyminer.Z@gmail.com>
2023-03-21 15:44:54 +0100

    Fix fetching list of translators (bitfireAT/davx5#232)
    * Changed `password` with `token`
    
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>
    
    * Fetch script: don't use --minimum-perc 20 because it's in .tx/config
    
    ---------
    
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>
    Co-authored-by: Ricki Hirner <hirner@bitfire.at>

--- scripts/fetch-translations.sh
+++ scripts/fetch-translations.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-#TX_TOKEN=`awk '/password =/ { print $3; }' <$HOME/.transifexrc`
+TX_TOKEN=`awk '/token *=/ { print $3; }' <$HOME/.transifexrc`
 
 (cd .. && tx pull -a -f --use-git-timestamps)
 
-# FIXME
-#curl -H "Authorization: Bearer $TX_TOKEN" 'https://rest.api.transifex.com/team_memberships?filter\[organization\]=o:bitfireAT&filter\[team\]=o:bitfireAT:t:davx5-team' \
-#  | ./rewrite-translators.rb >../app/src/main/assets/translators.json
+curl -H "Authorization: Bearer $TX_TOKEN" 'https://rest.api.transifex.com/team_memberships?filter\[organization\]=o:bitfireAT&filter\[team\]=o:bitfireAT:t:davx5-team' \
+  | ./rewrite-translators.rb >../app/src/main/assets/translators.json
