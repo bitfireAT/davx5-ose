1e59b834e82e3f1f9aa5c9e778332c2099447427
Ricki Hirner <hirner@bitfire.at>
2023-10-07 11:53:23 +0200

    Beta feedback (gplay flavor): provide alternative to in-app reviews (#401)

--- app/src/gplay/kotlin/at/bitfire/davdroid/ui/GplayAccountsDrawerHandler.kt
+++ app/src/gplay/kotlin/at/bitfire/davdroid/ui/GplayAccountsDrawerHandler.kt
@@ -5,10 +5,17 @@
 package at.bitfire.davdroid.ui
 
 import android.app.Activity
+import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.net.Uri
 import android.view.MenuItem
+import android.widget.Toast
+import at.bitfire.davdroid.BuildConfig
 import at.bitfire.davdroid.R
+import at.bitfire.davdroid.log.Logger
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.play.core.review.ReviewManagerFactory
+import java.util.logging.Level
 import javax.inject.Inject
 
 class GplayAccountsDrawerHandler @Inject constructor() : StandardAccountsDrawerHandler() {
@@ -18,13 +25,23 @@ class GplayAccountsDrawerHandler @Inject constructor() : StandardAccountsDrawerH
             R.id.nav_beta_feedback -> {
                 // use In-App Review API to submit private feedback
                 val manager = ReviewManagerFactory.create(activity)
-                manager.requestReviewFlow()
-                    .addOnSuccessListener { reviewInfo ->
-                        manager.launchReviewFlow(activity, reviewInfo)
-                    }.addOnFailureListener {
-                        // fall back to email in case of failure
-                        super.onNavigationItemSelected(activity, item)
+                val request = manager.requestReviewFlow()
+                request.addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Logger.log.info("Launching in-app review flow")
+                        manager.launchReviewFlow(activity, task.result)
+
+                        // provide alternative if in-app review flow didn't show up
+                        Snackbar.make(activity.requireViewById(android.R.id.content), R.string.nav_feedback_inapp_didnt_appear, Snackbar.LENGTH_SHORT)
+                            .setAction(R.string.nav_feedback_google_play) {
+                                fallbackToStore(activity, item)
+                            }
+                            .show()
+                    } else {
+                        Logger.log.log(Level.WARNING, "Couldn't start in-app review flow", task.exception)
+                        fallbackToStore(activity, item)
                     }
+                }
             }
 
             R.id.nav_donate ->
@@ -33,6 +50,20 @@ class GplayAccountsDrawerHandler @Inject constructor() : StandardAccountsDrawerH
             else ->
                 // If we did not find a matching menu item, try the base
                 super.onNavigationItemSelected(activity, item)
+        }
+    }
+
+    private fun fallbackToStore(activity: Activity, item: MenuItem) {
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            data = Uri.parse("https://play.google.com/store/apps/details?id=${BuildConfig.APPLICATION_ID}")
+            setPackage("com.android.vending")   // Google Play only (this is only for the gplay flavor)
+        }
+        try {
+            activity.startActivity(intent)
+            Toast.makeText(activity, R.string.nav_feedback_scroll_to_reviews, Toast.LENGTH_LONG).show()
+        } catch (e: ActivityNotFoundException) {
+            // fall back to email
+            super.onNavigationItemSelected(activity, item)
         }
     }
 
