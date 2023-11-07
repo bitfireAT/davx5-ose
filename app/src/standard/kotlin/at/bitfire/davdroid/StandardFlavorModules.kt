2d6bc27482485139bb0061300a3e7cfbe81815b1
Ricki Hirner <hirner@bitfire.at>
2023-11-07 15:00:23 +0100

    Clean up hilt modules

--- app/src/standard/kotlin/at/bitfire/davdroid/StandardFlavorModules.kt
+++ app/src/standard/kotlin/at/bitfire/davdroid/StandardFlavorModules.kt
@@ -4,24 +4,22 @@
 
 package at.bitfire.davdroid
 
-import at.bitfire.davdroid.ui.AboutActivity
 import at.bitfire.davdroid.ui.AccountsDrawerHandler
 import at.bitfire.davdroid.ui.StandardAccountsDrawerHandler
-import at.bitfire.davdroid.ui.intro.IntroFragmentFactory
-import at.bitfire.davdroid.ui.intro.PermissionsIntroFragment
-import at.bitfire.davdroid.ui.intro.TasksIntroFragment
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
-import dagger.hilt.components.SingletonComponent
-import dagger.multibindings.IntoSet
 
-@Module
-@InstallIn(SingletonComponent::class)
-interface StandardFlavorModule {
+interface StandardFlavorModules {
 
-    @Binds
-    fun accountsDrawerHandler(handler: StandardAccountsDrawerHandler): AccountsDrawerHandler
+    @Module
+    @InstallIn(ActivityComponent::class)
+    interface ForActivities {
+
+        @Binds
+        fun accountsDrawerHandler(handler: StandardAccountsDrawerHandler): AccountsDrawerHandler
+
+    }
 
 }
