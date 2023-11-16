c05f20708a4fd770be15264e204baceacaf98dea
Arnau Mora <arnyminerz@proton.me>
2023-11-16 09:39:23 +0100

    Added `CardWithImage`
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

---
+++ app/src/main/kotlin/at/bitfire/davdroid/ui/widget/CardWithImage.kt
@@ -0,0 +1,74 @@
+package at.bitfire.davdroid.ui.widget
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import at.bitfire.davdroid.R
+
+@Composable
+fun CardWithImage(
+    image: Painter,
+    title: String,
+    message: String,
+    modifier: Modifier = Modifier,
+    contentDescription: String? = null,
+    content: @Composable ColumnScope.() -> Unit
+) {
+    Card(modifier) {
+        Column(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Image(
+                painter = image,
+                contentDescription = contentDescription,
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            ) {
+                Text(
+                    text = title,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 12.dp),
+                    style = MaterialTheme.typography.h6
+                )
+                Text(
+                    text = message,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 12.dp),
+                    style = MaterialTheme.typography.body1
+                )
+
+                content()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CardWithImagePreview() {
+    CardWithImage(
+        image = painterResource(R.drawable.intro_tasks),
+        title = "Demo card",
+        message = "This is the message to be displayed under the title, but before the content."
+    ) {
+
+    }
+}
