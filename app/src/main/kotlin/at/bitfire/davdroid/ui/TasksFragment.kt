28fb37783b7b019afef0e00c34445356764827c9
Arnau Mora <arnyminerz@proton.me>
2023-11-17 08:08:05 +0100

    Removed preview since not usable
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- app/src/main/kotlin/at/bitfire/davdroid/ui/TasksFragment.kt
+++ app/src/main/kotlin/at/bitfire/davdroid/ui/TasksFragment.kt
@@ -42,7 +42,6 @@ import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.ExperimentalTextApi
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -320,10 +319,4 @@ fun TasksCard(
             )
         }
     }
-}
-
-@Preview
-@Composable
-fun TasksCard_Preview() {
-    TasksCard()
 }
