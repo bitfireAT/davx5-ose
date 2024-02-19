0889dc52974eb27f657e423206758a55652ee03d
Ricki Hirner <hirner@bitfire.at>
2024-02-19 19:23:46 +0100

    Use SafeAndroidUriHandler instead of UiUtils.launchUri

--- app/src/main/kotlin/at/bitfire/davdroid/ui/intro/BatteryOptimizationsFragment.kt
+++ app/src/main/kotlin/at/bitfire/davdroid/ui/intro/BatteryOptimizationsFragment.kt
@@ -34,8 +34,8 @@ import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
@@ -44,6 +44,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -58,9 +60,9 @@ import at.bitfire.davdroid.App
 import at.bitfire.davdroid.BuildConfig
 import at.bitfire.davdroid.R
 import at.bitfire.davdroid.settings.SettingsManager
-import at.bitfire.davdroid.ui.UiUtils
 import at.bitfire.davdroid.ui.intro.BatteryOptimizationsFragment.Model.Companion.HINT_AUTOSTART_PERMISSION
 import at.bitfire.davdroid.ui.intro.BatteryOptimizationsFragment.Model.Companion.HINT_BATTERY_OPTIMIZATIONS
+import at.bitfire.davdroid.ui.widget.SafeAndroidUriHandler
 import com.google.accompanist.themeadapter.material.MdcTheme
 import dagger.Binds
 import dagger.Module
@@ -99,22 +101,25 @@ class BatteryOptimizationsFragment: Fragment() {
                             ignoreBatteryOptimizationsResultLauncher.launch(BuildConfig.APPLICATION_ID)
                     }
 
-                    Content(
-                        dontShowBattery = dontShowBattery,
-                        onChangeDontShowBattery = {
-                            model.dontShowBattery.set(it)
-                            dontShowBattery = it
-                        },
-                        isWhitelisted = isWhitelisted,
-                        shouldBeWhitelisted = shouldBeWhitelisted,
-                        onChangeShouldBeWhitelisted = model.shouldBeWhitelisted::postValue,
-                        dontShowAutostart = dontShowAutostart,
-                        onChangeDontShowAutostart = {
-                            model.dontShowAutostart.set(it)
-                            dontShowAutostart = it
-                        },
-                        manufacturerWarning = Model.manufacturerWarning
-                    )
+                    val uriHandler = SafeAndroidUriHandler(LocalContext.current)
+                    CompositionLocalProvider(LocalUriHandler provides uriHandler) {
+                        Content(
+                            dontShowBattery = dontShowBattery,
+                            onChangeDontShowBattery = {
+                                model.dontShowBattery.set(it)
+                                dontShowBattery = it
+                            },
+                            isWhitelisted = isWhitelisted,
+                            shouldBeWhitelisted = shouldBeWhitelisted,
+                            onChangeShouldBeWhitelisted = model.shouldBeWhitelisted::postValue,
+                            dontShowAutostart = dontShowAutostart,
+                            onChangeDontShowAutostart = {
+                                model.dontShowAutostart.set(it)
+                                dontShowAutostart = it
+                            },
+                            manufacturerWarning = Model.manufacturerWarning
+                        )
+                    }
                 }
             }
         }
@@ -154,6 +159,8 @@ class BatteryOptimizationsFragment: Fragment() {
         onChangeDontShowAutostart: (Boolean) -> Unit,
         manufacturerWarning: Boolean
     ) {
+        val uriHandler = LocalUriHandler.current
+
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -237,8 +244,7 @@ class BatteryOptimizationsFragment: Fragment() {
                         )
                         OutlinedButton(
                             onClick = {
-                                UiUtils.launchUri(
-                                    requireActivity(),
+                                uriHandler.openUri(
                                     App.homepageUrl(requireActivity())
                                         .buildUpon()
                                         .appendPath("faq")
@@ -247,7 +253,7 @@ class BatteryOptimizationsFragment: Fragment() {
                                             "manufacturer",
                                             Build.MANUFACTURER.lowercase(Locale.ROOT)
                                         )
-                                        .build()
+                                        .build().toString()
                                 )
                             }
                         ) {
