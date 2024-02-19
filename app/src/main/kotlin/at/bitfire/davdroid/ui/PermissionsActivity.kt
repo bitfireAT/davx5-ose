066e772808a26a971625cad9cc05160c8b8418c3
Arnau Mora <arnyminerz@proton.me>
2024-02-19 15:03:28 +0100

    Migrated `PermissionsActivity` to Compose
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- app/src/main/kotlin/at/bitfire/davdroid/ui/PermissionsActivity.kt
+++ app/src/main/kotlin/at/bitfire/davdroid/ui/PermissionsActivity.kt
@@ -5,17 +5,29 @@
 package at.bitfire.davdroid.ui
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.viewModels
+import com.google.accompanist.themeadapter.material.MdcTheme
 
 class PermissionsActivity: AppCompatActivity() {
+
+    val model by viewModels<PermissionsFragment.Model>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if (savedInstanceState == null)
-            supportFragmentManager.beginTransaction()
-                    .add(android.R.id.content, PermissionsFragment())
-                    .commit()
+        setContent {
+            MdcTheme {
+                PermissionsFragmentContent()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        model.checkPermissions()
     }
 
 }
