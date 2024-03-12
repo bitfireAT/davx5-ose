7d64630a324eb9cb7b92172bf62fd20bf34d0d1b
Arnau Mora <arnyminerz@proton.me>
2024-03-12 15:59:20 +0100

    Fixed activity name
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- app/src/main/kotlin/at/bitfire/davdroid/ui/account/CreateCalendarActivity.kt
+++ app/src/main/kotlin/at/bitfire/davdroid/ui/account/CreateCalendarActivity.kt
@@ -259,8 +259,8 @@ class CreateCalendarActivity: AppCompatActivity() {
         onCreateCollectionRequested: () -> Unit
     ) {
         fun onBackRequested() {
-            val intent = Intent(this, AccountActivity2::class.java)
-            intent.putExtra(AccountActivity2.EXTRA_ACCOUNT, model.account)
+            val intent = Intent(this, AccountActivity::class.java)
+            intent.putExtra(AccountActivity.EXTRA_ACCOUNT, model.account)
             NavUtils.navigateUpTo(this, intent)
         }
 
