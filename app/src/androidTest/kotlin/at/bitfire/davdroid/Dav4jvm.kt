8c12300be3a9e40233d83156aa69b849e69c9f34
Ricki Hirner <hirner@bitfire.at>
2024-04-17 13:23:47 +0200

    Update dav4jvm to support relaxed parsing (bitfireAT/davx5#563)
    * Update dav4jvm
    
    * Add test

---
+++ app/src/androidTest/kotlin/at/bitfire/davdroid/Dav4jvm.kt
@@ -0,0 +1,20 @@
+/***************************************************************************************************
+ * Copyright © All Contributors. See LICENSE and AUTHORS in the root directory for details.
+ **************************************************************************************************/
+
+package at.bitfire.davdroid
+
+import android.util.Xml
+import at.bitfire.dav4jvm.XmlUtils
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class Dav4jvm {
+
+    @Test
+    fun test_Dav4jvm_XmlUtils_NewPullParser_RelaxedParsing() {
+        val parser = XmlUtils.newPullParser()
+        assertTrue(parser.getFeature(Xml.FEATURE_RELAXED))
+    }
+
+}
