c33ea84c777c5991f23b4e4f94d7a3ecc40def63
Ricki Hirner <hirner@bitfire.at>
2024-04-26 11:20:55 +0200

    Rewrite AccountsActivity to M3 (#749)
    * [WIP] Rewrite AccountsActivity to M3
    
    * [WIP] AccountsScreen: FAB, previews
    
    * [WIP] Warning cards
    
    * Adapt FABs

--- app/src/main/kotlin/at/bitfire/davdroid/ui/AccountsDrawerHandler.kt
+++ app/src/main/kotlin/at/bitfire/davdroid/ui/AccountsDrawerHandler.kt
@@ -9,34 +9,32 @@ import android.content.Context
 import android.content.Intent
 import androidx.annotation.StringRes
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Divider
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.SnackbarHostState
-import androidx.compose.material.SnackbarResult
-import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Feedback
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.Storage
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
@@ -47,6 +45,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
 import at.bitfire.davdroid.BuildConfig
@@ -77,7 +76,7 @@ abstract class AccountsDrawerHandler {
         snackbarHostState: SnackbarHostState,
         onCloseDrawer: () -> Unit
     ) {
-        Column(Modifier
+        Column(modifier = Modifier
             .fillMaxWidth()
             .verticalScroll(rememberScrollState())
         ) {
@@ -181,15 +180,14 @@ abstract class AccountsDrawerHandler {
     fun Header() {
         Column(
             Modifier
-            .background(Color.DarkGray)
-            .fillMaxWidth()
-            .padding(16.dp)
+                .background(Color.DarkGray)
+                .fillMaxWidth()
+                .padding(16.dp)
         ) {
             Spacer(Modifier.height(16.dp))
             Box(
-                Modifier
-                .background(
-                    color = MaterialTheme.colors.primary,
+                Modifier.background(
+                    color = MaterialTheme.colorScheme.primary,
                     shape = RoundedCornerShape(16.dp)
                 )
             ) {
@@ -199,8 +197,7 @@ abstract class AccountsDrawerHandler {
                     tint = Color.White,
                     modifier = Modifier
                         .scale(1.2f)
-                        .height(56.dp)
-                        .width(56.dp)
+                        .size(64.dp)
                 )
             }
             Spacer(Modifier.height(8.dp))
@@ -208,12 +205,12 @@ abstract class AccountsDrawerHandler {
             Text(
                 stringResource(R.string.app_name),
                 color = Color.White,
-                style = MaterialTheme.typography.body1
+                style = MaterialTheme.typography.bodyLarge
             )
             Text(
                 stringResource(R.string.navigation_drawer_subtitle),
                 color = Color.White.copy(alpha = 0.7f),
-                style = MaterialTheme.typography.body2
+                style = MaterialTheme.typography.bodyMedium
             )
         }
 
@@ -222,11 +219,11 @@ abstract class AccountsDrawerHandler {
 
     @Composable
     fun MenuHeading(text: String) {
-        Divider(Modifier.padding(vertical = 8.dp))
+        HorizontalDivider(Modifier.padding(vertical = 8.dp))
 
         Text(
             text,
-            style = MaterialTheme.typography.body2,
+            style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
             modifier = Modifier.padding(8.dp)
         )
     }
@@ -241,28 +238,15 @@ abstract class AccountsDrawerHandler {
         onClick: () -> Unit
     ) {
         val closeHandler = localCloseDrawerHandler.current
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-                .clickable(onClick = {
-                    onClick()
-                    closeHandler.closeDrawer()
-                })
-                .padding(4.dp)
-        ) {
-            Icon(
-                icon,
-                contentDescription = title,
-                modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
-            )
-            Text(
-                title,
-                style = MaterialTheme.typography.body2,
-                modifier = Modifier
-                    .padding(start = 8.dp)
-                    .weight(1f)
-            )
-        }
+        NavigationDrawerItem(
+            icon = { Icon(icon, contentDescription = title) },
+            label = { Text(title) },
+            selected = false,
+            onClick = {
+                onClick()
+                closeHandler.closeDrawer()
+            }
+        )
     }
 
     @Composable
---
+++ app/src/main/kotlin/at/bitfire/davdroid/ui/AccountsModel.kt
@@ -0,0 +1,187 @@
+package at.bitfire.davdroid.ui
+
+import android.accounts.Account
+import android.app.Application
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.ShortcutManager
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import android.os.Build
+import android.os.PowerManager
+import androidx.core.content.getSystemService
+import androidx.lifecycle.ViewModel
+import androidx.work.WorkInfo
+import androidx.work.WorkManager
+import androidx.work.WorkQuery
+import at.bitfire.davdroid.db.AppDatabase
+import at.bitfire.davdroid.servicedetection.RefreshCollectionsWorker
+import at.bitfire.davdroid.syncadapter.AccountRepository
+import at.bitfire.davdroid.syncadapter.BaseSyncWorker
+import at.bitfire.davdroid.syncadapter.OneTimeSyncWorker
+import at.bitfire.davdroid.syncadapter.SyncUtils
+import at.bitfire.davdroid.util.broadcastReceiverFlow
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.map
+import java.text.Collator
+import javax.inject.Inject
+
+@HiltViewModel
+class AccountsModel @Inject constructor(
+    val context: Application,
+    val accountRepository: AccountRepository,
+    private val db: AppDatabase
+): ViewModel() {
+
+    // UI state
+
+    /** Tri-state enum to represent active / pending / idle status */
+    enum class Progress {
+        Active,     // syncing or refreshing
+        Pending,    // sync pending
+        Idle
+    }
+
+    enum class FABStyle {
+        WithText,
+        Standard,
+        None
+    }
+
+    data class AccountInfo(
+        val name: Account,
+        val progress: Progress
+    )
+
+    private val accounts = accountRepository.getAllFlow()
+    val showAddAccount: Flow<FABStyle> = accounts.map {
+        if (it.isEmpty())
+            FABStyle.WithText
+        else
+            FABStyle.Standard
+    }
+    val showSyncAll: Flow<Boolean> = accounts.map { it.isNotEmpty() }
+
+    private val workManager = WorkManager.getInstance(context)
+    private val runningWorkers = workManager.getWorkInfosFlow(WorkQuery.fromStates(WorkInfo.State.ENQUEUED, WorkInfo.State.RUNNING))
+
+    val accountInfos: Flow<List<AccountInfo>> = combine(accounts, runningWorkers) { accounts, workInfos ->
+        val authorities = SyncUtils.syncAuthorities(context)
+        val collator = Collator.getInstance()
+
+        accounts
+            .sortedWith { a, b -> collator.compare(a.name, b.name) }
+            .map { account ->
+                val services = db.serviceDao().getIdsByAccountAsync(account.name)
+                val progress = when {
+                    workInfos.any { info ->
+                        info.state == WorkInfo.State.RUNNING && (
+                                services.any { serviceId ->
+                                    info.tags.contains(RefreshCollectionsWorker.workerName(serviceId))
+                                } || authorities.any { authority ->
+                                    info.tags.contains(BaseSyncWorker.commonTag(account, authority))
+                                }
+                            )
+                    } -> Progress.Active
+
+                    workInfos.any { info ->
+                        info.state == WorkInfo.State.ENQUEUED && authorities.any { authority ->
+                            info.tags.contains(OneTimeSyncWorker.workerName(account, authority))
+                        }
+                    } -> Progress.Pending
+
+                    else -> Progress.Idle
+                }
+
+                AccountInfo(account, progress)
+            }
+    }
+
+
+    // warnings
+
+    private val connectivityManager = context.getSystemService<ConnectivityManager>()!!
+    private val powerManager: PowerManager = context.getSystemService<PowerManager>()!!
+
+    /** whether a usable network connection is available (sync framework won't run synchronization otherwise) */
+    val networkAvailable = callbackFlow<Boolean> {
+        val networkCallback = object: ConnectivityManager.NetworkCallback() {
+            val availableNetworks = hashSetOf<Network>()
+
+            override fun onAvailable(network: Network) {
+                availableNetworks += network
+                update()
+            }
+
+            override fun onLost(network: Network) {
+                availableNetworks -= network
+                update()
+            }
+
+            private fun update() {
+                trySend(availableNetworks.isNotEmpty())
+            }
+        }
+
+        val networkRequest = NetworkRequest.Builder()
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+            .build()
+        connectivityManager.registerNetworkCallback(networkRequest, networkCallback)
+
+        awaitClose {
+            connectivityManager.unregisterNetworkCallback(networkCallback)
+        }
+    }
+
+    /** whether battery saver is active */
+    val batterySaverActive =
+        broadcastReceiverFlow(
+            context = context,
+            filter = IntentFilter(PowerManager.ACTION_POWER_SAVE_MODE_CHANGED),
+            immediate = true
+        ).map { powerManager.isPowerSaveMode }
+
+    /** whether data saver is restricting background synchronization ([ConnectivityManager.RESTRICT_BACKGROUND_STATUS_ENABLED]) */
+    val dataSaverEnabled =
+        broadcastReceiverFlow(
+            context = context,
+            filter = IntentFilter(ConnectivityManager.ACTION_RESTRICT_BACKGROUND_CHANGED),
+            immediate = true
+        ).map { connectivityManager.restrictBackgroundStatus == ConnectivityManager.RESTRICT_BACKGROUND_STATUS_ENABLED }
+
+    /** whether storage is low (prevents sync framework from running synchronization) */
+    val storageLow =
+        broadcastReceiverFlow(
+            context = context,
+            filter = IntentFilter().apply {
+                addAction(Intent.ACTION_DEVICE_STORAGE_LOW)
+                addAction(Intent.ACTION_DEVICE_STORAGE_OK)
+            },
+            immediate = false     // "storage low" intent is sticky
+        ).map { intent ->
+            when (intent.action) {
+                Intent.ACTION_DEVICE_STORAGE_LOW -> true
+                else -> false
+            }
+        }
+
+    
+    // actions
+
+    fun syncAllAccounts() {
+        if (Build.VERSION.SDK_INT >= 25)
+            context.getSystemService<ShortcutManager>()?.reportShortcutUsed(UiUtils.SHORTCUT_SYNC_ALL)
+
+        // Enqueue sync worker for all accounts and authorities. Will sync once internet is available
+        for (account in accountRepository.getAll())
+            OneTimeSyncWorker.enqueueAllAuthorities(context, account, manual = true)
+    }
+
+}
