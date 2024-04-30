20e88c65d9a9b9d16f0b073f8617e2ed21674ed8
Arnau Mora <arnyminerz@proton.me>
2024-04-30 12:58:50 +0200

    Added `ReplaceWith` to `lastSegmentOfUrl`
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- app/src/main/kotlin/at/bitfire/davdroid/util/DavUtils.kt
+++ app/src/main/kotlin/at/bitfire/davdroid/util/DavUtils.kt
@@ -60,7 +60,13 @@ object DavUtils {
         return String.format(Locale.ROOT, "#%06X%02X", color, alpha)
     }
 
-    @Deprecated("Use HttpUrl.lastSegment in UrlUtils")
+    @Deprecated(
+        message = "Use HttpUrl.lastSegment in UrlUtils",
+        replaceWith = ReplaceWith(
+            "url.lastSegment()",
+            "at.bitfire.davdroid.util.lastSegment"
+        )
+    )
     fun lastSegmentOfUrl(url: HttpUrl): String {
         // the list returned by HttpUrl.pathSegments() is unmodifiable, so we have to create a copy
         val segments = LinkedList(url.pathSegments)
