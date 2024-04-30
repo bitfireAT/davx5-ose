7cd01294ad17c6e11a4ab48a4f3887676db8e80f
Arnau Mora <arnyminerz@proton.me>
2024-04-30 15:32:57 +0200

    Added tests for UrlUtils
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

---
+++ app/src/test/kotlin/at/bitfire/davdroid/UrlUtilsTest.kt
@@ -0,0 +1,18 @@
+package at.bitfire.davdroid
+
+import at.bitfire.davdroid.util.lastSegment
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import org.junit.Assert
+import org.junit.Test
+
+class UrlUtilsTest {
+    private val exampleURL = "http://example.com/"
+
+    @Test
+    fun testLastSegmentOfUrl() {
+        Assert.assertEquals("/", exampleURL.toHttpUrl().lastSegment())
+        Assert.assertEquals("dir", (exampleURL + "dir").toHttpUrl().lastSegment())
+        Assert.assertEquals("dir", (exampleURL + "dir/").toHttpUrl().lastSegment())
+        Assert.assertEquals("file.html", (exampleURL + "dir/file.html").toHttpUrl().lastSegment())
+    }
+}
