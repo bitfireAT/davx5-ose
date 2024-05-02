9c6c95a2495d9c8092f4d0640a7da5c022e04858
Sunik Kupfer <kupfer@bitfire.at>
2024-05-02 12:43:08 +0200

    Start at LoginDetails page if logging in via intent (#772)
    * Start at LoginDetails page if login via intent has data
    
    * Ignore intent on re-creation

--- app/src/main/kotlin/at/bitfire/davdroid/ui/setup/LoginActivity.kt
+++ app/src/main/kotlin/at/bitfire/davdroid/ui/setup/LoginActivity.kt
@@ -109,7 +109,13 @@ class LoginActivity @Inject constructor(): AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         // start with login info from Intent
-        model.updateLoginInfo(loginInfoFromIntent(intent))
+        if (savedInstanceState == null) {
+            val loginInfo = loginInfoFromIntent(intent)
+            if (loginInfo.baseUri != null) {
+                model.updateLoginInfo(loginInfo)
+                model.navToPage(LoginScreenModel.Page.LoginDetails)
+            }
+        }
 
         setContent {
             LoginScreen(
