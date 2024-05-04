1ad8e892b626d7747dba7447d15e6f5827d176f8
Ricki Hirner <hirner@bitfire.at>
2024-05-04 20:10:36 +0200

    Intro pages: use Hilt for dependency injection (#779)
    * Accounts: move syncAll to model; better Hilt usage
    
    * Move calculation of whether IntroActivity is shown into AccountsModel
    
    * Intro pages: inject dependencies with Hilt
    
    * Fix preview

--- app/src/main/kotlin/at/bitfire/davdroid/ui/intro/PermissionsIntroPage.kt
+++ app/src/main/kotlin/at/bitfire/davdroid/ui/intro/PermissionsIntroPage.kt
@@ -7,18 +7,21 @@ package at.bitfire.davdroid.ui.intro
 import android.app.Application
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
-import at.bitfire.davdroid.ui.PermissionsScreen
 import at.bitfire.davdroid.ui.PermissionsModel
+import at.bitfire.davdroid.ui.PermissionsScreen
 import at.bitfire.davdroid.util.PermissionUtils
 import at.bitfire.davdroid.util.PermissionUtils.CALENDAR_PERMISSIONS
 import at.bitfire.davdroid.util.PermissionUtils.CONTACT_PERMISSIONS
 import at.bitfire.ical4android.TaskProvider
+import javax.inject.Inject
 
-class PermissionsIntroPage: IntroPage {
+class PermissionsIntroPage @Inject constructor(
+    private val application: Application
+): IntroPage {
 
     var model: PermissionsModel? = null
 
-    override fun getShowPolicy(application: Application): IntroPage.ShowPolicy {
+    override fun getShowPolicy(): IntroPage.ShowPolicy {
         // show PermissionsFragment as intro fragment when no permissions are granted
         val permissions = CONTACT_PERMISSIONS + CALENDAR_PERMISSIONS +
                 TaskProvider.PERMISSIONS_JTX +
@@ -32,15 +35,7 @@ class PermissionsIntroPage: IntroPage {
 
     @Composable
     override fun ComposePage() {
-        val newModel: PermissionsModel = viewModel()
-        model = newModel
-
-        PermissionsScreen(model = newModel)
-    }
-
-    // Check whether permissions have changed after user comes back from settings app
-    override fun onResume(application: Application) {
-        model?.checkPermissions()
+        PermissionsScreen()
     }
 
 }
