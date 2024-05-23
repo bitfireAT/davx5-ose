5deffd2efe8604f6fa31811db2824aca7c49b62b
Sunik Kupfer <kupfer@bitfire.at>
2024-05-23 09:48:58 +0200

    Remove comment

--- app/src/main/kotlin/at/bitfire/davdroid/ui/AccountsScreen.kt
+++ app/src/main/kotlin/at/bitfire/davdroid/ui/AccountsScreen.kt
@@ -29,7 +29,6 @@ import androidx.compose.material.icons.filled.Storage
 import androidx.compose.material.icons.filled.Sync
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.DrawerDefaults
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
@@ -57,7 +56,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalInspectionMode
@@ -170,8 +168,7 @@ fun AccountsScreen(
                         }
                     )
                 }
-            },
-//            scrimColor = MaterialTheme.colorScheme.secondary
+            }
         ) {
             Scaffold(
                 topBar = {
