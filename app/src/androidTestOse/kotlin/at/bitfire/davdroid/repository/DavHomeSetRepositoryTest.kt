9c6f2d47aead693cb3f02630925ea86dbafbc1c6
Ricki Hirner <hirner@bitfire.at>
2024-06-10 09:46:08 +0200

    Extract RefreshCollectionsWorker; move some HomeSetDao calls to DavHomeSetRepository

---
+++ app/src/androidTestOse/kotlin/at/bitfire/davdroid/repository/DavHomeSetRepositoryTest.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright © All Contributors. See LICENSE and AUTHORS in the root directory for details.
+ */
+
+package at.bitfire.davdroid.repository
+
+import at.bitfire.davdroid.db.HomeSet
+import at.bitfire.davdroid.db.Service
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import javax.inject.Inject
+
+@HiltAndroidTest
+class DavHomeSetRepositoryTest {
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var repository: DavHomeSetRepository
+
+    @Inject
+    lateinit var serviceRepository: DavServiceRepository
+
+    @Before
+    fun setUp() {
+        hiltRule.inject()
+    }
+
+
+    @Test
+    fun testInsertOrUpdate() {
+        // should insert new row or update (upsert) existing row - without changing its key!
+        val serviceId = createTestService()
+
+        val entry1 = HomeSet(id=0, serviceId=serviceId, personal=true, url="https://example.com/1".toHttpUrl())
+        val insertId1 = repository.insertOrUpdateByUrl(entry1)
+        Assert.assertEquals(1L, insertId1)
+        Assert.assertEquals(entry1.apply { id = 1L }, repository.getById(1L))
+
+        val updatedEntry1 = HomeSet(id=0, serviceId=serviceId, personal=true, url="https://example.com/1".toHttpUrl(), displayName="Updated Entry")
+        val updateId1 = repository.insertOrUpdateByUrl(updatedEntry1)
+        Assert.assertEquals(1L, updateId1)
+        Assert.assertEquals(updatedEntry1.apply { id = 1L }, repository.getById(1L))
+
+        val entry2 = HomeSet(id=0, serviceId=serviceId, personal=true, url= "https://example.com/2".toHttpUrl())
+        val insertId2 = repository.insertOrUpdateByUrl(entry2)
+        Assert.assertEquals(2L, insertId2)
+        Assert.assertEquals(entry2.apply { id = 2L }, repository.getById(2L))
+    }
+
+    @Test
+    fun testDelete() {
+        // should delete row with given primary key (id)
+        val serviceId = createTestService()
+
+        val entry1 = HomeSet(id=1, serviceId=serviceId, personal=true, url= "https://example.com/1".toHttpUrl())
+
+        val insertId1 = repository.insertOrUpdateByUrl(entry1)
+        Assert.assertEquals(1L, insertId1)
+        Assert.assertEquals(entry1, repository.getById(1L))
+
+        repository.delete(entry1)
+        Assert.assertEquals(null, repository.getById(1L))
+    }
+
+
+    private fun createTestService() : Long {
+        val service = Service(id=0, accountName="test", type= Service.TYPE_CALDAV, principal = null)
+        return serviceRepository.insertOrReplace(service)
+    }
+
+}
--- app/src/main/kotlin/at/bitfire/davdroid/repository/DavHomeSetRepository.kt
+++ app/src/main/kotlin/at/bitfire/davdroid/repository/DavHomeSetRepository.kt
@@ -5,7 +5,9 @@
 package at.bitfire.davdroid.repository
 
 import android.accounts.Account
+import androidx.room.Transaction
 import at.bitfire.davdroid.db.AppDatabase
+import at.bitfire.davdroid.db.HomeSet
 import at.bitfire.davdroid.db.Service
 import javax.inject.Inject
 
@@ -18,7 +20,27 @@ class DavHomeSetRepository @Inject constructor(
     fun getAddressBookHomeSetsFlow(account: Account) =
         dao.getBindableByAccountAndServiceTypeFlow(account.name, Service.TYPE_CARDDAV)
 
+    fun getById(id: Long) = dao.getById(id)
+
     fun getCalendarHomeSetsFlow(account: Account) =
         dao.getBindableByAccountAndServiceTypeFlow(account.name, Service.TYPE_CALDAV)
+
+
+    /**
+     * Tries to insert new row, but updates existing row if already present.
+     * This method preserves the primary key, as opposed to using "@Insert(onConflict = OnConflictStrategy.REPLACE)"
+     * which will create a new row with incremented ID and thus breaks entity relationships!
+     *
+     * @return ID of the row, that has been inserted or updated. -1 If the insert fails due to other reasons.
+     */
+    @Transaction
+    fun insertOrUpdateByUrl(homeset: HomeSet): Long =
+        dao.getByUrl(homeset.serviceId, homeset.url.toString())?.let { existingHomeset ->
+            dao.update(homeset.copy(id = existingHomeset.id))
+            existingHomeset.id
+        } ?: dao.insert(homeset)
+
+
+    fun delete(homeSet: HomeSet) = dao.delete(homeSet)
 
 }
