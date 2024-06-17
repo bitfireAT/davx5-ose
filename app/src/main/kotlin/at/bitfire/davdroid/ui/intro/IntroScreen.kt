0935bca1a28f1d3bdca72a4a25e5d07de0ecf382
Arnau Mora <arnyminer.z@gmail.com>
2024-06-17 13:20:12 +0200

    Added content description
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- app/src/main/kotlin/at/bitfire/davdroid/ui/intro/IntroScreen.kt
+++ app/src/main/kotlin/at/bitfire/davdroid/ui/intro/IntroScreen.kt
@@ -20,8 +20,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import at.bitfire.davdroid.R
 import at.bitfire.davdroid.ui.AppTheme
 import at.bitfire.davdroid.ui.composable.ButtonWithIcon
 import at.bitfire.davdroid.ui.composable.PositionIndicator
@@ -69,7 +71,7 @@ fun IntroScreen(
                 } else {
                     Icons.AutoMirrored.Default.ArrowForward
                 },
-                contentDescription = null,
+                contentDescription = stringResource(R.string.intro_next),
                 modifier = Modifier
                     .padding(end = 16.dp)
                     .align(Alignment.CenterEnd)
