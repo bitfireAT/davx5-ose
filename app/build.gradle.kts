60219aafc778c6bd370739bb66110f11866bd25b
Ricki Hirner <hirner@bitfire.at>
2024-07-11 23:59:54 +0200

    Version bump to 4.4.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
     defaultConfig {
         applicationId = "at.bitfire.davdroid"
 
-        versionCode = 404010005
-        versionName = "4.4.1"
+        versionCode = 404010101
+        versionName = "4.4.1.1"
 
         buildConfigField("long", "buildTime", "${System.currentTimeMillis()}L")
 
