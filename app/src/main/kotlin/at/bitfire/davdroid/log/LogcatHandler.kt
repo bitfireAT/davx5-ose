798952a97fc6ec0c2248c4ea5ac1289cc2b61e19
Ricki Hirner <hirner@bitfire.at>
2024-07-13 09:37:54 +0200

    LogcatHandler: log source class as tag and not in the text, remove max length

--- app/src/main/kotlin/at/bitfire/davdroid/log/LogcatHandler.kt
+++ app/src/main/kotlin/at/bitfire/davdroid/log/LogcatHandler.kt
@@ -4,9 +4,9 @@
 
 package at.bitfire.davdroid.log
 
+import android.os.Build
 import android.util.Log
-
-import org.apache.commons.lang3.math.NumberUtils
+import com.google.common.base.Ascii
 
 import java.util.logging.Handler
 import java.util.logging.Level
@@ -14,29 +14,26 @@ import java.util.logging.LogRecord
 
 object LogcatHandler: Handler() {
 
-    private const val MAX_LINE_LENGTH = 3000
-
     init {
         formatter = PlainTextFormatter.LOGCAT
-        level = Level.ALL
     }
 
     override fun publish(r: LogRecord) {
-        val text = formatter.format(r)
         val level = r.level.intValue()
+        val text = formatter.format(r)
 
-        val end = text.length
-        var pos = 0
-        while (pos < end) {
-            val line = text.substring(pos, NumberUtils.min(pos + MAX_LINE_LENGTH, end))
-            when {
-                level >= Level.SEVERE.intValue()  -> Log.e(r.loggerName, line)
-                level >= Level.WARNING.intValue() -> Log.w(r.loggerName, line)
-                level >= Level.CONFIG.intValue()  -> Log.i(r.loggerName, line)
-                level >= Level.FINER.intValue()   -> Log.d(r.loggerName, line)
-                else                              -> Log.v(r.loggerName, line)
-            }
-            pos += MAX_LINE_LENGTH
+        val className = PlainTextFormatter.shortClassName(r.sourceClassName)
+        val tag = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O)
+            Ascii.truncate(className, 23, "")
+        else
+            className
+
+        when {
+            level >= Level.SEVERE.intValue()  -> Log.e(tag, text, r.thrown)
+            level >= Level.WARNING.intValue() -> Log.w(tag, text, r.thrown)
+            level >= Level.CONFIG.intValue()  -> Log.i(tag, text, r.thrown)
+            level >= Level.FINER.intValue()   -> Log.d(tag, text, r.thrown)
+            else                              -> Log.v(tag, text, r.thrown)
         }
     }
 
--- app/src/main/kotlin/at/bitfire/davdroid/log/PlainTextFormatter.kt
+++ app/src/main/kotlin/at/bitfire/davdroid/log/PlainTextFormatter.kt
@@ -4,7 +4,6 @@
 
 package at.bitfire.davdroid.log
 
-import org.apache.commons.lang3.StringUtils
 import org.apache.commons.lang3.exception.ExceptionUtils
 import org.apache.commons.lang3.time.DateFormatUtils
 import java.util.Locale
@@ -12,47 +11,57 @@ import java.util.logging.Formatter
 import java.util.logging.LogRecord
 
 class PlainTextFormatter private constructor(
-        private val logcat: Boolean
+    private val forLogcat: Boolean
 ): Formatter() {
 
     companion object {
+
+        /**
+         * Formatter intended for logcat output.
+         */
         val LOGCAT = PlainTextFormatter(true)
+
+        /**
+         * Formatter intended for file output.
+         */
         val DEFAULT = PlainTextFormatter(false)
 
-        const val MAX_MESSAGE_LENGTH = 20000
+        fun shortClassName(className: String) = className
+            .replace(Regex("^at\\.bitfire\\.(dav|cert4an|dav4an|ical4an|vcard4an)droid\\."), ".")
+            .replace(Regex("\\$.*$"), "")
+
     }
+
 
     override fun format(r: LogRecord): String {
         val builder = StringBuilder()
 
-        if (!logcat)
+        if (!forLogcat) {
             builder .append(DateFormatUtils.format(r.millis, "yyyy-MM-dd HH:mm:ss", Locale.ROOT))
                     .append(" ").append(r.threadID).append(" ")
 
-        val className = shortClassName(r.sourceClassName)
-        if (className != r.loggerName)
-            builder.append("[").append(className).append("] ")
-
-        builder.append(StringUtils.abbreviate(r.message, MAX_MESSAGE_LENGTH))
-
-        r.thrown?.let {
-            builder .append("\nEXCEPTION ")
-                    .append(ExceptionUtils.getStackTrace(it))
+            val className = shortClassName(r.sourceClassName)
+            if (className != r.loggerName)
+                builder.append("[").append(className).append("] ")
         }
+
+        builder.append(r.message)
+
+        if (!forLogcat)
+            r.thrown?.let {
+                builder .append("\nEXCEPTION ")
+                        .append(ExceptionUtils.getStackTrace(it))
+            }
 
         r.parameters?.let {
             for ((idx, param) in it.withIndex())
                 builder.append("\n\tPARAMETER #").append(idx).append(" = ").append(param)
         }
 
-        if (!logcat)
+        if (!forLogcat)
             builder.append("\n")
 
         return builder.toString()
     }
-
-    private fun shortClassName(className: String) = className
-            .replace(Regex("^at\\.bitfire\\.(dav|cert4an|dav4an|ical4an|vcard4an)droid\\."), "")
-            .replace(Regex("\\$.*$"), "")
 
 }
