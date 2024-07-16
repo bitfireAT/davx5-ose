c92b34a3c0d27f2bc72ca202637b3c84a04d1f85
Ricki Hirner <hirner@bitfire.at>
2024-07-16 13:06:12 +0200

    KDoc

--- app/src/main/kotlin/at/bitfire/davdroid/startup/StartupPlugin.kt
+++ app/src/main/kotlin/at/bitfire/davdroid/startup/StartupPlugin.kt
@@ -7,8 +7,8 @@ package at.bitfire.davdroid.startup
 interface StartupPlugin {
 
     /**
-     * Runs synchronously during [App.onCreate]. Use only for tasks that must be completed before
-     * the app can be initialized.
+     * Runs synchronously during [at.bitfire.davdroid.App.onCreate]. Use only for tasks that must be completed before
+     * the app can run. Causes the app to start slower.
      *
      * Will be run before [onAppCreateAsync].
      */
@@ -21,11 +21,11 @@ interface StartupPlugin {
 
 
     /**
-     * Runs asynchronously after [App.onCreate]. Use for tasks that can be run in the background.
+     * Runs asynchronously after [at.bitfire.davdroid.App.onCreate]. Use for tasks that can be run in the background.
      *
      * Will be run after [onAppCreate].
      *
-     * The coroutine scope will usually be [GlobalScope] (which has no end because we don't get
+     * The coroutine scope will usually be `GlobalScope` (which has no end because we don't get
      * a signal before the app is terminated) on the default dispatcher, but can be a custom scope
      * for testing.
      */
