a790dd6c9a4baa1909d7a3d169003e881d6e124d
Ricki Hirner <hirner@bitfire.at>
2024-07-19 17:57:01 +0200

    SyncWorker: use Provider for lazy injection

--- app/src/androidTest/kotlin/at/bitfire/davdroid/sync/worker/TestSyncWorker.kt
+++ app/src/androidTest/kotlin/at/bitfire/davdroid/sync/worker/TestSyncWorker.kt
@@ -6,24 +6,15 @@ package at.bitfire.davdroid.sync.worker
 
 import android.content.Context
 import androidx.work.WorkerParameters
-import at.bitfire.davdroid.settings.AccountSettings
-import at.bitfire.davdroid.sync.SyncConditions
-import at.bitfire.davdroid.ui.NotificationRegistry
 import dagger.hilt.android.qualifiers.ApplicationContext
 import io.mockk.mockk
 import kotlinx.coroutines.Dispatchers
 import javax.inject.Inject
 
 class TestSyncWorker @Inject constructor(
-    @ApplicationContext context: Context,
-    accountSettingsFactory: AccountSettings.Factory,
-    notificationRegistry: NotificationRegistry,
-    syncConditionsFactory: SyncConditions.Factory
+    @ApplicationContext context: Context
 ): BaseSyncWorker(
     context,
     workerParams = mockk<WorkerParameters>(),
-    accountSettingsFactory,
-    notificationRegistry,
-    syncConditionsFactory,
     syncDispatcher = Dispatchers.Main
 )
