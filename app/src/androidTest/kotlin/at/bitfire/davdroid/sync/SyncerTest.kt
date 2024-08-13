e3afd4a75c22ecfe8791241e8ee26957d0ded141
Sunik Kupfer <kupfer@bitfire.at>
2024-08-13 15:36:52 +0200

    Write weak tests for the individual methods

--- app/src/androidTest/kotlin/at/bitfire/davdroid/sync/SyncerTest.kt
+++ app/src/androidTest/kotlin/at/bitfire/davdroid/sync/SyncerTest.kt
@@ -18,7 +18,10 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.spyk
 import io.mockk.verify
+import okhttp3.HttpUrl.Companion.toHttpUrl
 import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -35,11 +38,14 @@ class SyncerTest {
 
     lateinit var account: Account
 
+    lateinit var syncer: TestSyncer
+
     @Before
     fun setUp() {
         hiltRule.inject()
 
         account = TestAccountAuthenticator.create()
+        syncer = spyk(testSyncer.create(account, arrayOf(), SyncResult()))
     }
 
     @After
@@ -50,32 +56,102 @@ class SyncerTest {
 
     @Test
     fun testSync_prepare_fails() {
-        val syncer = spyk(testSyncer.create(account, arrayOf(), SyncResult()))
-        val provider = mockk<ContentProviderClient>(relaxed = true)
+        val provider = mockk<ContentProviderClient>()
         every { syncer.prepare(provider) } returns false
-        every { syncer.getSyncCollections(any()) } returns emptyList()
+        every { syncer.getSyncEnabledCollections() } returns emptyMap()
 
+        // Should stop the sync after prepare returns false
         syncer.sync(provider)
-
         verify(exactly = 1) { syncer.prepare(provider) }
-        verify(exactly = 0) { syncer.getSyncCollections(any()) }
+        verify(exactly = 0) { syncer.getSyncEnabledCollections() }
     }
 
     @Test
     fun testSync_prepare_succeeds() {
-        val syncer = spyk(testSyncer.create(account, arrayOf(), SyncResult()))
-        val provider = mockk<ContentProviderClient>(relaxed = true)
+        val provider = mockk<ContentProviderClient>()
         every { syncer.prepare(provider) } returns true
-        every { syncer.getSyncCollections(any()) } returns emptyList()
-        every { syncer.localSyncCollections(any()) } returns emptyList()
+        every { syncer.getSyncEnabledCollections() } returns emptyMap()
 
+        // Should continue the sync after prepare returns true
         syncer.sync(provider)
-
         verify(exactly = 1) { syncer.prepare(provider) }
-//        verify(exactly = 1) { syncer.getSyncCollections(any()) }
-        verify(exactly = 1) { syncer.localSyncCollections(any()) }
+        verify(exactly = 1) { syncer.getSyncEnabledCollections() }
     }
 
+    @Test
+    fun testUpdateCollections_deletesCollection() {
+        val providerCollection = mockk<LocalTestCollection>()
+        every { providerCollection.collectionUrl } returns "http://delete.the/collection"
+        every { providerCollection.deleteCollection() } returns true
+
+        // Should delete the providerCollection (local) if dbCollection (remote) does not exist
+        syncer.updateCollections(listOf(providerCollection), dbCollections = emptyMap())
+        verify(exactly = 1) { providerCollection.deleteCollection() }
+    }
+
+    @Test
+    fun testUpdateCollections_updatesCollection() {
+        val localCollection = mockk<LocalTestCollection>()
+        val dbCollection = mockk<Collection>()
+        val dbCollections = mapOf("http://update.the/collection".toHttpUrl() to dbCollection)
+        every { dbCollection.url } returns "http://update.the/collection".toHttpUrl()
+        every { localCollection.collectionUrl } returns "http://update.the/collection"
+
+        // Should update the providerCollection (local) if it exists ...
+        val newCollections = syncer.updateCollections(listOf(localCollection), dbCollections)
+        verify(exactly = 1) { syncer.update(localCollection, dbCollection) }
+        // ... and remove it from the "new found" collections which are to be created
+        assertTrue(newCollections.isEmpty())
+    }
+
+    @Test
+    fun testUpdateCollections_findsNewCollection() {
+        val dbCollection = mockk<Collection>()
+        val dbCollections = mapOf("http://newly.found/collection".toHttpUrl() to dbCollection)
+        every { dbCollection.url } returns "http://newly.found/collection".toHttpUrl()
+
+        // Should return the new collection, because it was not updated
+        val newCollections = syncer.updateCollections(listOf(), dbCollections)
+        assertEquals(dbCollection, newCollections["http://newly.found/collection".toHttpUrl()])
+    }
+
+    @Test
+    fun testCreateLocalCollections() {
+        val provider = mockk<ContentProviderClient>()
+        val localCollection = mockk<LocalTestCollection>()
+        val dbCollection = mockk<Collection>()
+        val dbCollections = mapOf("http://newly.found/collection".toHttpUrl() to dbCollection)
+        every { syncer.create(provider, dbCollection) } returns localCollection
+        every { dbCollection.url } returns "http://newly.found/collection".toHttpUrl()
+
+        // Should return list of newly created local collections
+        val newLocalCollections = syncer.createLocalCollections(provider, dbCollections)
+        assertEquals(listOf(localCollection), newLocalCollections)
+    }
+    
+    @Test
+    fun testSyncCollectionContents() {
+        val provider = mockk<ContentProviderClient>()
+        val dbCollection1 = mockk<Collection>()
+        val dbCollection2 = mockk<Collection>()
+        val dbCollections = mapOf(
+            "http://newly.found/collection1".toHttpUrl() to dbCollection1,
+            "http://newly.found/collection2".toHttpUrl() to dbCollection2
+        )
+        val localCollection1 = mockk<LocalTestCollection>()
+        val localCollection2 = mockk<LocalTestCollection>()
+        val localCollections = listOf(localCollection1, localCollection2)
+        every { localCollection1.collectionUrl } returns "http://newly.found/collection1"
+        every { localCollection2.collectionUrl } returns "http://newly.found/collection2"
+
+        // Should call the collection content sync on both collections
+        syncer.syncCollectionContents(provider, localCollections, dbCollections)
+        verify(exactly = 1) { syncer.syncCollection(provider, localCollection1, dbCollection1) }
+        verify(exactly = 1) { syncer.syncCollection(provider, localCollection2, dbCollection2) }
+    }
+    
+
+    // Test helpers
 
     class TestSyncer @AssistedInject constructor(
         @Assisted account: Account,
@@ -93,13 +169,17 @@ class SyncerTest {
         override val serviceType: String
             get() = ""
 
-        override fun prepare(provider: ContentProviderClient): Boolean = true
+        override fun prepare(provider: ContentProviderClient): Boolean =
+            true
 
-        override fun localSyncCollections(provider: ContentProviderClient): List<LocalTestCollection> = emptyList()
+        override fun localSyncCollections(provider: ContentProviderClient): List<LocalTestCollection> =
+            emptyList()
 
-        override fun getSyncCollections(serviceId: Long): List<Collection> = emptyList()
+        override fun getSyncCollections(serviceId: Long): List<Collection> =
+            emptyList()
 
-        override fun create(provider: ContentProviderClient, remoteCollection: Collection) {}
+        override fun create(provider: ContentProviderClient, remoteCollection: Collection): LocalTestCollection =
+            LocalTestCollection()
 
         override fun syncCollection(
             provider: ContentProviderClient,
