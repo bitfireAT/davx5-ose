ecdbcac25321f0ee7530314afd2f3b90490e627b
Ricki Hirner <hirner@bitfire.at>
2024-10-18 16:36:05 +0200

    Don't crash when logging null parameter

--- app/src/test/kotlin/at/bitfire/davdroid/log/PlainTextFormatterTest.kt
+++ app/src/test/kotlin/at/bitfire/davdroid/log/PlainTextFormatterTest.kt
@@ -4,18 +4,43 @@
 
 package at.bitfire.davdroid.log
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import java.util.logging.Level
 import java.util.logging.LogRecord
 
 class PlainTextFormatterTest {
 
+    private val minimum = PlainTextFormatter(
+        withTime = false,
+        withSource = false,
+        withException = false,
+        lineSeparator = null
+    )
+
     @Test
-    fun test_format_TruncatesMessage() {
-        val formatter = PlainTextFormatter.DEFAULT
-        val result = formatter.format(LogRecord(Level.INFO, "a".repeat(50000)))
-        // PlainTextFormatter.MAX_LENGTH is 10,000, so the message should be truncated to 10,000 + something
-        assert(result.length <= 10100)
+    fun test_format_param_null() {
+        val result = minimum.format(LogRecord(Level.INFO, "Message").apply {
+            parameters = arrayOf(null)
+        })
+        assertEquals("Message\n\tPARAMETER #1 = (null)", result)
+    }
+
+    @Test
+    fun test_format_param_object() {
+        val result = minimum.format(LogRecord(Level.INFO, "Message").apply {
+            parameters = arrayOf(object {
+                override fun toString() = "SomeObject[]"
+            })
+        })
+        assertEquals("Message\n\tPARAMETER #1 = SomeObject[]", result)
+    }
+
+    @Test
+    fun test_format_truncatesMessage() {
+        val result = minimum.format(LogRecord(Level.INFO, "a".repeat(50000)))
+        // PlainTextFormatter.MAX_LENGTH is 10,000
+        assertEquals(10000, result.length)
     }
 
 }
