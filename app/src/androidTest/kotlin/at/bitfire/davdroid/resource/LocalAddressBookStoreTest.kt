71b3525acf33abbbba1e8ac43116b0c631616d5d
Sunik Kupfer <kupfer@bitfire.at>
2024-11-14 12:07:33 +0100

    Test accountName

--- app/src/androidTest/kotlin/at/bitfire/davdroid/resource/LocalAddressBookStoreTest.kt
+++ app/src/androidTest/kotlin/at/bitfire/davdroid/resource/LocalAddressBookStoreTest.kt
@@ -1,13 +1,76 @@
 package at.bitfire.davdroid.resource
 
 import at.bitfire.davdroid.db.Collection
+import at.bitfire.davdroid.db.Service
 import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.SpyK
+import io.mockk.junit4.MockKRule
 import io.mockk.mockk
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
+import org.junit.Rule
 import org.junit.Test
 
 class LocalAddressBookStoreTest {
+
+    @get:Rule
+    val mockkRule = MockKRule(this)
+
+    @SpyK
+    @InjectMockKs
+    var localAddressBookStore = LocalAddressBookStore(
+        addressBookFactory = mockk(relaxed = true),
+        collectionRepository = mockk(relaxed = true),
+        context = mockk(relaxed = true),
+        localAddressBookFactory = mockk(relaxed = true),
+        logger = mockk(relaxed = true),
+        serviceRepository = mockk(relaxed = true) {
+            every { get(any<Long>()) } returns null
+            every { get(200) } returns mockk<Service> {
+                every { accountName } returns "MrRobert@example.com"
+            }
+        },
+        settings = mockk(relaxed = true)
+    )
+
+
+    @Test
+    fun test_accountName_missingService() {
+        val collection = mockk<Collection> {
+            every { id } returns 42
+            every { url } returns "https://example.com/addressbook/funnyfriends".toHttpUrl()
+            every { displayName } returns null
+            every { serviceId } returns 404
+        }
+        assertEquals("funnyfriends #42", localAddressBookStore.accountName(collection))
+    }
+
+    @Test
+    fun test_accountName_missingDisplayName() {
+        val collection = mockk<Collection> {
+            every { id } returns 42
+            every { url } returns "https://example.com/addressbook/funnyfriends".toHttpUrl()
+            every { displayName } returns null
+            every { serviceId } returns 200
+        }
+        val accountName = localAddressBookStore.accountName(collection)
+        assertEquals("funnyfriends (MrRobert@example.com) #42", accountName)
+    }
+
+    @Test
+    fun test_accountName_missingDisplayNameAndService() {
+        val collection = mockk<Collection> {
+            every { id } returns 1
+            every { url } returns "https://example.com/addressbook/funnyfriends".toHttpUrl()
+            every { displayName } returns null
+            every { serviceId } returns 404 // missing service
+        }
+        assertEquals("funnyfriends #1", localAddressBookStore.accountName(collection))
+    }
+
 
     /**
      * Tests the calculation of read only state is correct
