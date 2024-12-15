d892dd2b9cfa79a027749d94a004adad2c972d56
Arnau Mora <arnyminerz@proton.me>
2024-12-15 20:01:31 +0100

    Fixed padding problems with Edge-To-Edge (#1171)
    * Added navigation bars paddign
    
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>
    
    * Only consuming top padding
    
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>
    
    ---------
    
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- app/src/main/kotlin/at/bitfire/davdroid/ui/AccountsDrawerHandler.kt
+++ app/src/main/kotlin/at/bitfire/davdroid/ui/AccountsDrawerHandler.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
@@ -81,6 +82,7 @@ abstract class AccountsDrawerHandler {
         Column(modifier = Modifier
             .fillMaxWidth()
             .verticalScroll(rememberScrollState())
+            .navigationBarsPadding()
         ) {
             BrandingHeader()
 
