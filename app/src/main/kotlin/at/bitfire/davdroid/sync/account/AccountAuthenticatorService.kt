4cbe03b351a53dae597bab9d86cb76bf33e3b434
Ricki Hirner <hirner@bitfire.at>
2025-01-01 19:02:30 +0100

    Hide sync entries in system accounts (#1214)
    Hide sync entries in system settings; don't allow to manage accounts over system accounts anymore

--- app/src/main/kotlin/at/bitfire/davdroid/sync/account/AccountAuthenticatorService.kt
+++ app/src/main/kotlin/at/bitfire/davdroid/sync/account/AccountAuthenticatorService.kt
@@ -12,7 +12,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.core.os.bundleOf
-import at.bitfire.davdroid.ui.setup.LoginActivity
+import at.bitfire.davdroid.R
 
 
 /**
@@ -27,20 +27,21 @@ class AccountAuthenticatorService: Service() {
     }
 
     override fun onBind(intent: Intent?) =
-            accountAuthenticator.iBinder.takeIf { intent?.action == AccountManager.ACTION_AUTHENTICATOR_INTENT }
+        accountAuthenticator.iBinder.takeIf { intent?.action == AccountManager.ACTION_AUTHENTICATOR_INTENT }
 
 
     private class AccountAuthenticator(
-            val context: Context
+        val context: Context
     ): AbstractAccountAuthenticator(context) {
 
-        override fun addAccount(response: AccountAuthenticatorResponse?, accountType: String?, authTokenType: String?, requiredFeatures: Array<String>?, options: Bundle?): Bundle {
-            val intent = Intent(context, LoginActivity::class.java)
-            intent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE, response)
-            return bundleOf(AccountManager.KEY_INTENT to intent)
-        }
+        override fun addAccount(response: AccountAuthenticatorResponse?, accountType: String?, authTokenType: String?, requiredFeatures: Array<String>?, options: Bundle?) =
+            bundleOf(
+                AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE to response,
+                AccountManager.KEY_ERROR_CODE to AccountManager.ERROR_CODE_UNSUPPORTED_OPERATION,
+                AccountManager.KEY_ERROR_MESSAGE to context.getString(R.string.account_prefs_use_app)
+            )
 
-        override fun editProperties(response: AccountAuthenticatorResponse?, accountType: String?)  = null
+        override fun editProperties(response: AccountAuthenticatorResponse?, accountType: String?) = null
         override fun getAuthTokenLabel(p0: String?) = null
         override fun confirmCredentials(p0: AccountAuthenticatorResponse?, p1: Account?, p2: Bundle?) = null
         override fun updateCredentials(p0: AccountAuthenticatorResponse?, p1: Account?, p2: String?, p3: Bundle?) = null
