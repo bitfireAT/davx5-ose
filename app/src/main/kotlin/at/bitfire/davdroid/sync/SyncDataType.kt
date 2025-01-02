49628cc826caafc6592eb0aac08ba38637ad59ae
Sunik Kupfer <kupfer@bitfire.at>
2025-01-02 12:40:52 +0100

    Remove obsolete method

--- app/src/main/kotlin/at/bitfire/davdroid/sync/SyncDataType.kt
+++ app/src/main/kotlin/at/bitfire/davdroid/sync/SyncDataType.kt
@@ -11,7 +11,6 @@ import at.bitfire.davdroid.R
 import at.bitfire.ical4android.TaskProvider
 import dagger.hilt.EntryPoint
 import dagger.hilt.InstallIn
-import dagger.hilt.android.EntryPointAccessors
 import dagger.hilt.components.SingletonComponent
 
 enum class SyncDataType {
@@ -39,25 +38,6 @@ enum class SyncDataType {
             TASKS ->
                 TaskProvider.ProviderName.entries.map { it.authority }
         }
-
-
-    /**
-     * Gets the authority that should be used for managing sync adapters for this account and data type.
-     *
-     * In case of contacts, it will return the address books authority (and not the contacts authority).
-     */
-    fun toSyncAuthority(context: Context): String? = when(this) {
-        CONTACTS ->
-            context.getString(R.string.address_books_authority)
-        EVENTS ->
-            CalendarContract.AUTHORITY
-        TASKS -> {
-            val entryPoint = EntryPointAccessors.fromApplication<SyncDataTypeEntryPoint>(context)
-            val tasksAppManager = entryPoint.tasksAppManager()
-            tasksAppManager.currentProvider()?.authority
-        }
-    }
-
 
     companion object {
 
