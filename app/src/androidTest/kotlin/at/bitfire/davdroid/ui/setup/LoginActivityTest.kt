3bb03d26aae17cbff00112fd7ae94974727b4f1e
Sunik Kupfer <kupfer@bitfire.at>
2025-01-15 14:08:01 +0100

    Add test

--- app/src/androidTest/kotlin/at/bitfire/davdroid/ui/setup/LoginActivityTest.kt
+++ app/src/androidTest/kotlin/at/bitfire/davdroid/ui/setup/LoginActivityTest.kt
@@ -10,6 +10,19 @@ class LoginActivityTest {
     @Test
     fun loginInfoFromIntent() {
         val intent = Intent().apply {
+            data = Uri.parse("https://example.com/nextcloud")
+            putExtra(LoginActivity.EXTRA_USERNAME, "user")
+            putExtra(LoginActivity.EXTRA_PASSWORD, "password")
+        }
+        val loginInfo = LoginActivity.loginInfoFromIntent(intent)
+        assertEquals("https://example.com/nextcloud", loginInfo.baseUri.toString())
+        assertEquals("user", loginInfo.credentials!!.username)
+        assertEquals("password", loginInfo.credentials.password)
+    }
+
+    @Test
+    fun loginInfoFromIntent_withPort() {
+        val intent = Intent().apply {
             data = Uri.parse("https://example.com:444/nextcloud")
             putExtra(LoginActivity.EXTRA_USERNAME, "user")
             putExtra(LoginActivity.EXTRA_PASSWORD, "password")
