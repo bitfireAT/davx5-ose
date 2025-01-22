331f8d574377d7e78871aa47cd47f8814e69f7f7
Ricki Hirner <hirner@bitfire.at>
2025-01-22 15:53:40 +0100

    Bump version to 4.4.6-rc.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
     defaultConfig {
         applicationId = "at.bitfire.davdroid"
 
-        versionCode = 404060001
-        versionName = "4.4.6-beta.1"
+        versionCode = 404060002
+        versionName = "4.4.6-rc.1"
 
         setProperty("archivesBaseName", "davx5-ose-$versionName")
 
