c8834ed8eede07010cad8988f577b65db02e560f
Arnau Mora <arnyminerz@proton.me>
2025-03-10 19:42:34 +0100

    Added sync icon widget
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- app/src/main/kotlin/at/bitfire/davdroid/ui/widget/IconSyncButtonWidget.kt
+++ app/src/main/kotlin/at/bitfire/davdroid/ui/widget/IconSyncButtonWidget.kt
@@ -8,7 +8,6 @@ import android.content.Context
 import android.widget.Toast
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.glance.ColorFilter
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
@@ -21,12 +20,9 @@ import androidx.glance.appwidget.cornerRadius
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
 import androidx.glance.layout.Alignment
-import androidx.glance.layout.Row
-import androidx.glance.layout.fillMaxWidth
-import androidx.glance.layout.padding
+import androidx.glance.layout.Box
+import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.size
-import androidx.glance.text.Text
-import androidx.glance.text.TextDefaults
 import androidx.glance.unit.ColorProvider
 import at.bitfire.davdroid.R
 import at.bitfire.davdroid.ui.M3ColorScheme
@@ -36,9 +32,9 @@ import dagger.hilt.android.EntryPointAccessors
 import dagger.hilt.components.SingletonComponent
 
 /**
- * A widget with a "Sync all" button displaying an icon and a label.
+ * A widget with a "Sync all" button displaying just an icon to indicate the action.
  */
-class LabeledSyncButtonWidget : GlanceAppWidget() {
+class IconSyncButtonWidget : GlanceAppWidget() {
 
     // Hilt over @AndroidEntryPoint is not available for widgets
     @EntryPoint
@@ -63,35 +59,23 @@ class LabeledSyncButtonWidget : GlanceAppWidget() {
     private fun WidgetContent(model: SyncWidgetModel) {
         val context = LocalContext.current
 
-        Row(
+        Box(
             modifier = GlanceModifier
-                .fillMaxWidth()
+                .size(50.dp)
                 .background(ColorProvider(M3ColorScheme.primaryLight))
-                .cornerRadius(16.dp)
-                .padding(4.dp)
+                .cornerRadius(25.dp)
                 .clickable {
                     model.requestSync()
                     Toast.makeText(context, R.string.sync_started, Toast.LENGTH_SHORT).show()
                 },
-            verticalAlignment = Alignment.CenterVertically
+            contentAlignment = Alignment.Center,
         ) {
-            val onPrimary = ColorProvider(M3ColorScheme.onPrimaryLight)
             Image(
                 provider = ImageProvider(R.drawable.ic_sync),
                 contentDescription = context.getString(R.string.widget_sync_all_accounts),
-                modifier = GlanceModifier
-                    .padding(vertical = 8.dp, horizontal = 8.dp)
-                    .size(32.dp),
-                colorFilter = ColorFilter.tint(onPrimary)
-            )
-            Text(
-                text = context.getString(R.string.widget_sync_all),
-                modifier = GlanceModifier
-                    .defaultWeight()
-                    .padding(end = 8.dp),
-                style = TextDefaults.defaultTextStyle.copy(
-                    color = onPrimary,
-                    fontSize = 16.sp
+                modifier = GlanceModifier.fillMaxSize().size(32.dp),
+                colorFilter = ColorFilter.tint(
+                    ColorProvider(M3ColorScheme.onPrimaryLight)
                 )
             )
         }
---
+++ app/src/main/kotlin/at/bitfire/davdroid/ui/widget/IconSyncButtonWidgetReceiver.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright © All Contributors. See LICENSE and AUTHORS in the root directory for details.
+ */
+
+package at.bitfire.davdroid.ui.widget
+
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+
+class IconSyncButtonWidgetReceiver : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget = IconSyncButtonWidget()
+}
