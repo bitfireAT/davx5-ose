32bec93d6de31f035ca5108e99f485858973725c
Sunik Kupfer <kupfer@bitfire.at>
2025-04-15 12:31:18 +0200

    Rename method for clarity and update kdoc
    Signed-off-by: Sunik Kupfer <kupfer@bitfire.at>

--- app/src/main/kotlin/at/bitfire/davdroid/resource/LocalJtxCollection.kt
+++ app/src/main/kotlin/at/bitfire/davdroid/resource/LocalJtxCollection.kt
@@ -61,9 +61,12 @@ class LocalJtxCollection(account: Account, client: ContentProviderClient, id: Lo
     }
 
     /**
-     * Finds and returns a recurring instance of a [LocalJtxICalObject]
+     * Finds and returns a recurrence instance of a [LocalJtxICalObject]
+     * @param uid       UID of the main VTODO
+     * @param recurid   RECURRENCE-ID of the recurrence instance
+     * @return LocalJtxICalObject or null if none or multiple entries found
      */
-    fun findRecurring(uid: String, recurid: String): LocalJtxICalObject? {
+    fun findRecurInstance(uid: String, recurid: String): LocalJtxICalObject? {
         val values = queryRecur(uid, recurid) ?: return null
         return LocalJtxICalObject.Factory.fromProvider(this, values)
     }
--- app/src/main/kotlin/at/bitfire/davdroid/sync/JtxSyncManager.kt
+++ app/src/main/kotlin/at/bitfire/davdroid/sync/JtxSyncManager.kt
@@ -168,7 +168,7 @@ class JtxSyncManager @AssistedInject constructor(
             // we update the existing (generated) entry
             val recurid = jtxICalObject.recurid
             if(recurid != null) {
-                val local = localCollection.findRecurring(jtxICalObject.uid, recurid)
+                val local = localCollection.findRecurInstance(jtxICalObject.uid, recurid)
                 SyncException.wrapWithLocalResource(local) {
                     logger.log(Level.INFO, "Updating $fileName with recur instance $recurid in local list", jtxICalObject)
                     if(local != null) {
