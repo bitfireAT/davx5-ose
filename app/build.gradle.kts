77a6e5c5ab199304e206d249b07933279bdd690b
Ricki Hirner <hirner@bitfire.at>
2025-04-24 13:12:38 +0200

    Bump version to 4.4.10-alpha.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
     defaultConfig {
         applicationId = "at.bitfire.davdroid"
 
-        versionCode = 404090001
-        versionName = "4.4.9"
+        versionCode = 404100000
+        versionName = "4.4.10-alpha.1"
 
         setProperty("archivesBaseName", "davx5-ose-$versionName")
 
