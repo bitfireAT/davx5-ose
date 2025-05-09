827a1b954fc836c95c5bee13a2185b26e9120d2b
Ricki Hirner <hirner@bitfire.at>
2025-05-09 10:59:34 +0200

    Update version code and name and increase max build metaspace size to 1GB

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
     defaultConfig {
         applicationId = "at.bitfire.davdroid"
 
-        versionCode = 404100003
-        versionName = "4.4.10-rc.1"
+        versionCode = 404100004
+        versionName = "4.4.10"
 
         setProperty("archivesBaseName", "davx5-ose-$versionName")
 
