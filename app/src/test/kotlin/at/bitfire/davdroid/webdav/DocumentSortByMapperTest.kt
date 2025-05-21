00437321f098e36256c4ec46f521f3ed008c9b6c
Ricki Hirner <hirner@bitfire.at>
2025-05-21 10:18:16 +0200

    Rename DavDocumentsProviderTest to DocumentSortByMapperTest and update test method name

--- app/src/test/kotlin/at/bitfire/davdroid/webdav/DocumentSortByMapperTest.kt
+++ app/src/test/kotlin/at/bitfire/davdroid/webdav/DocumentSortByMapperTest.kt
@@ -7,41 +7,46 @@ package at.bitfire.davdroid.webdav
 import android.provider.DocumentsContract.Document
 import junit.framework.TestCase.assertEquals
 import org.junit.Test
+import java.util.logging.Logger
 
-class DavDocumentsProviderTest {
+class DocumentSortByMapperTest {
+
+    private val mapper = DocumentSortByMapper(Logger.getGlobal())
+
     @Test
-    fun `test processOrderByQuery`() {
+    fun test_MapContentProviderToSql() {
         // Valid column without direction
         assertEquals(
             "displayName",
-            DavDocumentsProvider.processOrderByQuery(Document.COLUMN_DISPLAY_NAME),
+            mapper.mapContentProviderToSql(Document.COLUMN_DISPLAY_NAME)
         )
         // Valid column with direction
         assertEquals(
             "displayName ASC",
-            DavDocumentsProvider.processOrderByQuery("${Document.COLUMN_DISPLAY_NAME} ASC"),
+            mapper.mapContentProviderToSql("${Document.COLUMN_DISPLAY_NAME} ASC")
         )
         // Valid column with direction and multiple spaces
         assertEquals(
             "displayName ASC",
-            DavDocumentsProvider.processOrderByQuery("${Document.COLUMN_DISPLAY_NAME}   ASC"),
+            mapper.mapContentProviderToSql("${Document.COLUMN_DISPLAY_NAME}   ASC")
         )
         // Invalid column without direction
         assertEquals(
             null,
-            DavDocumentsProvider.processOrderByQuery("invalid"),
+            mapper.mapContentProviderToSql("invalid")
         )
         // Invalid column with direction
         assertEquals(
             null,
-            DavDocumentsProvider.processOrderByQuery("invalid ASC"),
+            mapper.mapContentProviderToSql("invalid ASC")
         )
         // Valid and invalid columns with and without directions
         assertEquals(
             "displayName, mimeType ASC",
-            DavDocumentsProvider.processOrderByQuery(
+            mapper.mapContentProviderToSql(
                 "${Document.COLUMN_DISPLAY_NAME}, invalid DESC, ${Document.COLUMN_MIME_TYPE} ASC"
-            ),
+            )
         )
     }
+
 }
