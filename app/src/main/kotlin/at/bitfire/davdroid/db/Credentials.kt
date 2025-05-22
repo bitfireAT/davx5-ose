8dbc07155f8071c7ed318eb8fdb530f720f3b46e
Ricki Hirner <hirner@bitfire.at>
2025-05-22 19:07:17 +0200

    Fix tests

--- app/src/main/kotlin/at/bitfire/davdroid/db/Credentials.kt
+++ app/src/main/kotlin/at/bitfire/davdroid/db/Credentials.kt
@@ -32,4 +32,27 @@ data class Credentials(
         return "Credentials(" + s.joinToString(", ") + ")"
     }
 
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Credentials
+
+        if (username != other.username) return false
+        if (!password.contentEquals(other.password)) return false
+        if (certificateAlias != other.certificateAlias) return false
+        if (authState != other.authState) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = username?.hashCode() ?: 0
+        result = 31 * result + (password?.contentHashCode() ?: 0)
+        result = 31 * result + (certificateAlias?.hashCode() ?: 0)
+        result = 31 * result + (authState?.hashCode() ?: 0)
+        return result
+    }
+
 }
