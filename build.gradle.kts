bbc7fbfa1e2de2097d4df8e3e923e7536c6e5b9b
Arnau Mora <arnyminerz@proton.me>
2025-05-26 19:35:11 +0200

    Added missing plugin
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.ksp) apply false
 
     alias(libs.plugins.mikepenz.aboutLibraries) apply false
