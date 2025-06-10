bdfd17e4a744e959e14a02c9af6e264162a915b5
Arnau Mora <arnyminerz@proton.me>
2025-06-10 12:49:10 +0200

    Move url text to top of fields
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- app/src/main/kotlin/at/bitfire/davdroid/ui/setup/UrlLogin.kt
+++ app/src/main/kotlin/at/bitfire/davdroid/ui/setup/UrlLogin.kt
@@ -103,6 +103,19 @@ fun UrlLoginScreen(
                     .padding(vertical = 8.dp)
             )
 
+            val manualUrl = Constants.MANUAL_URL.buildUpon()
+                .appendPath(Constants.MANUAL_PATH_ACCOUNTS_COLLECTIONS)
+                .fragment(Constants.MANUAL_FRAGMENT_SERVICE_DISCOVERY)
+                .build()
+            val urlInfo = HtmlCompat.fromHtml(stringResource(R.string.login_base_url_info, manualUrl), HtmlCompat.FROM_HTML_MODE_COMPACT)
+            Text(
+                text = urlInfo.toAnnotatedString(),
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp, bottom = 16.dp)
+            )
+
             OutlinedTextField(
                 value = url,
                 onValueChange = onSetUrl,
@@ -119,19 +132,6 @@ fun UrlLoginScreen(
                 modifier = Modifier
                     .fillMaxWidth()
                     .focusRequester(focusRequester)
-            )
-
-            val manualUrl = Constants.MANUAL_URL.buildUpon()
-                .appendPath(Constants.MANUAL_PATH_ACCOUNTS_COLLECTIONS)
-                .fragment(Constants.MANUAL_FRAGMENT_SERVICE_DISCOVERY)
-                .build()
-            val urlInfo = HtmlCompat.fromHtml(stringResource(R.string.login_base_url_info, manualUrl), HtmlCompat.FROM_HTML_MODE_COMPACT)
-            Text(
-                text = urlInfo.toAnnotatedString(),
-                style = MaterialTheme.typography.bodyLarge,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 8.dp, bottom = 16.dp)
             )
 
             OutlinedTextField(
