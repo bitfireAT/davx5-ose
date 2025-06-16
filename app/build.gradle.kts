dd453a7837e45f04e85a287605c5e485adf5774d
Ricki Hirner <hirner@bitfire.at>
2025-06-16 13:17:46 +0200

    Update version to 4.4.12-rc.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
     defaultConfig {
         applicationId = "at.bitfire.davdroid"
 
-        versionCode = 404120000
-        versionName = "4.4.12-alpha.1"
+        versionCode = 404120001
+        versionName = "4.4.12-rc.1"
 
         setProperty("archivesBaseName", "davx5-ose-$versionName")
 
