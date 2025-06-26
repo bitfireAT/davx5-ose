09e6d2742031f52576a203c1d78822d4fa4bfcff
Sunik Kupfer <kupfer@bitfire.at>
2025-06-26 11:20:07 +0200

    Remove unused uri handler variable
    Signed-off-by: Sunik Kupfer <kupfer@bitfire.at>

--- app/src/main/kotlin/at/bitfire/davdroid/ui/setup/GoogleLogin.kt
+++ app/src/main/kotlin/at/bitfire/davdroid/ui/setup/GoogleLogin.kt
@@ -33,7 +33,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
@@ -133,7 +132,6 @@ fun GoogleLoginScreen(
     onLogin: () -> Unit = {}
 ) {
     val context = LocalContext.current
-    val uriHandler = LocalUriHandler.current
 
     Column(
         Modifier
