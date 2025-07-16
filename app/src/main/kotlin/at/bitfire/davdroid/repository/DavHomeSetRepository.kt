2f53702c26d3f3650edecf962641e0bd2876ebb1
Ricki Hirner <hirner@bitfire.at>
2025-07-16 14:35:50 +0200

    Move homeset insert/update logic from repository to DAO; add thread-safety test

--- app/src/main/kotlin/at/bitfire/davdroid/repository/DavHomeSetRepository.kt
+++ app/src/main/kotlin/at/bitfire/davdroid/repository/DavHomeSetRepository.kt
@@ -5,7 +5,6 @@
 package at.bitfire.davdroid.repository
 
 import android.accounts.Account
-import androidx.room.Transaction
 import at.bitfire.davdroid.db.AppDatabase
 import at.bitfire.davdroid.db.HomeSet
 import at.bitfire.davdroid.db.Service
@@ -29,21 +28,8 @@ class DavHomeSetRepository @Inject constructor(
     fun getCalendarHomeSetsFlow(account: Account) =
         dao.getBindableByAccountAndServiceTypeFlow(account.name, Service.TYPE_CALDAV)
 
-
-    /**
-     * Tries to insert new row, but updates existing row if already present.
-     * This method preserves the primary key, as opposed to using "@Insert(onConflict = OnConflictStrategy.REPLACE)"
-     * which will create a new row with incremented ID and thus breaks entity relationships!
-     *
-     * @return ID of the row, that has been inserted or updated. -1 If the insert fails due to other reasons.
-     */
-    @Transaction
-    fun insertOrUpdateByUrlBlocking(homeset: HomeSet): Long =
-        dao.getByUrl(homeset.serviceId, homeset.url.toString())?.let { existingHomeset ->
-            dao.update(homeset.copy(id = existingHomeset.id))
-            existingHomeset.id
-        } ?: dao.insert(homeset)
-
+    fun insertOrUpdateByUrlBlocking(homeSet: HomeSet): Long =
+        dao.insertOrUpdateByUrlBlocking(homeSet)
 
     fun deleteBlocking(homeSet: HomeSet) = dao.delete(homeSet)
 
