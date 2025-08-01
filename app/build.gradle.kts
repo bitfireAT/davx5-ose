0e6644305a74675d52cdd6cdaeaf81ea3330e65c
Ricki Hirner <hirner@bitfire.at>
2025-08-01 10:49:28 +0200

    Bump version to 4.5.3-rc.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
     defaultConfig {
         applicationId = "at.bitfire.davdroid"
 
-        versionCode = 405030001
-        versionName = "4.5.3-rc.1"
+        versionCode = 405030002
+        versionName = "4.5.3-rc.2"
 
         setProperty("archivesBaseName", "davx5-ose-$versionName")
 
