2cb72d4a6d650d086845b84fcd51f9f71998e7d8
Ricki Hirner <hirner@bitfire.at>
2025-09-01 09:53:40 +0200

    Make SensitiveString comparable

--- app/src/main/kotlin/at/bitfire/davdroid/util/SensitiveString.kt
+++ app/src/main/kotlin/at/bitfire/davdroid/util/SensitiveString.kt
@@ -6,8 +6,8 @@ package at.bitfire.davdroid.util
 
 /**
  * Wrapper for passwords and other sensitive strings so that they're not directly [String]s,
- * so that they're less likely to be used in plain text unintentionally (for instance printed in logs
- * by [Any.toString]).
+ * so that they're less likely to be used in plain text unintentionally, like being printed in logs
+ * by [Any.toString].
  */
 class SensitiveString private constructor(
     private val data: String
@@ -15,6 +15,14 @@ class SensitiveString private constructor(
 
     fun asCharArray() = data.toCharArray()
     fun asString() = data
+
+
+    // make comparable by data
+
+    override fun equals(other: Any?) =
+        data == other
+
+    override fun hashCode() = data.hashCode()
 
     override fun toString() = "*****"
 
