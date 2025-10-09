5674d6b954a4a3a6165ec3bbfdbf8202ff26f6f8
Sunik Kupfer <kupfer@bitfire.at>
2025-10-09 12:05:31 +0200

    ExceptionInfoDialog: show explanation on http 405 and others (#1739)
    * Show explanation on http 405
    
    * Also show message for http 5xx
    
    * Correct spelling

--- app/src/main/kotlin/at/bitfire/davdroid/ui/composable/ExceptionInfoDialog.kt
+++ app/src/main/kotlin/at/bitfire/davdroid/ui/composable/ExceptionInfoDialog.kt
@@ -61,8 +61,17 @@ fun ExceptionInfoDialog(
             }
         },
         text = {
+            val message = if (exception is HttpException) {
+                 when (exception.statusCode) {
+                    403 -> context.getString(R.string.debug_info_http_403_description)
+                    404 -> context.getString(R.string.debug_info_http_404_description)
+                    405 -> context.getString(R.string.debug_info_http_405_description)
+                     in 500..599 -> context.getString(R.string.debug_info_http_5xx_description)
+                    else -> null
+                }
+            } else null
             Text(
-                exception::class.java.name + "\n" + exception.localizedMessage,
+                text = message ?: "${exception::class.java.name}\n${exception.localizedMessage}",
                 style = MaterialTheme.typography.bodyLarge
             )
         },
