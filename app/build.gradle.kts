84b9a14ba1435f00ae9d6ea3cef3f04703e20bfd
Ricki Hirner <hirner@bitfire.at>
2025-11-27 11:15:39 +0100

    Update version to 4.5.6.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
     defaultConfig {
         applicationId = "at.bitfire.davdroid"
 
-        versionCode = 405060004
-        versionName = "4.5.6"
+        versionCode = 405060100
+        versionName = "4.5.6.1"
 
         base.archivesName = "davx5-ose-$versionName"
 
