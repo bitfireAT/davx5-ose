d642b6e37c489a6047ecf8dec5ef47d14df68e9d
Ricki Hirner <hirner@bitfire.at>
2026-01-27 17:37:23 +0100

    Refactor socket factory caching logic for better clarity

--- app/src/main/kotlin/at/bitfire/davdroid/network/ConnectionSecurityManager.kt
+++ app/src/main/kotlin/at/bitfire/davdroid/network/ConnectionSecurityManager.kt
@@ -6,14 +6,12 @@ package at.bitfire.davdroid.network
 
 import at.bitfire.cert4android.CustomCertManager
 import java.lang.ref.SoftReference
-import java.security.KeyStore
 import java.util.Optional
 import java.util.concurrent.ConcurrentHashMap
 import javax.inject.Inject
 import javax.inject.Singleton
 import javax.net.ssl.SSLContext
 import javax.net.ssl.SSLSocketFactory
-import javax.net.ssl.TrustManagerFactory
 import javax.net.ssl.X509TrustManager
 import kotlin.jvm.optionals.getOrNull
 
@@ -23,7 +21,7 @@ import kotlin.jvm.optionals.getOrNull
 @Singleton
 class ConnectionSecurityManager @Inject constructor(
     private val customHostnameVerifier: Optional<CustomCertManager.HostnameVerifier>,
-    customTrustManager: Optional<CustomCertManager>,
+    private val customTrustManager: Optional<CustomCertManager>,
     private val keyManagerFactory: ClientCertKeyManager.Factory
 ) {
 
@@ -37,12 +35,6 @@ class ConnectionSecurityManager @Inject constructor(
         ConcurrentHashMap(2)    // usually not more than: one for no client certificates + one for a certain certificate alias
 
     /**
-     * The TrustManager that shall be used for all connections. This is either the cert4android custom TrustManager
-     * (if available and enabled by build flags), or the default TrustManager.
-     */
-    private val trustManager: X509TrustManager = customTrustManager.getOrNull() ?: defaultTrustManager()
-
-    /**
      * Provides the [ConnectionSecurityContext] for a given [certificateAlias].
      *
      * Uses [socketFactoryCache] to cache the entries (per [certificateAlias]).
@@ -51,43 +43,49 @@ class ConnectionSecurityManager @Inject constructor(
      * @return the connection security context
      */
     fun getContext(certificateAlias: String?): ConnectionSecurityContext {
-        val socketFactory = synchronized(socketFactoryCache) {      // synchronize map access
-            val certKey = Optional.ofNullable(certificateAlias)
-            val cachedFactory = socketFactoryCache[certKey]?.get()
-            if (cachedFactory != null)
-                return@synchronized cachedFactory
-            // no cached value, calculate and store into cache
-
-            // when a client certificate alias is given, create and use the respective ClientKeyManager
-            val clientKeyManager = certificateAlias?.let { keyManagerFactory.create(it) }
-
-            // create SSLContext that provides the SSLSocketFactory
-            val sslContext = SSLContext.getInstance("TLS").apply {
-                init(
-                    /* km = */ if (clientKeyManager != null) arrayOf(clientKeyManager) else null,
-                    /* tm = */ arrayOf(trustManager),
-                    /* random = */ null
-                )
-            }
-
-            // cache reference and return socket factory
-            sslContext.socketFactory.also { factory ->
-                socketFactoryCache[certKey] = SoftReference(factory)
-            }
-        }
+        /* We only need a custom socket factory for
+           - client certificates and/or
+           - when cert4android is active (= there's a custom trustManager). */
+        val socketFactory = if (certificateAlias != null || customTrustManager.isPresent)
+            getSocketFactory(certificateAlias)
+        else
+            null
 
         return ConnectionSecurityContext(
             sslSocketFactory = socketFactory,
-            trustManager = trustManager,
+            trustManager = customTrustManager.getOrNull(),
             hostnameVerifier = customHostnameVerifier.getOrNull(),
             disableHttp2 = certificateAlias != null
         )
     }
 
-    private fun defaultTrustManager(): X509TrustManager {
-        val factory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
-        factory.init(null as KeyStore?)
-        return factory.trustManagers.filterIsInstance<X509TrustManager>().first()
+    private fun getSocketFactory(certificateAlias: String?): SSLSocketFactory = synchronized(socketFactoryCache) {
+        // look up cache first
+        val certKey = Optional.ofNullable(certificateAlias)
+        val cachedFactory = socketFactoryCache[certKey]?.get()
+        if (cachedFactory != null)
+            return cachedFactory
+        // no cached value, calculate and store into cache
+
+        // when a client certificate alias is given, create and use the respective ClientKeyManager
+        val clientKeyManager = certificateAlias?.let { keyManagerFactory.create(it) }
+
+        // cert4android custom TrustManager (if available and enabled by build flags), or null
+        val trustManager: X509TrustManager? = customTrustManager.getOrNull()
+
+        // create SSLContext that provides the SSLSocketFactory
+        val sslContext = SSLContext.getInstance("TLS").apply {
+            init(
+                /* km = */ if (clientKeyManager != null) arrayOf(clientKeyManager) else null /* default KeyManager */,
+                /* tm = */ if (trustManager != null) arrayOf(trustManager) else null /* default TrustManager */,
+                /* random = */ null /* default RNG */
+            )
+        }
+
+        // cache reference and return socket factory
+        return sslContext.socketFactory.also { factory ->
+            socketFactoryCache[certKey] = SoftReference(factory)
+        }
     }
 
 }
