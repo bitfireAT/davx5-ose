3e979755ab380271cc04db09d2902ea51ccb7c4f
Arnau Mora Gras <arnyminerz@proton.me>
2026-01-29 11:52:33 +0100

    Improve explanation
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- app/src/main/kotlin/at/bitfire/davdroid/resource/LocalCalendarStore.kt
+++ app/src/main/kotlin/at/bitfire/davdroid/resource/LocalCalendarStore.kt
@@ -123,6 +123,8 @@ class LocalCalendarStore @Inject constructor(
         )
 
         // By default, set all new calendars as non-primary. Right now we do not support actually fetching the primary calendar.
+        // Google Calendar uses this field to determine whether to read the calendar name together with the account name in TalkBack:
+        //   if primary, the calendar name is excluded, and as such, all calendars in the account sound the same.
         values.put(Calendars.IS_PRIMARY, 0)
 
         if (withColor && info.color != null)
