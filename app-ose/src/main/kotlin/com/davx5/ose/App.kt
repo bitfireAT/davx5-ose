6b6c340844b9b8e042435e770f02b8f9ed1eb411
Ricki Hirner <hirner@bitfire.at>
2026-02-06 14:04:11 +0100

    Refactor App Initialization
    - Introduce `CoreApp` for common initialization logic
    - Refactor `App` to extend `CoreApp` and handle WorkManager configuration

--- app-ose/src/main/kotlin/com/davx5/ose/App.kt
+++ app-ose/src/main/kotlin/com/davx5/ose/App.kt
@@ -4,80 +4,27 @@
 
 package com.davx5.ose
 
-import android.app.Application
-import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
-import at.bitfire.davdroid.di.scope.DefaultDispatcher
-import at.bitfire.davdroid.log.LogManager
-import at.bitfire.davdroid.startup.StartupPlugin
-import at.bitfire.davdroid.sync.account.AccountsCleanupWorker
-import at.bitfire.davdroid.ui.UiUtils
+import at.bitfire.davdroid.CoreApp
 import dagger.hilt.android.HiltAndroidApp
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import java.util.logging.Logger
-import javax.inject.Inject
 
+/**
+ * Actual implementation of Application, used for Hilt. Delegates to [CoreApp].
+ */
 @HiltAndroidApp
-class App: Application(), Configuration.Provider {
-
-    @Inject
-    lateinit var logger: Logger
+class App: CoreApp(), Configuration.Provider {
 
     /**
-     * Creates the [at.bitfire.davdroid.log.LogManager] singleton and thus initializes logging.
+     * Required for Hilt/WorkManager integration, see:
+     * https://developer.android.com/training/dependency-injection/hilt-jetpack#workmanager
+     *
+     * This requires to remove the androidx.work.WorkManagerInitializer from App Startup
+     * in the AndroidManifest, see:
+     * https://developer.android.com/develop/background-work/background-tasks/persistent/configuration/custom-configuration#remove-default
      */
-    @Inject
-    lateinit var logManager: LogManager
-
-    @Inject
-    @DefaultDispatcher
-    lateinit var defaultDispatcher: CoroutineDispatcher
-
-    @Inject
-    lateinit var plugins: Set<@JvmSuppressWildcards StartupPlugin>
-
-    @Inject
-    lateinit var workerFactory: HiltWorkerFactory
-
     override val workManagerConfiguration: Configuration
         get() = Configuration.Builder()
             .setWorkerFactory(workerFactory)
             .build()
-
-
-    override fun onCreate() {
-        super.onCreate()
-
-        logger.fine("Logging using LogManager $logManager")
-
-        // set light/dark mode
-        UiUtils.updateTheme(this)   // when this is called in the asynchronous thread below, it recreates
-                                 // some current activity and causes an IllegalStateException in rare cases
-
-        // run startup plugins (sync)
-        for (plugin in plugins.sortedBy { it.priority() }) {
-            logger.fine("Running startup plugin: $plugin (onAppCreate)")
-            plugin.onAppCreate()
-        }
-
-        // don't block UI for some background checks
-        @OptIn(DelicateCoroutinesApi::class)
-        GlobalScope.launch(defaultDispatcher) {
-            // clean up orphaned accounts in DB from time to time
-            AccountsCleanupWorker.Companion.enable(this@App)
-
-            // create/update app shortcuts
-            UiUtils.updateShortcuts(this@App)
-
-            // run startup plugins (async)
-            for (plugin in plugins.sortedBy { it.priorityAsync() }) {
-                logger.fine("Running startup plugin: $plugin (onAppCreateAsync)")
-                plugin.onAppCreateAsync()
-            }
-        }
-    }
 
 }
---
+++ core/src/main/kotlin/at/bitfire/davdroid/CoreApp.kt
@@ -0,0 +1,79 @@
+/*
+ * Copyright © All Contributors. See LICENSE and AUTHORS in the root directory for details.
+ */
+
+package at.bitfire.davdroid
+
+import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import at.bitfire.davdroid.di.scope.DefaultDispatcher
+import at.bitfire.davdroid.log.LogManager
+import at.bitfire.davdroid.startup.StartupPlugin
+import at.bitfire.davdroid.sync.account.AccountsCleanupWorker
+import at.bitfire.davdroid.ui.UiUtils
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.util.logging.Logger
+import javax.inject.Inject
+
+/**
+ * The actual app should extend this class. The derived class must then be set
+ * as `@HiltAndroidApp.`
+ */
+open class CoreApp: Application() {
+
+    @Inject
+    lateinit var logger: Logger
+
+    /**
+     * Creates the [at.bitfire.davdroid.log.LogManager] singleton and thus initializes logging.
+     */
+    @Inject
+    lateinit var logManager: LogManager
+
+    @Inject
+    @DefaultDispatcher
+    lateinit var defaultDispatcher: CoroutineDispatcher
+
+    @Inject
+    lateinit var plugins: Set<@JvmSuppressWildcards StartupPlugin>
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+
+    override fun onCreate() {
+        super.onCreate()
+
+        logger.fine("Logging using LogManager $logManager")
+
+        // set light/dark mode
+        UiUtils.updateTheme(this)   // when this is called in the asynchronous thread below, it recreates
+                                 // some current activity and causes an IllegalStateException in rare cases
+
+        // run startup plugins (sync)
+        for (plugin in plugins.sortedBy { it.priority() }) {
+            logger.fine("Running startup plugin: $plugin (onAppCreate)")
+            plugin.onAppCreate()
+        }
+
+        // don't block UI for some background checks
+        @OptIn(DelicateCoroutinesApi::class)
+        GlobalScope.launch(defaultDispatcher) {
+            // clean up orphaned accounts in DB from time to time
+            AccountsCleanupWorker.Companion.enable(this@CoreApp)
+
+            // create/update app shortcuts
+            UiUtils.updateShortcuts(this@CoreApp)
+
+            // run startup plugins (async)
+            for (plugin in plugins.sortedBy { it.priorityAsync() }) {
+                logger.fine("Running startup plugin: $plugin (onAppCreateAsync)")
+                plugin.onAppCreateAsync()
+            }
+        }
+    }
+
+}
