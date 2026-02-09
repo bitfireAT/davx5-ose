19f55f33a5db395296ec1c92b678871ab88ec7f0
Arnau Mora <arnyminerz@proton.me>
2026-02-09 18:31:40 +0100

    Add `SyncValidator`
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

---
+++ core/src/main/kotlin/at/bitfire/davdroid/sync/SyncValidator.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright © All Contributors. See LICENSE and AUTHORS in the root directory for details.
+ */
+
+package at.bitfire.davdroid.sync
+
+import dagger.BindsOptionalOf
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+interface SyncValidator {
+
+    /**
+     * Called before synchronization within a sync adapter is started. Can be used for
+     * license checks etc. Must be thread-safe.
+     *
+     * @return whether synchronization shall take place (false to abort)
+     */
+    fun beforeSync(): Boolean
+
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface SyncValidatorModule {
+    @BindsOptionalOf
+    fun syncValidator(): SyncValidator
+}
