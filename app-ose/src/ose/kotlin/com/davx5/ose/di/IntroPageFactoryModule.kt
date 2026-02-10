1b8f215ffc6c3b006e1520d9b593b53bd5ece333
Ricki Hirner <hirner@bitfire.at>
2026-02-10 11:56:43 +0100

    Update intro page factory module name
    - Rename `Global` interface to `IntroPageFactoryModule` for clarity

--- app-ose/src/ose/kotlin/com/davx5/ose/di/IntroPageFactoryModule.kt
+++ app-ose/src/ose/kotlin/com/davx5/ose/di/IntroPageFactoryModule.kt
@@ -13,7 +13,7 @@ import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
-interface Global {
+interface IntroPageFactoryModule {
     @Binds
     fun introPageFactory(impl: OseIntroPageFactory): IntroPageFactory
 }
