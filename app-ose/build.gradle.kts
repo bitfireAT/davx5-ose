e796448bad8bd529e2f2287db21823c073ba6088
Ricki Hirner <hirner@bitfire.at>
2026-02-10 11:53:11 +0100

    Split subprojects: fix aboutLibraries (bitfireAT/davx5#794)
    Fix aboutLibraries plugin
    
    - Add aboutLibraries plugin to root build.gradle.kts
    - Apply aboutLibraries plugin in app-non-ose module

--- app-ose/build.gradle.kts
+++ app-ose/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.hilt)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.mikepenz.aboutLibraries.android)
 }
 
 android {
