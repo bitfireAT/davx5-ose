f4129d2d0cd6806bbfaacfb7eae766600b2b31af
Ricki Hirner <hirner@bitfire.at>
2026-03-27 18:37:37 +0100

    Remove trailing comma test from LenientJsonTest
    * Remove test case for trailing commas in lenient JSON parsing

--- core/src/test/kotlin/at/bitfire/davdroid/network/LenientJsonTest.kt
+++ core/src/test/kotlin/at/bitfire/davdroid/network/LenientJsonTest.kt
@@ -17,16 +17,6 @@ class LenientJsonTest {
     )
 
     @Test
-    fun `lenientJson parses trailing comma`() {
-        // Test that lenient JSON can parse JSON with trailing commas
-        val jsonString = "{\"name\": \"test\", \"value\": 42,}"
-        
-        val result = lenientJson.decodeFromString<TestDataClass>(jsonString)
-        assertEquals("test", result.name)
-        assertEquals(42, result.value)
-    }
-
-    @Test
     fun `lenientJson parses unquoted keys`() {
         // Test that lenient JSON can parse JSON with unquoted keys
         val jsonString = "{name: \"test\", value: 42}"
