30cc36d3e7960cd1ff80271cb4bfeb3d6287f06b
cketti <ck@cketti.de>
2026-03-31 13:34:09 +0200

    Use `ICalendar.timezoneDefToTzId()` from synctools instead of `ICalendarUtils`

--- core/src/main/kotlin/at/bitfire/davdroid/db/Collection.kt
+++ core/src/main/kotlin/at/bitfire/davdroid/db/Collection.kt
@@ -28,8 +28,8 @@ import at.bitfire.dav4jvm.property.webdav.CurrentUserPrivilegeSet
 import at.bitfire.dav4jvm.property.webdav.DisplayName
 import at.bitfire.dav4jvm.property.webdav.ResourceType
 import at.bitfire.davdroid.util.DavUtils.lastSegment
-import at.bitfire.davdroid.util.ICalendarUtils
 import at.bitfire.davdroid.util.trimToNull
+import at.bitfire.ical4android.ICalendar
 import okhttp3.HttpUrl
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 
@@ -201,7 +201,7 @@ data class Collection(
                     dav[CalendarTimezoneId::class.java]?.let { timezoneId = it.identifier }
                     if (timezoneId == null)
                         dav[CalendarTimezone::class.java]?.vTimeZone?.let {
-                            timezoneId = ICalendarUtils.getVTimeZoneId(it)
+                            timezoneId = ICalendar.timezoneDefToTzId(it)
                         }
 
                     if (type == TYPE_CALENDAR) {
--- core/src/main/kotlin/at/bitfire/davdroid/db/migration/AutoMigration16.kt
+++ core/src/main/kotlin/at/bitfire/davdroid/db/migration/AutoMigration16.kt
@@ -8,7 +8,7 @@ import androidx.room.ProvidedAutoMigrationSpec
 import androidx.room.RenameColumn
 import androidx.room.migration.AutoMigrationSpec
 import androidx.sqlite.db.SupportSQLiteDatabase
-import at.bitfire.davdroid.util.ICalendarUtils
+import at.bitfire.ical4android.ICalendar
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -29,7 +29,7 @@ class AutoMigration16 @Inject constructor(): AutoMigrationSpec {
             while (cursor.moveToNext()) {
                 val id: Long = cursor.getLong(0)
                 val timezoneDef: String = cursor.getString(1) ?: continue
-                val timezoneId = ICalendarUtils.getVTimeZoneId(timezoneDef)
+                val timezoneId = ICalendar.timezoneDefToTzId(timezoneDef)
                 db.execSQL("UPDATE collection SET timezoneId=? WHERE id=?", arrayOf<Any?>(timezoneId, id))
             }
         }
