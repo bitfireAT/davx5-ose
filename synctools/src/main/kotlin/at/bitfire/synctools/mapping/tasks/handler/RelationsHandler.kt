bd21c17a39b32f5d2a322474fbbc2b517740c323
Arnau Mora Gras <arnyminerz@proton.me>
2026-05-27 13:01:23 +0200

    Add relations
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

---
+++ synctools/src/main/kotlin/at/bitfire/synctools/mapping/tasks/handler/RelationsHandler.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright © All Contributors. See LICENSE and AUTHORS in the root directory for details.
+ */
+
+package at.bitfire.synctools.mapping.tasks.handler
+
+import android.content.ContentValues
+import at.bitfire.ical4android.Task
+import net.fortuna.ical4j.model.parameter.RelType
+import net.fortuna.ical4j.model.property.RelatedTo
+import org.dmfs.tasks.contract.TaskContract.Property.Relation
+
+class RelationsHandler : DmfsTaskPropertyHandler {
+    override fun process(row: ContentValues, to: Task) {
+        val uid = row.getAsString(Relation.RELATED_UID)
+        if (uid == null) {
+            // logger.warning("Task relation doesn't refer to same task list; can't be synchronized")
+            return
+        }
+
+        to.relatedTo.add(
+            RelatedTo(uid)
+                // add relation type as reltypeparam
+                .add(
+                    when (row.getAsInteger(Relation.RELATED_TYPE)) {
+                        Relation.RELTYPE_CHILD ->
+                            RelType.CHILD
+                        Relation.RELTYPE_SIBLING ->
+                            RelType.SIBLING
+                        else /* Relation.RELTYPE_PARENT, default value */ ->
+                            RelType.PARENT
+                    }
+                )
+        )
+    }
+}
