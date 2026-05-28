2039c7734eea543339221cf67de7aadeff14e6e6
Ricki Hirner <hirner@bitfire.at>
2026-05-28 21:17:47 +0200

    Update KDoc

--- synctools/src/main/kotlin/at/bitfire/synctools/util/TemporalAdapterCompat.kt
+++ synctools/src/main/kotlin/at/bitfire/synctools/util/TemporalAdapterCompat.kt
@@ -12,7 +12,8 @@ object TemporalAdapterCompat {
     /**
      * Compatibility version of [net.fortuna.ical4j.model.TemporalAdapter.isBefore] that works around
      * https://github.com/ical4j/ical4j/issues/880.
-     * Safe to use with any Temporal types including Instant and ZonedDateTime.
+     *
+     * Safe to use with any Temporal type that is supported by [toInstant].
      */
     fun isBefore(a: Temporal, b: Temporal): Boolean {
         return a.toInstant() < b.toInstant()
@@ -21,7 +22,8 @@ object TemporalAdapterCompat {
     /**
      * Compatibility version of [net.fortuna.ical4j.model.TemporalAdapter.isAfter] that works around
      * https://github.com/ical4j/ical4j/issues/880.
-     * Safe to use with any Temporal types including Instant and ZonedDateTime.
+     *
+     * Safe to use with any Temporal type that is supported by [toInstant].
      */
     fun isAfter(a: Temporal, b: Temporal): Boolean {
         return a.toInstant() > b.toInstant()
