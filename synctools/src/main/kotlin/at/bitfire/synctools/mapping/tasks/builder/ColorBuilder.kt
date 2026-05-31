3b4b3d86f4c5069bd1919e914f1dd86de844b7ad
Arnau Mora Gras <arnyminerz@proton.me>
2026-05-31 10:38:33 +0200

    Re-implement old build
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- synctools/src/main/kotlin/at/bitfire/synctools/mapping/tasks/builder/ColorBuilder.kt
+++ synctools/src/main/kotlin/at/bitfire/synctools/mapping/tasks/builder/ColorBuilder.kt
@@ -4,13 +4,19 @@
 
 package at.bitfire.synctools.mapping.tasks.builder
 
+import android.content.Entity
 import at.bitfire.ical4android.Task
 import at.bitfire.synctools.icalendar.Css3Color
 import at.bitfire.synctools.icalendar.plusAssign
 import net.fortuna.ical4j.model.component.VToDo
 import net.fortuna.ical4j.model.property.Color
+import org.dmfs.tasks.contract.TaskContract.Tasks
 
-class ColorBuilder : DmfsTaskFieldBuilderVToDo {
+class ColorBuilder : DmfsTaskFieldBuilder, DmfsTaskFieldBuilderVToDo {
+
+    override fun build(from: Task, to: Entity) {
+        to.entityValues.put(Tasks.TASK_COLOR, from.color)
+    }
 
     override fun build(from: Task, to: VToDo) {
         to += Color(null, from.color?.let { Css3Color.nearestMatch(it).name })
