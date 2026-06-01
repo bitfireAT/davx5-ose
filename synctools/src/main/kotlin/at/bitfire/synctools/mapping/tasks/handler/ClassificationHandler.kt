a741f83a652f9c196e2fe3aac53920ab825acf9f
cketti <ck@cketti.de>
2026-06-01 15:23:05 +0200

    [DmfsTask] Make `ClassificationHandler` implement `DmfsTaskFieldHandler2`

--- synctools/src/main/kotlin/at/bitfire/synctools/mapping/tasks/handler/ClassificationHandler.kt
+++ synctools/src/main/kotlin/at/bitfire/synctools/mapping/tasks/handler/ClassificationHandler.kt
@@ -5,11 +5,15 @@
 package at.bitfire.synctools.mapping.tasks.handler
 
 import android.content.ContentValues
+import android.content.Entity
 import at.bitfire.ical4android.Task
+import at.bitfire.synctools.icalendar.plusAssign
+import net.fortuna.ical4j.model.component.VToDo
 import net.fortuna.ical4j.model.property.Clazz
+import net.fortuna.ical4j.model.property.immutable.ImmutableClazz
 import org.dmfs.tasks.contract.TaskContract.Tasks
 
-class ClassificationHandler : DmfsTaskFieldHandler {
+class ClassificationHandler : DmfsTaskFieldHandler, DmfsTaskFieldHandler2 {
 
     override fun process(from: ContentValues, to: Task) {
         to.classification = when (from.getAsInteger(Tasks.CLASSIFICATION)) {
@@ -20,4 +24,16 @@ class ClassificationHandler : DmfsTaskFieldHandler {
         }
     }
 
+    override fun process(from: Entity, main: Entity, to: VToDo) {
+        val classification = when (from.entityValues.getAsInteger(Tasks.CLASSIFICATION)) {
+            Tasks.CLASSIFICATION_PUBLIC -> ImmutableClazz.PUBLIC
+            Tasks.CLASSIFICATION_PRIVATE -> ImmutableClazz.PRIVATE
+            Tasks.CLASSIFICATION_CONFIDENTIAL -> ImmutableClazz.CONFIDENTIAL
+            else -> null
+        }
+
+        if (classification != null) {
+            to += classification
+        }
+    }
 }
--- synctools/src/test/kotlin/at/bitfire/synctools/mapping/tasks/handler/ClassificationHandlerTest.kt
+++ synctools/src/test/kotlin/at/bitfire/synctools/mapping/tasks/handler/ClassificationHandlerTest.kt
@@ -5,9 +5,12 @@
 package at.bitfire.synctools.mapping.tasks.handler
 
 import android.content.ContentValues
+import android.content.Entity
 import androidx.core.content.contentValuesOf
 import at.bitfire.ical4android.Task
+import net.fortuna.ical4j.model.component.VToDo
 import net.fortuna.ical4j.model.property.Clazz
+import net.fortuna.ical4j.model.property.immutable.ImmutableClazz
 import org.dmfs.tasks.contract.TaskContract.Tasks
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNull
@@ -21,38 +24,87 @@ class ClassificationHandlerTest {
     private val handler = ClassificationHandler()
 
     @Test
-    fun `No CLASSIFICATION`() {
+    fun `legacy No CLASSIFICATION`() {
         val task = Task()
         handler.process(ContentValues(), task)
         assertNull(task.classification)
     }
 
     @Test
-    fun `CLASSIFICATION_PUBLIC maps to CLASS PUBLIC`() {
+    fun `legacy CLASSIFICATION_PUBLIC maps to CLASS PUBLIC`() {
         val task = Task()
         handler.process(contentValuesOf(Tasks.CLASSIFICATION to Tasks.CLASSIFICATION_PUBLIC), task)
         assertEquals(Clazz(Clazz.VALUE_PUBLIC), task.classification)
     }
 
     @Test
-    fun `CLASSIFICATION_PRIVATE maps to CLASS PRIVATE`() {
+    fun `legacy CLASSIFICATION_PRIVATE maps to CLASS PRIVATE`() {
         val task = Task()
         handler.process(contentValuesOf(Tasks.CLASSIFICATION to Tasks.CLASSIFICATION_PRIVATE), task)
         assertEquals(Clazz(Clazz.VALUE_PRIVATE), task.classification)
     }
 
     @Test
-    fun `CLASSIFICATION_CONFIDENTIAL maps to CLASS CONFIDENTIAL`() {
+    fun `legacy CLASSIFICATION_CONFIDENTIAL maps to CLASS CONFIDENTIAL`() {
         val task = Task()
         handler.process(contentValuesOf(Tasks.CLASSIFICATION to Tasks.CLASSIFICATION_CONFIDENTIAL), task)
         assertEquals(Clazz(Clazz.VALUE_CONFIDENTIAL), task.classification)
     }
 
     @Test
-    fun `Unknown CLASSIFICATION maps to null`() {
+    fun `legacy Unknown CLASSIFICATION maps to null`() {
         val task = Task()
         handler.process(contentValuesOf(Tasks.CLASSIFICATION to 99), task)
         assertNull(task.classification)
     }
 
+    @Test
+    fun `No CLASSIFICATION`() {
+        val input = Entity(ContentValues())
+        val task = VToDo()
+
+        handler.process(from = input, main = input, to = task)
+
+        assertNull(task.classification)
+    }
+
+    @Test
+    fun `CLASSIFICATION_PUBLIC maps to CLASS PUBLIC`() {
+        val input = Entity(contentValuesOf(Tasks.CLASSIFICATION to Tasks.CLASSIFICATION_PUBLIC))
+        val task = VToDo()
+
+        handler.process(from = input, main = input, to = task)
+
+        assertEquals(ImmutableClazz.PUBLIC, task.classification)
+    }
+
+    @Test
+    fun `CLASSIFICATION_PRIVATE maps to CLASS PRIVATE`() {
+        val input = Entity(contentValuesOf(Tasks.CLASSIFICATION to Tasks.CLASSIFICATION_PRIVATE))
+        val task = VToDo()
+
+        handler.process(from = input, main = input, to = task)
+
+        assertEquals(ImmutableClazz.PRIVATE, task.classification)
+    }
+
+    @Test
+    fun `CLASSIFICATION_CONFIDENTIAL maps to CLASS CONFIDENTIAL`() {
+        val input = Entity(contentValuesOf(Tasks.CLASSIFICATION to Tasks.CLASSIFICATION_CONFIDENTIAL))
+        val task = VToDo()
+
+        handler.process(from = input, main = input, to = task)
+
+        assertEquals(ImmutableClazz.CONFIDENTIAL, task.classification)
+    }
+
+    @Test
+    fun `Unknown CLASSIFICATION maps to null`() {
+        val input = Entity(contentValuesOf(Tasks.CLASSIFICATION to 99))
+        val task = VToDo()
+
+        handler.process(from = input, main = input, to = task)
+
+        assertNull(task.classification)
+    }
 }
