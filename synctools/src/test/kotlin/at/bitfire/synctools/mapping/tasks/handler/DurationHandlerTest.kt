941b0d2334982c7254b34326eff56f9bc00ca772
cketti <ck@cketti.de>
2026-06-01 17:59:20 +0200

    [DmfsTask] Make `DurationHandler` implement `DmfsTaskFieldHandler2`

--- synctools/src/test/kotlin/at/bitfire/synctools/mapping/tasks/handler/DurationHandlerTest.kt
+++ synctools/src/test/kotlin/at/bitfire/synctools/mapping/tasks/handler/DurationHandlerTest.kt
@@ -5,9 +5,11 @@
 package at.bitfire.synctools.mapping.tasks.handler
 
 import android.content.ContentValues
+import android.content.Entity
 import androidx.core.content.contentValuesOf
 import at.bitfire.ical4android.Task
 import at.bitfire.synctools.util.AndroidTimeUtils
+import net.fortuna.ical4j.model.component.VToDo
 import net.fortuna.ical4j.model.property.Duration
 import org.dmfs.tasks.contract.TaskContract.Tasks
 import org.junit.Assert.assertEquals
@@ -22,24 +24,53 @@ class DurationHandlerTest {
     private val handler = DurationHandler()
 
     @Test
-    fun `No DURATION leaves duration null`() {
+    fun `legacy No DURATION leaves duration null`() {
         val task = Task()
         handler.process(ContentValues(), task)
         assertNull(task.duration)
     }
 
     @Test
-    fun `DURATION PT1H is mapped correctly`() {
+    fun `legacy DURATION PT1H is mapped correctly`() {
         val task = Task()
         handler.process(contentValuesOf(Tasks.DURATION to "PT1H"), task)
         assertEquals(Duration(AndroidTimeUtils.parseDuration("PT1H")), task.duration)
     }
 
     @Test
-    fun `DURATION P1D is mapped correctly`() {
+    fun `legacy DURATION P1D is mapped correctly`() {
         val task = Task()
         handler.process(contentValuesOf(Tasks.DURATION to "P1D"), task)
         assertEquals(Duration(AndroidTimeUtils.parseDuration("P1D")), task.duration)
     }
 
+    @Test
+    fun `No DURATION leaves duration null`() {
+        val input = Entity(ContentValues())
+        val task = VToDo()
+
+        handler.process(from = input, main = input, to = task)
+
+        assertNull(task.duration)
+    }
+
+    @Test
+    fun `DURATION PT1H is mapped correctly`() {
+        val input = Entity(contentValuesOf(Tasks.DURATION to "PT1H"))
+        val task = VToDo()
+
+        handler.process(from = input, main = input, to = task)
+
+        assertEquals(Duration("PT1H"), task.duration)
+    }
+
+    @Test
+    fun `DURATION P1D is mapped correctly`() {
+        val input = Entity(contentValuesOf(Tasks.DURATION to "P1D"))
+        val task = VToDo()
+
+        handler.process(from = input, main = input, to = task)
+
+        assertEquals(Duration("P1D"), task.duration)
+    }
 }
