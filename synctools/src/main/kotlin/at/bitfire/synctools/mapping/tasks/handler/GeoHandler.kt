6e7f451692efe7421121c7bba5d5cbf61c00c6fe
cketti <ck@cketti.de>
2026-06-02 12:02:14 +0200

    [DmfsTask] Make `GeoHandler` implement `DmfsTaskFieldHandler2`

--- synctools/src/main/kotlin/at/bitfire/synctools/mapping/tasks/handler/GeoHandler.kt
+++ synctools/src/main/kotlin/at/bitfire/synctools/mapping/tasks/handler/GeoHandler.kt
@@ -5,28 +5,52 @@
 package at.bitfire.synctools.mapping.tasks.handler
 
 import android.content.ContentValues
+import android.content.Entity
 import at.bitfire.ical4android.Task
+import at.bitfire.synctools.icalendar.plusAssign
+import net.fortuna.ical4j.model.component.VToDo
 import net.fortuna.ical4j.model.property.Geo
 import org.dmfs.tasks.contract.TaskContract.Tasks
 import java.util.logging.Level
 import java.util.logging.Logger
 
-class GeoHandler : DmfsTaskFieldHandler {
+class GeoHandler : DmfsTaskFieldHandler, DmfsTaskFieldHandler2 {
 
     private val logger
         get() = Logger.getLogger(javaClass.name)
 
     override fun process(from: ContentValues, to: Task) {
-        val geo = from.getAsString(Tasks.GEO) ?: return
-        val commaIdx = geo.indexOf(',')
-        if (commaIdx < 0) return
-        val lng = geo.substring(0, commaIdx)
-        val lat = geo.substring(commaIdx + 1)
-        try {
-            to.geoPosition = Geo(lat.toBigDecimal(), lng.toBigDecimal())
-        } catch (e: NumberFormatException) {
-            logger.log(Level.WARNING, "Invalid GEO value: $geo", e)
+        val geo = parseGeoString(from.getAsString(Tasks.GEO))
+        if (geo != null) {
+            to.geoPosition = geo
         }
     }
 
+    override fun process(from: Entity, main: Entity, to: VToDo) {
+        val geo = parseGeoString(from.entityValues.getAsString(Tasks.GEO))
+        if (geo != null) {
+            to += geo
+        }
+    }
+
+    private fun parseGeoString(geoString: String?): Geo? {
+        if (geoString == null) {
+            return null
+        }
+
+        val commaIdx = geoString.indexOf(',')
+        if (commaIdx < 0) {
+            return null
+        }
+
+        val longitude = geoString.substring(0, commaIdx)
+        val latitude = geoString.substring(commaIdx + 1)
+
+        return try {
+            Geo(latitude.toBigDecimal(), longitude.toBigDecimal())
+        } catch (e: NumberFormatException) {
+            logger.log(Level.WARNING, "Invalid GEO value: $geoString", e)
+            null
+        }
+    }
 }
