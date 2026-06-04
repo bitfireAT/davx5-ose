de3f0f18453097d322867453838449600bb0747f
Sunik Kupfer <kupfer@bitfire.at>
2026-06-04 11:13:43 +0200

    DmfsTask: Migrate unknown properties and URL handlers to VToDo
    Signed-off-by: Sunik Kupfer <kupfer@bitfire.at>

--- synctools/src/main/kotlin/at/bitfire/synctools/mapping/tasks/handler/UrlHandler.kt
+++ synctools/src/main/kotlin/at/bitfire/synctools/mapping/tasks/handler/UrlHandler.kt
@@ -5,13 +5,30 @@
 package at.bitfire.synctools.mapping.tasks.handler
 
 import android.content.ContentValues
+import android.content.Entity
 import at.bitfire.ical4android.Task
+import at.bitfire.synctools.icalendar.plusAssign
+import net.fortuna.ical4j.model.component.VToDo
+import net.fortuna.ical4j.model.property.Url
 import org.dmfs.tasks.contract.TaskContract.Tasks
+import java.net.URI
+import java.net.URISyntaxException
 
-class UrlHandler : DmfsTaskFieldHandler {
+class UrlHandler : DmfsTaskFieldHandler, DmfsTaskFieldHandler2 {
 
     override fun process(from: ContentValues, to: Task) {
         to.url = from.getAsString(Tasks.URL)
+    }
+
+    override fun process(from: Entity, main: Entity, to: VToDo) {
+        val url = from.entityValues.getAsString(Tasks.URL)
+        if (url != null) {
+            try {
+                to += Url(URI(url))
+            } catch (_: URISyntaxException) {
+                // Ignore invalid URLs
+            }
+        }
     }
 
 }
--- synctools/src/test/kotlin/at/bitfire/synctools/mapping/tasks/handler/UrlHandlerTest.kt
+++ synctools/src/test/kotlin/at/bitfire/synctools/mapping/tasks/handler/UrlHandlerTest.kt
@@ -5,14 +5,20 @@
 package at.bitfire.synctools.mapping.tasks.handler
 
 import android.content.ContentValues
+import android.content.Entity
 import androidx.core.content.contentValuesOf
 import at.bitfire.ical4android.Task
+import net.fortuna.ical4j.model.Property
+import net.fortuna.ical4j.model.component.VToDo
+import net.fortuna.ical4j.model.property.Url
 import org.dmfs.tasks.contract.TaskContract.Tasks
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNull
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import java.net.URI
+import kotlin.jvm.optionals.getOrNull
 
 @RunWith(RobolectricTestRunner::class)
 class UrlHandlerTest {
@@ -20,17 +26,38 @@ class UrlHandlerTest {
     private val handler = UrlHandler()
 
     @Test
-    fun `No URL`() {
+    fun `legacy No URL`() {
         val task = Task()
         handler.process(ContentValues(), task)
         assertNull(task.url)
     }
 
     @Test
-    fun `URL set`() {
+    fun `legacy URL set`() {
         val task = Task()
         handler.process(contentValuesOf(Tasks.URL to "https://example.com"), task)
         assertEquals("https://example.com", task.url)
+    }
+
+    @Test
+    fun `No URL`() {
+        val input = Entity(ContentValues())
+        val task = VToDo()
+
+        handler.process(from = input, main = input, to = task)
+
+        assertNull(task.getProperty<Url>(Property.URL).getOrNull())
+    }
+
+    @Test
+    fun `URL set`() {
+        val input = Entity(contentValuesOf(Tasks.URL to "https://example.com"))
+        val task = VToDo()
+
+        handler.process(from = input, main = input, to = task)
+
+        val actual = task.getProperty<Url>(Property.URL).getOrNull()?.uri
+        assertEquals(URI("https://example.com"), actual)
     }
 
 }
