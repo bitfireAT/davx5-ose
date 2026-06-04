0a0ed9547e076e3fb97df233c1942fa0261bbf14
Sunik Kupfer <kupfer@bitfire.at>
2026-06-04 14:11:34 +0200

    DmfsTask: Migrate title, UID, and status handlers to VToDo (#2345)
    * DmfsTask: Migrate title, UID, and status handlers to VToDo
    
    * Implement DmfsTaskFieldHandler2 for TitleHandler, UidHandler, and StatusHandler
    * Add VToDo-based tests and rename legacy tests
    * Use Entity and VToDo in new handler implementations
    
    Signed-off-by: Sunik Kupfer <kupfer@bitfire.at>
    
    * Use Status.VALUE_IN_PROCESS
    
    Signed-off-by: Sunik Kupfer <kupfer@bitfire.at>
    
    * Update StatusHandlerTest to assert against whole property
    
    Signed-off-by: Sunik Kupfer <kupfer@bitfire.at>
    
    ---------
    
    Signed-off-by: Sunik Kupfer <kupfer@bitfire.at>

--- synctools/src/test/kotlin/at/bitfire/synctools/mapping/tasks/handler/UidHandlerTest.kt
+++ synctools/src/test/kotlin/at/bitfire/synctools/mapping/tasks/handler/UidHandlerTest.kt
@@ -5,14 +5,17 @@
 package at.bitfire.synctools.mapping.tasks.handler
 
 import android.content.ContentValues
+import android.content.Entity
 import androidx.core.content.contentValuesOf
 import at.bitfire.ical4android.Task
+import net.fortuna.ical4j.model.component.VToDo
 import org.dmfs.tasks.contract.TaskContract.Tasks
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNull
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import kotlin.jvm.optionals.getOrNull
 
 @RunWith(RobolectricTestRunner::class)
 class UidHandlerTest {
@@ -20,17 +23,37 @@ class UidHandlerTest {
     private val handler = UidHandler()
 
     @Test
-    fun `No UID`() {
+    fun `legacy No UID`() {
         val task = Task()
         handler.process(ContentValues(), task)
         assertNull(task.uid)
     }
 
     @Test
-    fun `UID set`() {
+    fun `legacy UID set`() {
         val task = Task()
         handler.process(contentValuesOf(Tasks._UID to "test-uid-123"), task)
         assertEquals("test-uid-123", task.uid)
+    }
+
+    @Test
+    fun `No UID`() {
+        val input = Entity(ContentValues())
+        val task = VToDo()
+
+        handler.process(from = input, main = input, to = task)
+
+        assertNull(task.uid.getOrNull())
+    }
+
+    @Test
+    fun `UID set`() {
+        val input = Entity(contentValuesOf(Tasks._UID to "test-uid-123"))
+        val task = VToDo()
+
+        handler.process(from = input, main = input, to = task)
+
+        assertEquals("test-uid-123", task.uid.getOrNull()?.value)
     }
 
 }
