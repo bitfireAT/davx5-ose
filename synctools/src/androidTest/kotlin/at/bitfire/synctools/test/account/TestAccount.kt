979f351a16cee44afb4de1d65d946035b45806a4
Ricki Hirner <hirner@bitfire.at>
2026-06-08 22:43:18 +0200

    Set account visibility for all task providers on API 26+

--- synctools/src/androidTest/kotlin/at/bitfire/synctools/test/account/TestAccount.kt
+++ synctools/src/androidTest/kotlin/at/bitfire/synctools/test/account/TestAccount.kt
@@ -6,7 +6,9 @@ package at.bitfire.synctools.test.account
 
 import android.accounts.Account
 import android.accounts.AccountManager
+import android.os.Build
 import androidx.test.platform.app.InstrumentationRegistry
+import at.bitfire.ical4android.TaskProvider
 import at.bitfire.synctools.test.R
 import at.bitfire.synctools.util.AndroidAccountUtils
 import org.junit.Assert.assertTrue
@@ -22,10 +24,20 @@ object TestAccount {
      * This is required for tests that need non-local dmfs task-provider behavior:
      * local accounts are deleted immediately, while synthetic non-local accounts are purged as
      * stale because the provider can't find them in [AccountManager].
+     *
+     * The account is made visible ([AccountManager.setAccountVisibility]) to all known [TaskProvider]s.
      */
     fun create(accountName: String = "Synctools Test Account"): Account {
         val account = Account(accountName, accountType)
         assertTrue(AndroidAccountUtils.createAccount(context, account, emptyMap()))
+
+        if (Build.VERSION.SDK_INT >= 26) {
+            val manager = AccountManager.get(context)
+            for ((providerName, _) in listOf(TaskProvider.ProviderName.entries))
+                if (manager.getAccountVisibility(account, providerName.packageName) != AccountManager.VISIBILITY_VISIBLE)
+                    manager.setAccountVisibility(account, providerName.packageName, AccountManager.VISIBILITY_VISIBLE)
+        }
+
         return account
     }
 
