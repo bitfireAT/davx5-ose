07338329e0f30440d0883c4b302e7722435c2ef4
Sunik Kupfer <kupfer@bitfire.at>
2026-06-10 13:16:35 +0200

    Add PriorityBuilder
    Signed-off-by: Sunik Kupfer <kupfer@bitfire.at>
    
    # Conflicts:
    #	synctools/src/main/kotlin/at/bitfire/synctools/mapping/jtx/JtxObjectBuilder.kt
    #	synctools/src/test/kotlin/at/bitfire/synctools/mapping/jtx/JtxObjectBuilderTest.kt

---
+++ synctools/src/main/kotlin/at/bitfire/synctools/mapping/jtx/builder/PriorityBuilder.kt
@@ -0,0 +1,18 @@
+/*
+ * Copyright © All Contributors. See LICENSE and AUTHORS in the root directory for details.
+ */
+
+package at.bitfire.synctools.mapping.jtx.builder
+
+import android.content.Entity
+import at.techbee.jtx.JtxContract
+import net.fortuna.ical4j.model.component.CalendarComponent
+import net.fortuna.ical4j.model.property.Priority
+import kotlin.jvm.optionals.getOrNull
+
+class PriorityBuilder : JtxObjectEntityBuilder {
+    override fun build(from: CalendarComponent, main: CalendarComponent, to: Entity) {
+        val priority = main.getProperty<Priority>(Priority.PRIORITY).getOrNull()?.level
+        to.entityValues.put(JtxContract.JtxICalObject.PRIORITY, priority)
+    }
+}
