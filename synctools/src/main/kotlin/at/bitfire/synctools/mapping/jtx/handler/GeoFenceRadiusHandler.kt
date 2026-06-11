58254622b218d94c099607d37c7151db432ee83b
Arnau Mora Gras <arnyminerz@proton.me>
2026-06-11 18:06:54 +0200

    [jtx rewrite] Implement basic info handlers
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

---
+++ synctools/src/main/kotlin/at/bitfire/synctools/mapping/jtx/handler/GeoFenceRadiusHandler.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright © All Contributors. See LICENSE and AUTHORS in the root directory for details.
+ */
+
+package at.bitfire.synctools.mapping.jtx.handler
+
+import android.content.Entity
+import at.bitfire.synctools.icalendar.plusAssign
+import at.techbee.jtx.JtxContract
+import net.fortuna.ical4j.model.component.CalendarComponent
+import net.fortuna.ical4j.model.property.XProperty
+
+class GeoFenceRadiusHandler : JtxObjectEntityHandler {
+    override fun process(from: Entity, main: Entity, to: CalendarComponent) {
+        from.entityValues.getAsInteger(JtxContract.JtxICalObject.GEOFENCE_RADIUS)?.let { radius ->
+            to += XProperty(radius.toString())
+        }
+    }
+}
---
+++ synctools/src/main/kotlin/at/bitfire/synctools/mapping/jtx/handler/UrlHandler.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright © All Contributors. See LICENSE and AUTHORS in the root directory for details.
+ */
+
+package at.bitfire.synctools.mapping.jtx.handler
+
+import android.content.Entity
+import at.bitfire.synctools.exception.InvalidLocalResourceException
+import at.bitfire.synctools.icalendar.plusAssign
+import at.techbee.jtx.JtxContract
+import net.fortuna.ical4j.model.component.CalendarComponent
+import net.fortuna.ical4j.model.property.Url
+import java.net.URI
+import java.net.URISyntaxException
+
+class UrlHandler : JtxObjectEntityHandler {
+    override fun process(from: Entity, main: Entity, to: CalendarComponent) {
+        from.entityValues.getAsString(JtxContract.JtxICalObject.URL)?.let { url ->
+            try {
+                to += Url(URI(url))
+            } catch (e: URISyntaxException) {
+                throw InvalidLocalResourceException("Got an invalid URL: $url", e)
+            }
+        }
+    }
+}
