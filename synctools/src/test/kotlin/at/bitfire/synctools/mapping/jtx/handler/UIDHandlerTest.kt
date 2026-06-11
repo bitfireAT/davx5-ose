33afc30d2d0924cd69e8af20e2baaabcacefbfb4
Arnau Mora Gras <arnyminerz@proton.me>
2026-06-11 13:51:45 +0200

    [jtx rewrite] Add handlers for `Sequence` and `UID`
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

---
+++ synctools/src/test/kotlin/at/bitfire/synctools/mapping/jtx/handler/UIDHandlerTest.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright © All Contributors. See LICENSE and AUTHORS in the root directory for details.
+ */
+
+package at.bitfire.synctools.mapping.jtx.handler
+
+import android.content.ContentValues
+import android.content.Entity
+import androidx.core.content.contentValuesOf
+import at.techbee.jtx.JtxContract
+import net.fortuna.ical4j.model.component.VToDo
+import net.fortuna.ical4j.model.property.Uid
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import kotlin.jvm.optionals.getOrNull
+
+@RunWith(RobolectricTestRunner::class)
+class UIDHandlerTest {
+
+    private val handler = UIDHandler()
+
+    @Test
+    fun `No UID`() {
+        val input = Entity(ContentValues())
+        val output = VToDo()
+
+        handler.process(from = input, main = input, to = output)
+
+        assertNull(output.uid.getOrNull())
+    }
+
+    @Test
+    fun `UID with value`() {
+        val input = Entity(contentValuesOf(JtxContract.JtxICalObject.UID to "test-uid-123"))
+        val output = VToDo()
+
+        handler.process(from = input, main = input, to = output)
+
+        assertEquals(Uid("test-uid-123"), output.uid.getOrNull())
+    }
+}
