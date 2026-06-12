b2f15d54d55a44402aed7469dfb43d1a52d4735d
Sunik Kupfer <kupfer@bitfire.at>
2026-06-12 10:10:12 +0200

    Have SequenceBuilder use Long type for sequence number
    Signed-off-by: Sunik Kupfer <kupfer@bitfire.at>

--- synctools/src/main/kotlin/at/bitfire/synctools/mapping/jtx/builder/SequenceBuilder.kt
+++ synctools/src/main/kotlin/at/bitfire/synctools/mapping/jtx/builder/SequenceBuilder.kt
@@ -15,7 +15,7 @@ class SequenceBuilder : JtxObjectEntityBuilder {
     override fun build(from: CalendarComponent, main: CalendarComponent, to: Entity) {
         /* When we build the SEQUENCE column from a real object, we set the sequence to 0 (not null), so that we
         can distinguish it from objects which have been created locally and have never been uploaded yet. */
-        val sequence = from.getProperty<Sequence>(Property.SEQUENCE).getOrNull()?.sequenceNo ?: 0
+        val sequence = from.getProperty<Sequence>(Property.SEQUENCE).getOrNull()?.sequenceNo?.toLong() ?: 0L
         to.entityValues.put(JtxContract.JtxICalObject.SEQUENCE, sequence)
     }
 }
