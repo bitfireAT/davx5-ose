1749a65153e2adde2e11db45b05ed10955f40501
Arnau Mora <arnyminerz@proton.me>
2026-06-16 18:29:44 +0200

    Convert to instant and let ical4j handle the conversion
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- synctools/src/main/kotlin/at/bitfire/synctools/mapping/jtx/handler/CompletedHandler.kt
+++ synctools/src/main/kotlin/at/bitfire/synctools/mapping/jtx/handler/CompletedHandler.kt
@@ -8,7 +8,6 @@ import android.content.Entity
 import at.bitfire.ical4android.JtxICalObject
 import at.bitfire.synctools.icalendar.plusAssign
 import at.techbee.jtx.JtxContract
-import net.fortuna.ical4j.model.ParameterList
 import net.fortuna.ical4j.model.component.CalendarComponent
 import net.fortuna.ical4j.model.component.VToDo
 import net.fortuna.ical4j.model.property.Completed
@@ -18,7 +17,6 @@ import java.time.Instant
 import java.time.LocalDate
 import java.time.ZoneId
 import java.time.ZoneOffset
-import java.time.format.DateTimeFormatter
 
 class CompletedHandler : JtxObjectEntityHandler {
     override fun process(from: Entity, main: Entity, to: CalendarComponent) {
@@ -30,8 +28,8 @@ class CompletedHandler : JtxObjectEntityHandler {
 
         if (timezone == JtxContract.JtxICalObject.TZ_ALLDAY) {
             // All-day completion: no time of day, COMPLETED is stored as midnight UTC
-            val date = LocalDate.ofInstant(instant, ZoneOffset.UTC)
-            to += Completed(ParameterList(emptyList()), DateTimeFormatter.BASIC_ISO_DATE.format(date))
+            val date = LocalDate.ofInstant(instant, ZoneOffset.UTC).atStartOfDay().toInstant(ZoneOffset.UTC)
+            to += Completed(date)
             to += XProperty(JtxICalObject.X_PROP_COMPLETEDTIMEZONE, timezone)
             return
         }
