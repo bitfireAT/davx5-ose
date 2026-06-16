0c234ca9a2323b4f1e5f76343eff8953777e0064
Arnau Mora <arnyminerz@proton.me>
2026-06-16 16:00:58 +0200

    Add test for `VJOURNAL`
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- synctools/src/test/kotlin/at/bitfire/synctools/mapping/jtx/handler/PriorityHandlerTest.kt
+++ synctools/src/test/kotlin/at/bitfire/synctools/mapping/jtx/handler/PriorityHandlerTest.kt
@@ -9,6 +9,7 @@ import android.content.Entity
 import androidx.core.content.contentValuesOf
 import at.techbee.jtx.JtxContract
 import net.fortuna.ical4j.model.Property
+import net.fortuna.ical4j.model.component.VJournal
 import net.fortuna.ical4j.model.component.VToDo
 import net.fortuna.ical4j.model.property.Priority
 import org.junit.Assert.assertEquals
@@ -50,6 +51,16 @@ class PriorityHandlerTest {
     fun `PRIORITY with invalid value`() {
         val input = Entity(contentValuesOf(JtxContract.JtxICalObject.PRIORITY to "a"))
         val output = VToDo()
+
+        handler.process(from = input, main = input, to = output)
+
+        assertNull(output.getProperty<Priority>(Property.PRIORITY).getOrNull())
+    }
+
+    @Test
+    fun `PRIORITY is never added to VJOURNAL`() {
+        val input = Entity(contentValuesOf(JtxContract.JtxICalObject.PRIORITY to 1))
+        val output = VJournal()
 
         handler.process(from = input, main = input, to = output)
 
