d4fb9dc71cb66899b0454643a05d79c9679ef30e
Arnau Mora <arnyminerz@proton.me>
2026-06-16 15:58:59 +0200

    Add more tests for percent complete
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- synctools/src/test/kotlin/at/bitfire/synctools/mapping/jtx/handler/PercentCompleteHandlerTest.kt
+++ synctools/src/test/kotlin/at/bitfire/synctools/mapping/jtx/handler/PercentCompleteHandlerTest.kt
@@ -9,6 +9,7 @@ import android.content.Entity
 import androidx.core.content.contentValuesOf
 import at.techbee.jtx.JtxContract
 import net.fortuna.ical4j.model.Property
+import net.fortuna.ical4j.model.component.VJournal
 import net.fortuna.ical4j.model.component.VToDo
 import net.fortuna.ical4j.model.property.PercentComplete
 import org.junit.Assert.assertEquals
@@ -50,6 +51,16 @@ class PercentCompleteHandlerTest {
     fun `PERCENT-COMPLETE with invalid value`() {
         val input = Entity(contentValuesOf(JtxContract.JtxICalObject.PERCENT to "a"))
         val output = VToDo()
+
+        handler.process(from = input, main = input, to = output)
+
+        assertNull(output.getProperty<PercentComplete>(Property.PERCENT_COMPLETE).getOrNull())
+    }
+
+    @Test
+    fun `PERCENT-COMPLETE is never added to VJOURNAL`() {
+        val input = Entity(contentValuesOf(JtxContract.JtxICalObject.PERCENT to 42))
+        val output = VJournal()
 
         handler.process(from = input, main = input, to = output)
 
