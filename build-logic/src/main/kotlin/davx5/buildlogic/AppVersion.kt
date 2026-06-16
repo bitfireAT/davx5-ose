8659a87d69ae6341eea71d146f3455943cde3a65
Ricki Hirner <hirner@bitfire.at>
2026-06-16 10:24:04 +0200

    Update app version to 4.5.15-rc.1

--- build-logic/src/main/kotlin/davx5/buildlogic/AppVersion.kt
+++ build-logic/src/main/kotlin/davx5/buildlogic/AppVersion.kt
@@ -23,7 +23,7 @@ package davx5.buildlogic
  */
 object AppVersion {
 
-    const val CODE: Int = 405150000
-    const val NAME: String = "4.5.15-beta.1"
+    const val CODE: Int = 405150001
+    const val NAME: String = "4.5.15-rc.1"
 
 }
