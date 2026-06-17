bb3289048227643e5b5aa3bb2da2e1e61df40941
Sunik Kupfer <kupfer@bitfire.at>
2026-06-17 11:29:30 +0200

    Refactor TimeFieldsHandler to use JtxTimeField

--- synctools/src/main/kotlin/at/bitfire/synctools/mapping/jtx/handler/TimeFieldsHandler.kt
+++ synctools/src/main/kotlin/at/bitfire/synctools/mapping/jtx/handler/TimeFieldsHandler.kt
@@ -16,21 +16,12 @@ import net.fortuna.ical4j.model.property.DtEnd
 import net.fortuna.ical4j.model.property.DtStart
 import net.fortuna.ical4j.model.property.Due
 import net.fortuna.ical4j.model.property.Duration
-import java.time.DateTimeException
-import java.time.Instant
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.ZoneId
-import java.time.ZoneOffset
-import java.time.ZonedDateTime
 import java.time.temporal.Temporal
-import java.util.logging.Logger
 
 /**
  * Handles the iCalendar properties: DTSTART, DTEND, DUE, DURATION
  */
 class TimeFieldsHandler : JtxObjectEntityHandler {
-    private val logger = Logger.getLogger(javaClass.name)
 
     override fun process(from: Entity, main: Entity, to: CalendarComponent) {
         appendDateField(from, to, JtxContract.JtxICalObject.DTSTART, JtxContract.JtxICalObject.DTSTART_TIMEZONE) { DtStart(it) }
@@ -56,20 +47,10 @@ class TimeFieldsHandler : JtxObjectEntityHandler {
 
     private fun appendDateField(from: Entity, to: CalendarComponent, property: String, timezoneProperty: String, builder: (Temporal) -> Property) {
         val epochMillis = from.entityValues.getAsLong(property) ?: return
-        val instant = Instant.ofEpochMilli(epochMillis)
-        val timezone = from.entityValues.getAsString(timezoneProperty)
-
-        val temporal: Temporal = when {
-            timezone == JtxContract.JtxICalObject.TZ_ALLDAY -> LocalDate.ofInstant(instant, ZoneOffset.UTC)
-            timezone == ZoneOffset.UTC.id || timezone == "UTC" -> instant
-            timezone.isNullOrEmpty() -> LocalDateTime.ofInstant(instant, ZoneId.systemDefault())
-            else -> try {
-                ZonedDateTime.ofInstant(instant, ZoneId.of(timezone))
-            } catch (e: DateTimeException) {
-                logger.warning("Invalid timezone '$timezone', interpreting as UTC. Error: $e")
-                instant
-            }
-        }
+        val temporal = JtxTimeField(
+            timestamp = epochMillis,
+            timeZone = from.entityValues.getAsString(timezoneProperty)
+        ).toTemporal()
 
         to += builder(temporal)
     }
