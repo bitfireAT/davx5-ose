6a7d0b1718972821371c58b76e63ee7f32c0e009
cketti <ck@cketti.de>
2026-06-18 11:45:30 +0200

    Run garbage collection when ical4j's `ZoneIdPool` runs empty (#2526)

--- synctools/src/main/kotlin/at/bitfire/synctools/icalendar/TimeZoneRegistryFactoryWorkaround.kt
+++ synctools/src/main/kotlin/at/bitfire/synctools/icalendar/TimeZoneRegistryFactoryWorkaround.kt
@@ -8,6 +8,7 @@ import net.fortuna.ical4j.model.DefaultTimeZoneRegistryFactory
 import net.fortuna.ical4j.model.TimeZone
 import net.fortuna.ical4j.model.TimeZoneRegistry
 import net.fortuna.ical4j.model.TimeZoneRegistryFactory
+import net.fortuna.ical4j.model.ZoneRulesProviderImpl
 import net.fortuna.ical4j.model.component.Observance
 import java.util.logging.Logger
 
@@ -18,7 +19,9 @@ class TimeZoneRegistryFactoryWorkaround : TimeZoneRegistryFactory() {
 }
 
 /**
- * Work around https://github.com/bitfireAT/davx5-ose/issues/2248
+ * Work around
+ * - https://github.com/bitfireAT/davx5-ose/issues/2248
+ * - https://github.com/bitfireAT/davx5/issues/914
  */
 class TimeZoneRegistryWorkaround(
     private val timeZoneRegistry: TimeZoneRegistry = DefaultTimeZoneRegistryFactory().createRegistry()
@@ -39,8 +42,15 @@ class TimeZoneRegistryWorkaround(
 
         if (standardObservances.isEmpty() && daylightObservances.isEmpty()) {
             logger.info("Ignoring invalid VTIMEZONE with TZID: ${timeZone.id}")
-        } else {
-            timeZoneRegistry.register(timeZone, update)
+            return
         }
+
+        if (ZoneRulesProviderImpl.INSTANCE.zoneIdPool.availableZoneIds() == 0) {
+            // Trigger garbage collection in the hope that old TimeZoneRegistry instances will be freed and in turn
+            // ZoneIdPool.cleanup() will be able to reclaim zone IDs that are no longer used.
+            Runtime.getRuntime().gc()
+        }
+
+        timeZoneRegistry.register(timeZone, update)
     }
 }
