8c117d6f632b92e1267706cc7fec9086f9f2bf2f
Arnau Mora Gras <arnyminerz@proton.me>
2026-06-24 11:03:13 +0200

    Remove deprecated functions
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- core/src/main/kotlin/at/bitfire/davdroid/db/Principal.kt
+++ core/src/main/kotlin/at/bitfire/davdroid/db/Principal.kt
@@ -8,18 +8,13 @@ import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.Index
 import androidx.room.PrimaryKey
-import at.bitfire.dav4jvm.HttpUtils.toHttpUrl
-import at.bitfire.dav4jvm.HttpUtils.toKtorUrl
 import at.bitfire.dav4jvm.ktor.Response
 import at.bitfire.dav4jvm.ktor.omitTrailingSlash
-import at.bitfire.dav4jvm.okhttp.UrlUtils
 import at.bitfire.dav4jvm.property.webdav.DisplayName
 import at.bitfire.dav4jvm.property.webdav.ResourceType
 import at.bitfire.dav4jvm.property.webdav.WebDAV
 import at.bitfire.synctools.util.trimToNull
 import io.ktor.http.Url
-import okhttp3.HttpUrl
-import at.bitfire.dav4jvm.okhttp.Response as OkHttpResponse
 
 /**
  * A principal entity representing a WebDAV principal (rfc3744).
@@ -49,29 +44,6 @@ data class Principal(
          * @param dav WebDAV response (make sure that you have queried `DAV:resource-type` and `DAV:display-name`)
          * @return generated principal data object (with `id`=0), `null` if the response doesn't represent a principal
          */
-        @Deprecated("Use Ktor overload")
-        fun fromDavResponse(serviceId: Long, dav: OkHttpResponse): Principal? {
-            // Check if response is a principal
-            val resourceType = dav[ResourceType::class.java] ?: return null
-            if (!resourceType.types.contains(WebDAV.Principal))
-                return null
-
-            // Try getting the display name of the principal
-            val displayName: String? = dav[DisplayName::class.java]?.displayName.trimToNull()
-
-            // Create and return principal - even without it's display name
-            return Principal(
-                serviceId = serviceId,
-                url = UrlUtils.omitTrailingSlash(dav.href).toKtorUrl(),
-                displayName = displayName
-            )
-        }
-
-        /**
-         * Generates a principal entity from a WebDAV response.
-         * @param dav WebDAV response (make sure that you have queried `DAV:resource-type` and `DAV:display-name`)
-         * @return generated principal data object (with `id`=0), `null` if the response doesn't represent a principal
-         */
         fun fromDavResponse(serviceId: Long, dav: Response): Principal? {
             // Check if response is a principal
             val resourceType = dav[ResourceType::class.java] ?: return null
@@ -88,12 +60,6 @@ data class Principal(
                 displayName = displayName
             )
         }
-
-        @Deprecated("Use Ktor overload")
-        fun fromServiceAndUrl(service: Service, url: HttpUrl) = Principal(
-            serviceId = service.id,
-            url = UrlUtils.omitTrailingSlash(url).toKtorUrl()
-        )
 
         fun fromServiceAndUrl(service: Service, url: Url) = Principal(
             serviceId = service.id,
