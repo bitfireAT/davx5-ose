f039f02d980f60d7a18e5ff43097b3582d6ed47d
Arnau Mora Gras <arnyminerz@proton.me>
2026-07-27 09:02:47 +0200

    Not necessary to use Singleton anymore since application scope is now managed by Hilt.
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- core/src/main/kotlin/at/bitfire/davdroid/webdav/operation/QueryChildDocumentsOperation.kt
+++ core/src/main/kotlin/at/bitfire/davdroid/webdav/operation/QueryChildDocumentsOperation.kt
@@ -40,13 +40,11 @@ import java.util.concurrent.ConcurrentHashMap
 import java.util.logging.Level
 import java.util.logging.Logger
 import javax.inject.Inject
-import javax.inject.Singleton
 
 /**
  * Singleton so that [applicationScope] (and the running-query bookkeeping it launches into) has a
  * single, well-defined lifetime instead of a new, uncancellable scope per Hilt injection.
  */
-@Singleton
 class QueryChildDocumentsOperation @Inject constructor(
     @ApplicationContext private val context: Context,
     private val db: AppDatabase,
