3e97c9fc46a45eeece47c3e14d0de94d57309ba1
Ricki Hirner <hirner@bitfire.at>
2026-07-27 16:39:59 +0200

    Update KNOWN_ALARM_PROPERTIES comment

--- synctools/src/main/kotlin/at/bitfire/synctools/mapping/jtx/AlarmProperties.kt
+++ synctools/src/main/kotlin/at/bitfire/synctools/mapping/jtx/AlarmProperties.kt
@@ -9,10 +9,9 @@ import net.fortuna.ical4j.model.Property
 /**
  * iCalendar `VALARM` property names that are mapped to dedicated `JtxContract.JtxAlarm` columns.
  *
- * Used to keep [at.bitfire.synctools.mapping.jtx.builder.RemindersBuilder] (which must strip these
- * out of `JtxContract.JtxAlarm.OTHER` when storing an alarm) and
- * [at.bitfire.synctools.mapping.jtx.handler.RemindersHandler] (which must not re-add them from
- * `JtxContract.JtxAlarm.OTHER`) in sync.
+ * Used to keep [at.bitfire.synctools.mapping.jtx.builder.RemindersBuilder] and
+ * [at.bitfire.synctools.mapping.jtx.handler.RemindersHandler] in sync, which must not process these
+ * properties when found in "other" properties.
  */
 val KNOWN_ALARM_PROPERTIES: Set<String> = setOf(
     Property.ACTION,
