2fcae343335e01b2f8feac184bd96b30a7b9be61
Ricki Hirner <hirner@bitfire.at>
2026-07-27 13:36:08 +0200

    Add test for KTOR-9722 deadlock; verify IoCoroutineWorker necessity

--- core/src/androidTest/kotlin/at/bitfire/davdroid/IoCoroutineWorkerTest.kt
+++ core/src/androidTest/kotlin/at/bitfire/davdroid/IoCoroutineWorkerTest.kt
@@ -14,15 +14,25 @@ import dagger.assisted.AssistedInject
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import io.ktor.client.plugins.auth.providers.DigestAuthCredentials
+import io.ktor.client.plugins.auth.providers.DigestAuthProvider
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.currentCoroutineContext
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runInterruptible
 import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import java.util.concurrent.CountDownLatch
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import javax.inject.Inject
@@ -67,6 +77,43 @@ class IoCoroutineWorkerTest {
         worker.doWork()
 
         assertEquals(testIoDispatcher, worker.actualDispatcher)
+    }
+
+    @Test
+    @OptIn(DelicateCoroutinesApi::class)
+    fun testStillRequired() {
+        // tests whether https://youtrack.jetbrains.com/issue/KTOR-9722 is still present
+
+        // congest default dispatcher
+        val processors = Runtime.getRuntime().availableProcessors()
+        val allCongested = CountDownLatch(processors)
+        val scope = CoroutineScope(Dispatchers.Default)
+        repeat(processors) {
+            scope.launch {
+                allCongested.countDown()
+                runInterruptible {
+                    Thread.sleep(10_000)
+                }
+            }
+        }
+        allCongested.await()
+
+        // now construct DigestAuthProvider and verify deadlock
+        val thread = Thread {
+            try {
+                DigestAuthProvider(credentials = { DigestAuthCredentials("user", "pass") })
+            } catch (_: InterruptedException) {
+                // thrown when thread.interrupt() is called below
+            }
+        }
+        try {
+            thread.start()
+            thread.join(1_000)
+            assertTrue("KTOR issue 9722 not present anymore, remove IoCoroutineWorker + tests", thread.isAlive)
+        } finally {
+            thread.interrupt()
+            scope.cancel()
+        }
     }
 
 
