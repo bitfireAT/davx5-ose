cbdeb1b65d2a451f9ffd95a8bec2b0325cbb9690
Arnau Mora <arnyminerz@proton.me>
2026-08-04 18:50:52 +0200

    Add AccountSettingsStore and initial implementations
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

---
+++ core/src/main/kotlin/at/bitfire/davdroid/settings/AccountSettingsStore.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright © All Contributors. See LICENSE and AUTHORS in the root directory for details.
+ */
+
+package at.bitfire.davdroid.settings
+
+import at.bitfire.synctools.util.SensitiveString
+
+sealed interface AccountSettingsStore {
+    /**
+     * Gets the value stored at [key].
+     */
+    fun getValue(key: String): String?
+
+    /**
+     * Stores [value] at [key].
+     */
+    fun putValue(key: String, value: String?)
+
+    /**
+     * Gets the value stored at [key] as a [SensitiveString].
+     */
+    fun getSensitiveValue(key: String): SensitiveString?
+
+    /**
+     * Stores [value] at [key].
+     *
+     * May be stored in a different location than [putValue], so keys stored using [putSensitiveValue] must only be
+     * retrieved using [getSensitiveValue].
+     */
+    fun putSensitiveValue(key: String, value: SensitiveString?)
+}
---
+++ core/src/main/kotlin/at/bitfire/davdroid/settings/DbAccountSettingsStore.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright © All Contributors. See LICENSE and AUTHORS in the root directory for details.
+ */
+
+package at.bitfire.davdroid.settings
+
+import at.bitfire.davdroid.accounts.AccountId
+import at.bitfire.davdroid.db.AppDatabase
+import at.bitfire.synctools.util.SensitiveString
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import jakarta.inject.Inject
+
+class DbAccountSettingsStore @Inject constructor(@Assisted private val account: AccountId, db: AppDatabase) : AccountSettingsStore {
+    private val dao = db.dbAccountDao()
+
+    @AssistedFactory
+    interface Factory {
+        fun create(account: AccountId): DbAccountSettingsStore
+    }
+
+    override fun getValue(key: String): String? {
+        throw NotImplementedError("The database DAO has not been implemented yet.")
+    }
+
+    override fun putValue(key: String, value: String?) {
+        throw NotImplementedError("The database DAO has not been implemented yet.")
+    }
+
+    override fun getSensitiveValue(key: String): SensitiveString? {
+        throw NotImplementedError("The database DAO has not been implemented yet.")
+    }
+
+    override fun putSensitiveValue(key: String, value: SensitiveString?) {
+        throw NotImplementedError("The database DAO has not been implemented yet.")
+    }
+}
