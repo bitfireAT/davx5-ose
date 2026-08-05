3cc8038637626f7447bc76a8474152dd60e7ebf3
Ricki Hirner <hirner@bitfire.at>
2026-08-05 17:52:22 +0200

    Update KDoc for WebDavCollection

--- core/src/main/kotlin/at/bitfire/davdroid/resource/remote/WebDavCollection.kt
+++ core/src/main/kotlin/at/bitfire/davdroid/resource/remote/WebDavCollection.kt
@@ -8,6 +8,9 @@ import at.bitfire.dav4jvm.ktor.DavCollection
 
 /**
  * Provides remote collection access as required for [at.bitfire.davdroid.sync.SyncManager].
+ *
+ * Currently, call sites just call through [davCollection], but the goal is to provide all remote collection
+ * operations that are required for synchronization here.
  */
 interface WebDavCollection {
 
