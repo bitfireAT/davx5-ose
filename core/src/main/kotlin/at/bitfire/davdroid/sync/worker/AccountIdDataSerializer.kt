1fefddf02c8d34829934383898c34a01ef0b0a18
Arnau Mora <arnyminerz@proton.me>
2026-08-13 11:18:05 +0200

    Add missing implementation
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- core/src/main/kotlin/at/bitfire/davdroid/sync/worker/AccountIdDataSerializer.kt
+++ core/src/main/kotlin/at/bitfire/davdroid/sync/worker/AccountIdDataSerializer.kt
@@ -7,8 +7,10 @@ package at.bitfire.davdroid.sync.worker
 import android.accounts.Account
 import androidx.work.Data
 import at.bitfire.davdroid.accounts.AccountId
+import at.bitfire.davdroid.accounts.DbAccount
 import at.bitfire.davdroid.accounts.LegacyAccount
 
+private const val INPUT_ACCOUNT_ID = "accountId"
 private const val INPUT_ACCOUNT_NAME = "accountName"
 private const val INPUT_ACCOUNT_TYPE = "accountType"
 
@@ -19,18 +21,27 @@ fun Data.Builder.putAccountId(accountId: AccountId): Data.Builder {
             putString(INPUT_ACCOUNT_NAME, account.name)
             putString(INPUT_ACCOUNT_TYPE, account.type)
         }
+        is DbAccount -> {
+            val account = accountId.account
+            putLong(INPUT_ACCOUNT_ID, account.id)
+            putString(INPUT_ACCOUNT_NAME, account.name)
+        }
     }
 
     return this
 }
 
 fun Data.getAccountId(): AccountId? {
+    val accountId = getLong(INPUT_ACCOUNT_ID, 0L)
     val accountName = getString(INPUT_ACCOUNT_NAME)
     val accountType = getString(INPUT_ACCOUNT_TYPE)
 
     if (accountName != null && accountType != null) {
         val account = Account(accountName, accountType)
         return LegacyAccount(account)
+    } else if (accountId > 0 && accountName != null) {
+        val account = at.bitfire.davdroid.db.DbAccount(accountId, accountName)
+        return DbAccount(account)
     }
 
     return null
