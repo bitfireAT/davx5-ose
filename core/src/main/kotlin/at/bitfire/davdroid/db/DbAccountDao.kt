e9f654f5762cc92c966de7ee7a94ea9afd6e4e73
Arnau Mora Gras <arnyminerz@proton.me>
2026-08-18 13:13:08 +0200

    Add the required DAO operations
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- core/src/main/kotlin/at/bitfire/davdroid/db/DbAccountDao.kt
+++ core/src/main/kotlin/at/bitfire/davdroid/db/DbAccountDao.kt
@@ -5,23 +5,37 @@
 package at.bitfire.davdroid.db
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import org.jetbrains.annotations.TestOnly
 
 // will be used by AccountRepository
 @Dao
 interface DbAccountDao {
-    /**
-     * Currently only intended to be used by tests. Proper implementations will be provided at some point.
-     */
-    @Insert
-    @TestOnly
-    fun insert(dbAccount: DbAccount): Long
+    @Query("SELECT * FROM account WHERE id = :id")
+    fun getBlocking(id: Long): DbAccount?
 
-    /**
-     * Currently only intended to be used by tests. Proper implementations will be provided at some point.
-     */
+    @Query("SELECT * FROM account WHERE id = :id")
+    suspend fun get(id: Long): DbAccount?
+
+    @Query("SELECT * FROM account WHERE name = :name")
+    suspend fun getFromName(name: String): DbAccount?
+
+    @Insert
+    fun insertBlocking(dbAccount: DbAccount): Long
+
+    @Insert
+    suspend fun insert(dbAccount: DbAccount): Long
+
+    @Update
+    fun updateBlocking(dbAccount: DbAccount)
+
+    @Delete
+    fun deleteBlocking(dbAccount: DbAccount)
+
+    @TestOnly
     @Query("DELETE FROM account")
     fun deleteAllBlocking()
 }
