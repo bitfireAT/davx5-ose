7c4f51afd55809891d83453ef3a06289d8df04c3
Arnau Mora Gras <arnyminerz@proton.me>
2026-08-18 09:38:10 +0200

    Simplify test
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- core/src/androidTest/kotlin/at/bitfire/davdroid/accounts/AccountIdIntentSerializerTest.kt
+++ core/src/androidTest/kotlin/at/bitfire/davdroid/accounts/AccountIdIntentSerializerTest.kt
@@ -6,35 +6,12 @@ package at.bitfire.davdroid.accounts
 
 import android.accounts.Account
 import android.content.Intent
-import androidx.core.content.IntentCompat
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class AccountIdIntentSerializerTest {
     @Test
-    fun test_addExtra_LegacyAccount() {
-        val account = LegacyAccount(Account("test", "test"))
-        val intent = Intent().apply {
-            AccountIdIntentSerializer.addExtra(this, "account", account)
-        }
-        val restoredAccount = IntentCompat.getParcelableExtra(intent, "account", Account::class.java)
-        assertEquals(account.androidAccount, restoredAccount)
-    }
-
-    @Test
-    fun test_addExtra_DbAccount() {
-        val account = DbAccountId(123L)
-        val intent = Intent().apply {
-            AccountIdIntentSerializer.addExtra(this, "account", account)
-        }
-        val restoredAccount = intent.getLongExtra("account", 0L).takeIf { it > 0L }?.let { id ->
-            DbAccountId(id)
-        }
-        assertEquals(account, restoredAccount)
-    }
-
-    @Test
-    fun test_fromIntent_LegacyAccount() {
+    fun test_roundTrip_LegacyAccount() {
         val account = LegacyAccount(Account("test", "test"))
         val intent = Intent().apply {
             AccountIdIntentSerializer.addExtra(this, "account", account)
@@ -44,7 +21,7 @@ class AccountIdIntentSerializerTest {
     }
 
     @Test
-    fun test_fromIntent_DbAccount() {
+    fun test_roundTrip_DbAccount() {
         val account = DbAccountId(123L)
         val intent = Intent().apply {
             AccountIdIntentSerializer.addExtra(this, "account", account)
