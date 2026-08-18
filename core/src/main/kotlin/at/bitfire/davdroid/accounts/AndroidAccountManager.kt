c89cab13e699802f15c9c388a75ebbb273fb2b00
Arnau Mora Gras <arnyminerz@proton.me>
2026-08-18 09:23:30 +0200

    Adjust error
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- core/src/main/kotlin/at/bitfire/davdroid/accounts/AndroidAccountManager.kt
+++ core/src/main/kotlin/at/bitfire/davdroid/accounts/AndroidAccountManager.kt
@@ -19,7 +19,7 @@ class AndroidAccountManager @Inject constructor(
     fun getAndroidAccount(accountId: AccountId): Account {
         return when (accountId) {
             is LegacyAccount -> accountId.androidAccount
-            else -> throw IllegalArgumentException("accountId must be an instance of LegacyAccount")
+            is DbAccountId -> TODO("It's still not possible to generate an Android account from a database account ID.")
         }
     }
 
