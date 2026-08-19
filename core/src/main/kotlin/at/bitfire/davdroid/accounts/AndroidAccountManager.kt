4fee43c21deef505f35b13c770e6ae41220b0582
Arnau Mora Gras <arnyminerz@proton.me>
2026-08-19 08:28:37 +0200

    Implement `getAndroidAccount` for `DbAccountId`
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- core/src/main/kotlin/at/bitfire/davdroid/accounts/AndroidAccountManager.kt
+++ core/src/main/kotlin/at/bitfire/davdroid/accounts/AndroidAccountManager.kt
@@ -6,7 +6,11 @@ package at.bitfire.davdroid.accounts
 
 import android.accounts.Account
 import android.accounts.AccountManager
+import android.content.Context
 import android.os.Build
+import at.bitfire.davdroid.R
+import at.bitfire.davdroid.db.AppDatabase
+import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import javax.inject.Provider
 
@@ -14,12 +18,29 @@ import javax.inject.Provider
  * A class to manage Android [Account]s.
  */
 class AndroidAccountManager @Inject constructor(
-    private val accountManager: Provider<AccountManager>
+    private val accountManager: Provider<AccountManager>,
+    @ApplicationContext context: Context,
+    database: AppDatabase
 ) {
+    private val accountType = context.getString(R.string.account_type)
+
+    private val dbAccountDao = database.dbAccountDao()
+
+    /**
+     * Returns the Android [Account] for a given [AccountId].
+     * @return the Android account for the given [accountId]
+     * @throws NoSuchElementException if the account does not exist
+     */
     fun getAndroidAccount(accountId: AccountId): Account {
         return when (accountId) {
             is LegacyAccount -> accountId.androidAccount
-            is DbAccountId -> TODO("It's still not possible to generate an Android account from a database account ID.")
+            is DbAccountId -> {
+                // note: this currently depends on the account being created before. at some point, the system accounts
+                //       should be dynamically created, and this method should be able to create the account if it
+                //       doesn't exist yet. for now, we just throw an exception.
+                dbAccountDao.getBlocking(accountId.id)?.let { Account(it.name, accountType) }
+                    ?: throw NoSuchElementException("No account found for id $accountId")
+            }
         }
     }
 
