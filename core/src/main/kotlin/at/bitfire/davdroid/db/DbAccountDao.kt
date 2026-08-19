8047e1e1635c9f338a87c711298ab56bd746e4c2
Arnau Mora Gras <arnyminerz@proton.me>
2026-08-19 09:12:43 +0200

    Update required DAO operations
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- core/src/main/kotlin/at/bitfire/davdroid/db/DbAccountDao.kt
+++ core/src/main/kotlin/at/bitfire/davdroid/db/DbAccountDao.kt
@@ -5,10 +5,10 @@
 package at.bitfire.davdroid.db
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
 import org.jetbrains.annotations.TestOnly
 
 // will be used by AccountRepository
@@ -20,8 +20,17 @@ interface DbAccountDao {
     @Query("SELECT * FROM account WHERE id = :id")
     suspend fun get(id: Long): DbAccount?
 
+    @Query("SELECT * FROM account WHERE id = :id")
+    fun getFlow(id: Long): Flow<DbAccount?>
+
     @Query("SELECT * FROM account WHERE name = :name")
     suspend fun getFromName(name: String): DbAccount?
+
+    @Query("SELECT * FROM account")
+    fun getAllFlow(): Flow<List<DbAccount>>
+
+    @Query("SELECT * FROM account")
+    fun getAllBlocking(): List<DbAccount>
 
     @Insert
     fun insertBlocking(dbAccount: DbAccount): Long
@@ -37,9 +46,6 @@ interface DbAccountDao {
 
     @Query("UPDATE account SET name = :newName WHERE id = :id")
     suspend fun rename(id: Long, newName: String)
-
-    @Delete
-    fun deleteBlocking(dbAccount: DbAccount)
 
     @TestOnly
     @Query("DELETE FROM account")
