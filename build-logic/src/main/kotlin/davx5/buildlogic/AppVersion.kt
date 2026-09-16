aa57b0af5c39fd2ec30bf8db60621c789dfd80f8
Ricki Hirner <hirner@bitfire.at>
2026-09-16 13:22:58 +0200

    Bump version to 4.5.20-beta.2

--- build-logic/src/main/kotlin/davx5/buildlogic/AppVersion.kt
+++ build-logic/src/main/kotlin/davx5/buildlogic/AppVersion.kt
@@ -30,7 +30,7 @@ import davx5.buildlogic.AppVersion.NAME
  */
 object AppVersion {
 
-    const val CODE: Int = 405200002
-    const val NAME: String = "4.5.20-beta.1"
+    const val CODE: Int = 405200003
+    const val NAME: String = "4.5.20-beta.2"
 
 }
